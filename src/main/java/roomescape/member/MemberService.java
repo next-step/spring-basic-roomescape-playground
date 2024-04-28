8971f615e3e7bdab3fedc14b487610036ea090b0
yeon2lee <md04254@naver.com>
2024-04-28 13:42:28 +0900

    refactor: 사용자 정보 로직 조회

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -59,6 +59,16 @@ public class MemberService {
         throw new AuthorizationException();
     }
 
+    public Member extractMemberFromToken(String token) {
+        Long memberId = Long.valueOf(Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor("Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=".getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject());
+
+        return memberDao.findById(memberId);
+    }
+
     public boolean checkValidLogin(String principal, String credentials) {
         return memberDao.existByEmailAndPassword(principal, credentials);
     }
