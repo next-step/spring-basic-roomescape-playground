11731050dbac3b4fc376c4db0ee957c88eca5fdb
[Kkimdoyeon] <[ehdus5176@naver.com]>
2024-04-30 15:21:44 +0900

    Login Request, Response 파일 생성

---
+++ src/main/java/roomescape/member/LoginResponse.java
@@ -0,0 +1,13 @@
+package roomescape.member;
+
+public class LoginResponse {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public LoginResponse(String name) {
+        this.name = name;
+    }
+}
