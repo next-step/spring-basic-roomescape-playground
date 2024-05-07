24f01a2bdb2443134ed05d06fb750a8b3beb7c2b
이가연 <99721126+dlrkdus@users.noreply.github.com>
2024-05-07 18:05:35 +0900

    1단계 로그인

--- src/main/java/roomescape/member/MemberController.java
+++ src/main/java/roomescape/member/MemberController.java
@@ -3,6 +3,7 @@ package roomescape.member;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import java.net.URI;
 public class MemberController {
     private MemberService memberService;
 
+
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
@@ -23,6 +25,17 @@ public class MemberController {
     public ResponseEntity createMember(@RequestBody MemberRequest memberRequest) {
         MemberResponse member = memberService.createMember(memberRequest);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).body(member);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody MemberRequest memberRequest, HttpServletResponse response){
+        MemberResponse member = memberService.findMember(memberRequest.getEmail(),memberRequest.getPassword()); //멤버조회
+        String token=memberService.createToken(member); //조회된 멤버 정보로 토큰 생성
+        Cookie cookie = new Cookie("token", token); //생성한 토큰으로 쿠키를 생성
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     @PostMapping("/logout")
