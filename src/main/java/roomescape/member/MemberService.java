6ace470ea1e64557df4528ea9337f51f2f05ff4d
최현영 <chy0310@nsonesoft.com>
2024-05-11 18:32:12 +0900

    feat: Authentication and Authorization

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -1,17 +1,43 @@
 package roomescape.member;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
+import roomescape.util.CookieUtil;
+import roomescape.util.JwtUtil;
+
 
 @Service
 public class MemberService {
     private MemberDao memberDao;
+    private JwtUtil jwtUtil;
 
-    public MemberService(MemberDao memberDao) {
+    private CookieUtil cookieUtil;
+
+
+    public MemberService(MemberDao memberDao, JwtUtil jwtUtil, CookieUtil cookieUtil) {
         this.memberDao = memberDao;
+        this.cookieUtil = cookieUtil;
+        this.jwtUtil = jwtUtil;
     }
 
-    public MemberResponse createMember(MemberRequest memberRequest) {
+    public MemberResponse.MemberInfoResponse createMember(MemberRequest memberRequest) {
         Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
-        return new MemberResponse(member.getId(), member.getName(), member.getEmail());
+
+        return new MemberResponse.MemberInfoResponse(member.getId(), member.getName(), member.getEmail());
     }
+
+    public String login(String email, String password) {
+        Member member = memberDao.findByEmailAndPassword(email, password);
+
+        return jwtUtil.createToken(member.getId(), member.getName(), member.getRole());
+    }
+
+    public MemberResponse.AuthorizationResponse check(HttpServletRequest request) {
+        String token = cookieUtil.extractTokenFromCookie(request);
+
+        return new MemberResponse.AuthorizationResponse(jwtUtil.getName(token), jwtUtil.getRole(token));
+    }
+
+
 }
