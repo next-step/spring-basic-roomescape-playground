c6f7d0a74df1106726354ae37de5cb0f060d7fda
Rulu <hafnium1923@gmail.com>
2024-05-14 23:05:11 +0900

    feat: Reservation 연관관계 매핑

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -2,29 +2,46 @@ package roomescape.reservation;
 
 import org.springframework.stereotype.Service;
 import roomescape.member.MemberResponse;
+import roomescape.member.MemberService;
+import roomescape.theme.Theme;
+import roomescape.theme.ThemeRepository;
+import roomescape.time.Time;
+import roomescape.time.TimeRepository;
 
 import java.util.List;
 
 @Service
 public class ReservationService {
-    private ReservationDao reservationDao;
+    private MemberService memberService;
+    private ReservationRepository reservationRepository;
+    private TimeRepository timeRepository;
+    private ThemeRepository themeRepository;
 
-    public ReservationService(ReservationDao reservationDao) {
-        this.reservationDao = reservationDao;
+    public ReservationService(ReservationRepository reservationRepository, MemberService memberService, TimeRepository timeRepository, ThemeRepository themeRepository) {
+        this.reservationRepository = reservationRepository;
+        this.memberService = memberService;
+        this.timeRepository = timeRepository;
+        this.themeRepository = themeRepository;
     }
-
     public ReservationResponse save(MemberResponse memberResponse, ReservationRequest reservationRequest) {
-        Reservation reservation = reservationDao.save( reservationRequest.getName() == null? new ReservationRequest(memberResponse.getName(),reservationRequest.getDate(),reservationRequest.getTheme(),reservationRequest.getTime()) : reservationRequest);
+        Time time = timeRepository.findById(reservationRequest.getTime()).orElseThrow(RuntimeException::new);
+        Theme theme = themeRepository.findById(reservationRequest.getTheme()).orElseThrow(RuntimeException::new);
+        String name = reservationRequest.getName() == null ? findMemberName(memberResponse) : reservationRequest.getName();
 
+        Reservation reservation = reservationRepository.save(new Reservation(name, reservationRequest.getDate(), time, theme));
         return new ReservationResponse(reservation.getId(), reservation.getName(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
     }
 
+    private String findMemberName(MemberResponse loginMember) {
+        return memberService.findById(loginMember.getId()).getName();
+    }
+
     public void deleteById(Long id) {
-        reservationDao.deleteById(id);
+        reservationRepository.deleteById(id);
     }
 
     public List<ReservationResponse> findAll() {
-        return reservationDao.findAll().stream()
+        return reservationRepository.findAll().stream()
                 .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
                 .toList();
     }
