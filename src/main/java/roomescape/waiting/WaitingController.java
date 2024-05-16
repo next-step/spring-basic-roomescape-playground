e3528eec38a696babc4474c304747a43eda33dfd
Rulu <hafnium1923@gmail.com>
2024-05-16 16:52:38 +0900

    feat: 예약 대기 요청 기능 구현

---
+++ src/main/java/roomescape/waiting/WaitingController.java
@@ -0,0 +1,27 @@
+package roomescape.waiting;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.member.MemberResponse;
+
+import java.net.URI;
+
+@RestController
+public class WaitingController {
+    private final WaitingService waitingService;
+
+    public WaitingController(WaitingService waitingService) {
+        this.waitingService = waitingService;
+    }
+
+    @PostMapping("/waitings")
+    public ResponseEntity<WaitingResponse> createWaiting(@RequestBody WaitingRequest waitingRequest, MemberResponse memberResponse) {
+        WaitingResponse waitingResponse = waitingService.createWaiting(memberResponse, waitingRequest);
+        return ResponseEntity.created(URI.create("/waitings/" + waitingResponse.getId())).body(waitingResponse);
+    }
+
+}
