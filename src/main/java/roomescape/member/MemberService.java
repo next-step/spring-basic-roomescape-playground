242b03992cc34bd2ceb812478e5d145983d8dddb
nyeroni <nir2y@naver.com>
2024-06-05 21:15:24 +0900

    feat: findById 및 findByEmailAndPassword 구현

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -14,4 +14,14 @@ public class MemberService {
         Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
     }
+
+    public ViewMemberResponse findMemberByEmailAndPassword (String email, String password) {
+        Member member = memberDao.findByEmailAndPassword(email, password);
+        return new ViewMemberResponse(member.getId(), member.getName(), member.getEmail(), member.getRole());
+    }
+
+    public ViewMemberResponse findMemberById (Long id) {
+        Member member = memberDao.findById(id);
+        return new ViewMemberResponse(member.getId(), member.getName(), member.getEmail(), member.getRole());
+    }
 }
