b8323702f2c59056f5dbd47e96b6211be0ff3518
mangsuyo <mangsuyo3610@gmail.com>
2024-06-23 22:05:51 +0900

    feat: [1단계] 로그인 요청시 토큰 발급

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -2,12 +2,24 @@ package roomescape.member;
 
 import org.springframework.stereotype.Service;
 
+import roomescape.jwt.JwtProvider;
+
 @Service
 public class MemberService {
     private MemberDao memberDao;
+    private JwtProvider jwtProvider;
 
     public MemberService(MemberDao memberDao) {
         this.memberDao = memberDao;
+        this.jwtProvider = new JwtProvider();
+    }
+
+    public String login(MemberLoginRequest request) {
+        Member member = memberDao.findByEmailAndPassword(request.email(), request.password());
+        if(member == null){
+            throw new IllegalArgumentException("Invalid email or password");
+        }
+        return jwtProvider.createToken(member);
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
