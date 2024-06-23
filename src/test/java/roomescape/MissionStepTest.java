814f97767de18604a7db4113caa1e34f4300cd35
mangsuyo <mangsuyo3610@gmail.com>
2024-06-23 22:43:39 +0900

    feat: [2단계] 테스트 적용

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -1,47 +1,82 @@
 package roomescape;
 
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import roomescape.reservation.ReservationResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class MissionStepTest {
 
-    @Test
-    void 일단계() {
-        Map<String, String> params = new HashMap<>();
-        params.put("email", "admin@email.com");
-        params.put("password", "password");
+	@Test
+	void 일단계() {
+		Map<String, String> params = new HashMap<>();
+		params.put("email", "admin@email.com");
+		params.put("password", "password");
 
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(params)
-                .when().post("/login")
-                .then().log().all()
-                .statusCode(200)
-                .extract();
+		ExtractableResponse<Response> response = RestAssured.given().log().all()
+			.contentType(ContentType.JSON)
+			.body(params)
+			.when().post("/login")
+			.then().log().all()
+			.statusCode(200)
+			.extract();
 
-        String token = response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
-        assertThat(token).isNotBlank();
+		String token = response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
+		assertThat(token).isNotBlank();
 
-        ExtractableResponse<Response> checkResponse = RestAssured.given().log().all()
-            .contentType(ContentType.JSON)
-            .cookie("token", token)
-            .when().get("/login/check")
-            .then().log().all()
-            .statusCode(200)
-            .extract();
+		ExtractableResponse<Response> checkResponse = RestAssured.given().log().all()
+			.contentType(ContentType.JSON)
+			.cookie("token", token)
+			.when().get("/login/check")
+			.then().log().all()
+			.statusCode(200)
+			.extract();
 
-        assertThat(checkResponse.body().jsonPath().getString("name")).isEqualTo("어드민");
-    }
+		assertThat(checkResponse.body().jsonPath().getString("name")).isEqualTo("어드민");
+	}
+
+	@Test
+	void 이단계() {
+		String token = createToken("admin@email.com", "password");  // 일단계에서 토큰을 추출하는 로직을 메서드로 따로 만들어서 활용하세요.
+		Map<String, String> params = new HashMap<>();
+		params.put("date", "2024-03-01");
+		params.put("time", "1");
+		params.put("theme", "1");
+
+		ExtractableResponse<Response> response = RestAssured.given().log().all()
+			.body(params)
+			.cookie("token", token)
+			.contentType(ContentType.JSON)
+			.post("/reservations")
+			.then().log().all()
+			.extract();
+
+		assertThat(response.statusCode()).isEqualTo(201);
+		assertThat(response.as(ReservationResponse.class).getName()).isEqualTo("어드민");
+
+		params.put("name", "브라운");
+
+		ExtractableResponse<Response> adminResponse = RestAssured.given().log().all()
+			.body(params)
+			.cookie("token", token)
+			.contentType(ContentType.JSON)
+			.post("/reservations")
+			.then().log().all()
+			.extract();
+
+		assertThat(adminResponse.statusCode()).isEqualTo(201);
+		assertThat(adminResponse.as(ReservationResponse.class).getName()).isEqualTo("브라운");
+	}
 }
