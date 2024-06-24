8e375adfd7025078483b840dcfea8298e77b362f
Kyuwon-Choi <kyuwon30@naver.com>
2024-06-25 00:24:31 +0900

    step 3

--- src/main/java/roomescape/auth/WebConfig.java
+++ src/main/java/roomescape/auth/WebConfig.java
@@ -3,6 +3,7 @@ package roomescape.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,9 +13,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
     private LoginMemberArgumentResolver loginMemberArgumentResolver;
+    @Autowired
+    private AdminInterceptor adminInterceptor;
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginMemberArgumentResolver);
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(adminInterceptor)
+                .addPathPatterns("/admin/**");
     }
 }
