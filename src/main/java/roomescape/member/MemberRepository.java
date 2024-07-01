205fc735bef96d9f390a372d4ecd148c1caa4460
Sumin <new.sumyang@gmail.com>
2024-07-01 12:50:15 +0900

    불필요한 부분 삭제

--- src/main/java/roomescape/member/MemberRepository.java
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -1,16 +1,12 @@
 package roomescape.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @Query("select m from Member m where m.email = :email and m.password = :password")
     Member findByEmailAndPassword(String email, String password);
 
-    @Query("select m from Member m where m.name=:name")
     Member findByName(String name);
 
-    @Query("select m from Member m where m.id=:id")
     Member findMemberById(Long id);
 }
--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -1,14 +1,11 @@
 package roomescape.reservation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-//    @Query("select r from Reservation r where r.date = :date and r.theme.id = :themeId")
     List<Reservation> findByDateAndThemeId(String date, Long themeId);
 
-//    @Query("select r from Reservation r where r.member.id = :memberId")
     List<Reservation> findByMemberId(Long memberId);
 }
