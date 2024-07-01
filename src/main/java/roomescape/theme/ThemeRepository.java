396a3a63f14bdce304792c94668663e60ebfa443
kokodak <kokodakadokok@gmail.com>
2024-07-02 02:25:08 +0900

    feat: 단일 조회 메서드에서 default 로 공통 예외처리 하도록 수정

--- src/main/java/roomescape/theme/ThemeRepository.java
+++ src/main/java/roomescape/theme/ThemeRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
 
+    default Theme getById(Long id) {
+        return this.findById(id)
+                   .orElseThrow(() -> new IllegalArgumentException("Theme not found"));
+    }
 }
--- src/main/java/roomescape/time/TimeRepository.java
+++ src/main/java/roomescape/time/TimeRepository.java
@@ -4,4 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TimeRepository extends JpaRepository<Time, Long> {
 
+    default Time getById(Long id) {
+        return this.findById(id)
+                   .orElseThrow(() -> new IllegalArgumentException("Time not found"));
+    }
 }
