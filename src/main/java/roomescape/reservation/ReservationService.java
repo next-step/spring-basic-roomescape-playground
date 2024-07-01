a7b2ca2496edf264e72eb199985fcd8b0dd89ee5
kokodak <kokodakadokok@gmail.com>
2024-07-02 05:15:22 +0900

    feat: 중복 예약이 불가능하도록 구현

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -8,20 +8,25 @@ import roomescape.theme.Theme;
 import roomescape.theme.ThemeRepository;
 import roomescape.time.Time;
 import roomescape.time.TimeRepository;
+import roomescape.waiting.WaitingRepository;
+import roomescape.waiting.WaitingWithRank;
 
 @Service
 public class ReservationService {
 
     private final ReservationRepository reservationRepository;
+    private final WaitingRepository waitingRepository;
     private final ThemeRepository themeRepository;
     private final TimeRepository timeRepository;
     private final MemberRepository memberRepository;
 
     public ReservationService(ReservationRepository reservationRepository,
+                              WaitingRepository waitingRepository,
                               ThemeRepository themeRepository,
                               TimeRepository timeRepository,
                               MemberRepository memberRepository) {
         this.reservationRepository = reservationRepository;
+        this.waitingRepository = waitingRepository;
         this.themeRepository = themeRepository;
         this.timeRepository = timeRepository;
         this.memberRepository = memberRepository;
@@ -32,6 +37,8 @@ public class ReservationService {
         Time time = timeRepository.getById(reservationRequest.time());
         Member member = memberRepository.findByName(reservationRequest.name())
                                         .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        validateExistence(reservationRequest, theme, time);
 
         Reservation reservation = new Reservation(reservationRequest.name(),
                                                   reservationRequest.date(),
@@ -44,6 +51,13 @@ public class ReservationService {
         return new ReservationResponse(reservation.getId(), reservationRequest.name(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
     }
 
+    private void validateExistence(final ReservationRequest reservationRequest, final Theme theme, final Time time) {
+        reservationRepository.findByDateAndThemeIdAndTimeId(reservationRequest.date(), theme.getId(), time.getId())
+                             .ifPresent(it -> {
+                                 throw new IllegalArgumentException("이미 예약된 시간입니다.");
+                             });
+    }
+
     public void deleteById(Long id) {
         reservationRepository.deleteById(id);
     }
@@ -54,9 +68,13 @@ public class ReservationService {
                                     .toList();
     }
 
-    public List<ReservationResponse> findAllByMemberName(String name) {
-        return reservationRepository.findByName(name).stream()
-                                    .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
-                                    .toList();
+    public List<MyReservationResponse> findMyReservationsByName(String name) {
+        Member member = memberRepository.findByName(name)
+                                        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다."));
+
+        final List<Reservation> reservations = reservationRepository.findByMemberId(member.getId());
+        final List<WaitingWithRank> waitingWithRanks = waitingRepository.findWaitingsWithRankByMemberId(member.getId());
+
+        return MyReservationResponse.of(reservations, waitingWithRanks);
     }
 }
