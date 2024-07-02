5f9ba747db72f36cd5215f1d0485c466f09a1d56
nyeroni <nir2y@naver.com>
2024-07-03 00:23:27 +0900

    feat: member jpa로 변경

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -4,24 +4,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-    private MemberDao memberDao;
+    private MemberRepository memberRepository;
 
-    public MemberService(MemberDao memberDao) {
-        this.memberDao = memberDao;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
-        Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
+        Member member = memberRepository.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
     }
 
     public ViewMemberResponse findMemberByEmailAndPassword (String email, String password) {
-        Member member = memberDao.findByEmailAndPassword(email, password);
+        Member member = memberRepository.findByEmailAndPassword(email, password);
         return new ViewMemberResponse(member.getId(), member.getName(), member.getEmail(), member.getRole());
     }
 
     public ViewMemberResponse findMemberById (Long id) {
-        Member member = memberDao.findById(id);
+        Member member = memberRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("찾을 수 없는 member id 입니다."));
         return new ViewMemberResponse(member.getId(), member.getName(), member.getEmail(), member.getRole());
     }
 }
