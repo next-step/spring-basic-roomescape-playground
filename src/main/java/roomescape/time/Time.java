1f4b2f0928c78bd38300bf84882cc6510dd44a43
nyeroni <nir2y@naver.com>
2024-07-03 00:21:38 +0900

    feat: time jpa로 변경

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,6 +1,16 @@
 package roomescape.time;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
 public class Time {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
     private String value;
 
