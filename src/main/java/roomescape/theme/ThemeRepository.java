ce4c9fa3a688761466c9deb56e06765870abc575
nyeroni <nir2y@naver.com>
2024-07-03 00:24:36 +0900

    feat: theme jpa로 변경

---
+++ src/main/java/roomescape/theme/ThemeRepository.java
@@ -0,0 +1,8 @@
+package roomescape.theme;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+}
