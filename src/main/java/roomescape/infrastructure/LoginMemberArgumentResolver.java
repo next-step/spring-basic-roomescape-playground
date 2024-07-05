a67c4b492ac48fbd42c25b72c9c179ef0d42bdab
gogo1414 <wnstn6945@gmail.com>
2024-07-05 21:57:33 +0900

    어노테이션 생성

--- src/main/java/roomescape/infrastructure/LoginMemberArgumentResolver.java
+++ src/main/java/roomescape/infrastructure/LoginMemberArgumentResolver.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.auth.LoginSession;
 import roomescape.member.LoginMember;
 import roomescape.member.Member;
 import roomescape.member.MemberService;
@@ -24,7 +25,8 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return parameter.getParameterType().equals(LoginMember.class);
+        return parameter.hasParameterAnnotation(LoginSession.class)
+                && parameter.getParameterType().equals(LoginMember.class);
     }
 
     @Override
