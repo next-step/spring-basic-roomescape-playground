468474ac82b8de9e1c0bf8ac8bb841118a0f80e6
Yehyeok Bang <qkddpgur318@gmail.com>
2024-07-29 17:09:11 +0900

    refactor: 역할 검증 로직 수정
    - 기존 열거형 타입 순서에 의존하던 방식에서 일치하는 지 확인하는 방식으로 수정합니다.

--- src/main/java/roomescape/global/login/RoleCheckInterceptor.java
+++ src/main/java/roomescape/global/login/RoleCheckInterceptor.java
@@ -10,6 +10,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+
 @Component
 public class RoleCheckInterceptor implements HandlerInterceptor {
     private static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
@@ -18,29 +21,45 @@ public class RoleCheckInterceptor implements HandlerInterceptor {
     public boolean preHandle(@NonNull HttpServletRequest request,
                              @NonNull HttpServletResponse response,
                              @NonNull Object handler) {
-        HandlerMethod handlerMethod = (HandlerMethod) handler;
-        RoleCheck roleCheck = handlerMethod.getMethodAnnotation(RoleCheck.class);
 
+        if (!(handler instanceof HandlerMethod handlerMethod)) {
+            return true;
+        }
+
+        RoleCheck roleCheck = handlerMethod.getMethodAnnotation(RoleCheck.class);
         if (roleCheck == null) {
             return true;
         }
 
-        Cookie[] cookies = request.getCookies();
+        Cookie[] cookies = getCookies(request);
         String token = extractTokenFromCookie(cookies);
 
         if (token.isEmpty()) {
-            response.setStatus(401);
+            int statusCode = UNAUTHORIZED.value();
+            response.setStatus(statusCode);
             return false;
         }
 
-        String role = parseMemberRole(token);
+        Role requiredRole = roleCheck.role();
+        Role memberRole = parseMemberRole(token);
+        if (memberRole == Role.ADMIN) {
+            return true;
+        }
 
-        if (Role.valueOf(role).ordinal() < roleCheck.role().ordinal()) {
-            response.setStatus(403);
+        if (isNotRoleExact(requiredRole, memberRole)) {
+            response.setStatus(FORBIDDEN.value());
             return false;
         }
 
         return true;
+    }
+
+    private boolean isNotRoleExact(Role requiredRole, Role memberRole) {
+        return requiredRole != memberRole;
+    }
+
+    private Cookie[] getCookies(HttpServletRequest request) {
+        return request.getCookies();
     }
 
     private String extractTokenFromCookie(Cookie[] cookies) {
@@ -53,12 +72,12 @@ public class RoleCheckInterceptor implements HandlerInterceptor {
         return "";
     }
 
-    private String parseMemberRole(String token) {
-        return Jwts.parserBuilder()
+    private Role parseMemberRole(String token) {
+        return Role.valueOf(Jwts.parserBuilder()
                 .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
-                .get("role", String.class);
+                .get("role", String.class));
     }
 }
