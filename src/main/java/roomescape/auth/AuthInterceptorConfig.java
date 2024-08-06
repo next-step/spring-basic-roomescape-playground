152a202d83deba4f296627566f01c3aaac0b36e8
Junyeong-An <2117ab@gmail.com>
2024-08-06 18:25:33 +0900

    refactor: 필요없는 import문 제거

--- src/main/java/roomescape/auth/AuthInterceptorConfig.java
+++ src/main/java/roomescape/auth/AuthInterceptorConfig.java
@@ -2,7 +2,6 @@ package roomescape.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import roomescape.interceptor.AuthInterceptor;
 
 @Configuration
 public class AuthInterceptorConfig {
