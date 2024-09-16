1108c4a4365e07af734b453938453d2d92e014a6
DHkimgit <dukekim001@naver.com>
2024-09-16 20:12:00 +0900

    feat: 2단계 - 로그인 리팩터링 구현

---
+++ src/main/java/roomescape/global/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,53 @@
+package roomescape.global.auth;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import roomescape.member.Member;
+import roomescape.member.MemberService;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+    private final CookieUtil cookieUtil;
+
+    @Autowired
+    public LoginMemberArgumentResolver(MemberService memberService, CookieUtil cookieUtil) {
+        this.memberService = memberService;
+        this.cookieUtil = cookieUtil;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterAnnotation(LoginMember.class) != null
+            && parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+        @NotNull MethodParameter parameter,
+        ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest,
+        WebDataBinderFactory binderFactory
+    ) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        Cookie[] cookies = request.getCookies();
+
+        String token = cookieUtil.extractTokenFromCookie(cookies);
+
+        if (!token.isEmpty()) {
+            return memberService.getAuth(token);
+        } else{
+            return null;
+        }
+    }
+}
