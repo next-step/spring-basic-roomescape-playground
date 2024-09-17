7de469b89f66a9c00b55b69e6c8fd1cc6b88e44e
DHkimgit <dukekim001@naver.com>
2024-09-17 14:39:19 +0900

    feat: 3단계 - 관리자 기능 구현

--- src/main/java/roomescape/global/config/WebConfig.java
+++ src/main/java/roomescape/global/config/WebConfig.java
@@ -2,23 +2,38 @@ package roomescape.global.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import roomescape.global.auth.AuthorizationInterceptor;
 import roomescape.global.auth.LoginMemberArgumentResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final AuthorizationInterceptor authorizationInterceptor;
 
-    public WebConfig(final LoginMemberArgumentResolver loginMemberArgumentResolver) {
+    @Autowired
+    public WebConfig(
+        LoginMemberArgumentResolver loginMemberArgumentResolver,
+        AuthorizationInterceptor authorizationInterceptor
+    ) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.authorizationInterceptor = authorizationInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authorizationInterceptor)
+            .addPathPatterns("/admin/**");
     }
 }
