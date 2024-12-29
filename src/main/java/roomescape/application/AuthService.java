aa6384d4408969562b32ef418fb5501a6f3ecda7
sinji <tobeabestns@naver.com>
2024-12-30 01:56:27 +0900

    refactor(AuthService): [1단계] 인증 실패 예외처리 통일

--- src/main/java/roomescape/application/AuthService.java
+++ src/main/java/roomescape/application/AuthService.java
@@ -21,7 +21,7 @@ public class AuthService {
     public MemberResponse findMemberByEmail(String email) {
         Member member = memberDao.findByEmail(email);
         if (member == null) {
-            throw new IllegalArgumentException("사용자를 찾을 수 없습니다.");
+            throw new AuthorizationException("사용자를 찾을 수 없습니다.");
         }
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
     }
@@ -40,7 +40,10 @@ public class AuthService {
 
     public boolean checkInvalidLogin(String email, String password) {
         Member member = memberDao.findByEmailAndPassword(email, password);
-        return member == null;
+        if(member == null){
+            throw new AuthorizationException("이메일 또는 비밀번호가 잘못되었습니다.");
+        }
+        return false;
     }
 
     public boolean verifyToken(String token) {
