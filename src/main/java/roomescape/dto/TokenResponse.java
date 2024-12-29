1f22ed7bf23b844bafef7d1e2893e285af7f1088
sinji <tobeabestns@naver.com>
2024-12-30 01:39:08 +0900

    feat(TokenResponse): [1단계] 로그인 응답 클래스 생성

---
+++ src/main/java/roomescape/dto/TokenResponse.java
@@ -0,0 +1,13 @@
+package roomescape.dto;
+
+public class TokenResponse {
+    private String accessToken;
+
+    public TokenResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
