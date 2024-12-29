3569c1377814c2622bf4df91e92aef30448b0910
sinji <tobeabestns@naver.com>
2024-12-30 01:33:59 +0900

    feat(AuthService): [1단계] 사용자 인증 처리 클래스 생성

---
+++ src/main/java/roomescape/application/AuthService.java
@@ -0,0 +1,53 @@
+package roomescape.application;
+
+import org.springframework.stereotype.Service;
+import roomescape.dto.TokenRequest;
+import roomescape.dto.TokenResponse;
+import roomescape.infrastructure.JwtTokenProvider;
+import roomescape.member.Member;
+import roomescape.member.MemberDao;
+import roomescape.member.MemberResponse;
+
+@Service
+public class AuthService {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final MemberDao memberDao;
+
+    public AuthService(JwtTokenProvider jwtTokenProvider, MemberDao memberDao) {
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.memberDao = memberDao;
+    }
+
+    public MemberResponse findMemberByEmail(String email) {
+        Member member = memberDao.findByEmail(email);
+        if (member == null) {
+            throw new IllegalArgumentException("사용자를 찾을 수 없습니다.");
+        }
+        return new MemberResponse(member.getId(), member.getName(), member.getEmail());
+    }
+
+    public String getEmailFromToken(String token) {
+        return jwtTokenProvider.getPayload(token);
+    }
+
+    public TokenResponse createToken(TokenRequest tokenRequest) {
+        if (checkInvalidLogin(tokenRequest.getEmail(), tokenRequest.getPassword())) {
+            throw new AuthorizationException();
+        }
+        String accessToken = jwtTokenProvider.createToken(tokenRequest.getEmail());
+        return new TokenResponse(accessToken);
+    }
+
+    public boolean checkInvalidLogin(String email, String password) {
+        Member member = memberDao.findByEmailAndPassword(email, password);
+        return member == null;
+    }
+
+    public boolean verifyToken(String token) {
+        try {
+            return jwtTokenProvider.validateToken(token);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
