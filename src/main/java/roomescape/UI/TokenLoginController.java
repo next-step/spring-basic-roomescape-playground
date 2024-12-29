088a20026e41d44ba383f59b258c4d0b8bd11d08
sinji <tobeabestns@naver.com>
2024-12-30 01:26:35 +0900

    feat(TokenLoginController): [1단계] 사용자 인증 및 로그인 요청 처리 기능 추가(tokenLogin)

---
+++ src/main/java/roomescape/UI/TokenLoginController.java
@@ -0,0 +1,38 @@
+package roomescape.UI;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.application.AuthService;
+import roomescape.dto.TokenRequest;
+import roomescape.dto.TokenResponse;
+import roomescape.infrastructure.AuthorizationExtractor;
+import roomescape.infrastructure.BearerAuthorizationExtractor;
+import roomescape.member.MemberResponse;
+
+@RestController
+public class TokenLoginController {
+    private final AuthService authService;
+    private final AuthorizationExtractor<String> authorizationExtractor;
+
+    public TokenLoginController(AuthService authService) {
+        this.authService = authService;
+        this.authorizationExtractor = new BearerAuthorizationExtractor();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenResponse> tokenLogin(@RequestBody TokenRequest tokenRequest, HttpServletResponse response){
+        TokenResponse tokenResponse = authService.createToken(tokenRequest);
+        Cookie cookie = new Cookie("token", tokenResponse.getAccessToken());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.ok().body(tokenResponse);
+    }
+}
