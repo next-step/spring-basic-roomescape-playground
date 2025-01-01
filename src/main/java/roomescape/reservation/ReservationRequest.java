57a7668b65bf487f4e5a68f78435e86c5199c5bf
sinji <tobeabestns@naver.com>
2025-01-01 22:13:02 +0900

    refactor(ReservationRequest): [2단계] DB에서 회원 데이터를 조회하기 위해 memberId 필드 추가

--- src/main/java/roomescape/reservation/ReservationRequest.java
+++ src/main/java/roomescape/reservation/ReservationRequest.java
@@ -1,13 +1,22 @@
 package roomescape.reservation;
 
 public class ReservationRequest {
+    private Long memberId;
     private String name;
     private String date;
     private Long theme;
     private Long time;
 
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDate() {
