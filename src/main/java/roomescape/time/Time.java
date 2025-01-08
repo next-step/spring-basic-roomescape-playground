920de2e097db5162844b41e870e540e8a8c6ba29
sinji <tobeabestns@naver.com>
2025-01-09 01:11:36 +0900

    refactor(TimeService): [4단계] Dao -> Repository 로직 변경

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,27 +1,36 @@
 package roomescape.time;
 
-public class Time {
-    private Long id;
-    private String value;
+import jakarta.persistence.*;
 
-    public Time(Long id, String value) {
+@Entity
+@Table(name = "time")
+public class Time {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "time_value")
+    private String time;
+
+    @Column(name = "deleted")
+    private Boolean deleted;
+
+    public Time(Long id, String time) {
         this.id = id;
-        this.value = value;
+        this.time = time;
     }
 
-    public Time(String value) {
-        this.value = value;
+    public Time(String time) {
+        this.time = time;
     }
 
     public Time() {
-
     }
 
     public Long getId() {
         return id;
     }
 
-    public String getValue() {
-        return value;
+    public String getTime(){
+        return time;
     }
 }
