664d873fff28e1e570956ba9b041d4fee203a65a
sinji <tobeabestns@naver.com>
2025-01-09 01:15:25 +0900

    refactor(Theme): [4단계] Theme 클래스 엔티티 설정 및 매핑

--- src/main/java/roomescape/theme/Theme.java
+++ src/main/java/roomescape/theme/Theme.java
@@ -1,11 +1,27 @@
 package roomescape.theme;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Theme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "deleted")
+    private Boolean deleted;
+
     public Theme() {
+    }
+
+    public Theme(Long id) {
+        this.id = id;
     }
 
     public Theme(Long id, String name, String description) {
