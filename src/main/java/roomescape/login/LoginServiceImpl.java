2d2c452c6bd8c7c3003019e4b353dbe10714ef24
haeyoon1 <hynam1220@naver.com>
2025-01-10 07:00:58 +0900

    feat: 로그인 기능, 토큰 발급, 사용자 정보 검증

---
+++ src/main/java/roomescape/login/LoginServiceImpl.java
@@ -0,0 +1,38 @@
+package roomescape.login;
+
+import org.springframework.stereotype.Service;
+import roomescape.jwt.JwtProvider;
+import roomescape.member.Member;
+import roomescape.member.MemberDao;
+import roomescape.member.MemberResponse;
+
+@Service
+public class LoginServiceImpl implements LoginService{
+
+    private final MemberDao memberDao;
+    private final JwtProvider jwtProvider;
+
+    public LoginServiceImpl(MemberDao memberDao, JwtProvider jwtProvider) {
+        this.memberDao = memberDao;
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public String login(String email, String password) {
+        Member member = memberDao.findByEmailAndPassword(email, password);
+        return jwtProvider.generateToken(member);
+    }
+
+    @Override
+    public MemberResponse validateToken(String token) {
+
+        if (!jwtProvider.isValidToken(token)) {
+            throw new IllegalArgumentException("Invalid token");
+        }
+
+        Long memberId = Long.valueOf(jwtProvider.extractSubject(token));
+        Member member = memberDao.findByName(memberId.toString());
+
+        return new MemberResponse(member.getId(), member.getName(), member.getEmail());
+    }
+}
