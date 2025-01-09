0c79c6520dcd27a1d765682fbbfed0118933d9d1
haeyoon1 <hynam1220@naver.com>
2025-01-10 06:50:53 +0900

    feat: JWT 토큰 생성 구현

---
+++ src/main/java/roomescape/jwt/JwtProvider.java
@@ -0,0 +1,10 @@
+package roomescape.jwt;
+
+import roomescape.member.Member;
+
+public interface JwtProvider {
+
+    String generateToken(Member member);
+    boolean isValidToken(String token);
+    Long extractSubject(String token);
+}
---
+++ src/main/java/roomescape/jwt/JwtProviderImpl.java
@@ -0,0 +1,47 @@
+package roomescape.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import roomescape.member.Member;
+
+import java.nio.charset.StandardCharsets;
+
+
+public class JwtProviderImpl implements JwtProvider{
+
+    private static final String SECRET_KEY="Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+    // private static final long EXPRIATION_TIME = 86400000; // 1일
+
+    @Override
+    public String generateToken(Member member) {
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                .compact();
+    }
+
+    @Override
+    public boolean isValidToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)))
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public Long extractSubject(String token) {
+        Long memberId = Long.valueOf(Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject());
+        return memberId;
+    }
+}
