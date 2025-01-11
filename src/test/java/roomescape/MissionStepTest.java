595ad0002546b3e9ede6a20f2e9bf63e84c1a493
sinji <tobeabestns@naver.com>
2025-01-12 04:35:54 +0900

    refactor(MissionStepTest): [5단계] 오단계 테스트 코드 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -10,10 +10,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.MyReservationResponse;
 import roomescape.reservation.ReservationResponse;
 import roomescape.time.TimeRepository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -126,17 +128,17 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
-//    @Test
-//    void 오단계() {
-//        String adminToken = createToken("admin@email.com", "password");
-//
-//        List<MyReservationResponse> reservations = RestAssured.given().log().all()
-//                .cookie("token", adminToken)
-//                .get("/reservations-mine")
-//                .then().log().all()
-//                .statusCode(200)
-//                .extract().jsonPath().getList(".", MyReservationResponse.class);
-//
-//        assertThat(reservations).hasSize(3);
-//    }
+    @Test
+    void 오단계() {
+        String adminToken = createToken("admin@email.com", "password");
+
+        List<MyReservationResponse> reservations = RestAssured.given().log().all()
+                .cookie("token", adminToken)
+                .get("/reservations-mine")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList(".", MyReservationResponse.class);
+
+        assertThat(reservations).hasSize(3);
+    }
 }
