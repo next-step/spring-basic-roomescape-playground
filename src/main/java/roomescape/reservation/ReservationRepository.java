f030098c66e983874a661d43d60a78a458f7013b
sinji <tobeabestns@naver.com>
2025-01-13 03:25:14 +0900

    refactor(ReservationRepository): [6단계] exist 메서드 추가

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> { // Long -> (pk)의 자료형
     List<Reservation> findReservationByDateAndThemeId(String date, Long themeId);
     List<Reservation> findByName(String name);
+    boolean existsByDateAndThemeIdAndTimeId(String date, Long themeId, Long timeId);
 }
