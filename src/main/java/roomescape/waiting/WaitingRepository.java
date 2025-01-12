6eb54918a46fd8fd6f343adfbe3cb7dd363618d1
sinji <tobeabestns@naver.com>
2025-01-13 03:23:15 +0900

    feat(WaitingRepository): [6단계] WaitingRepository 생성

---
+++ src/main/java/roomescape/waiting/WaitingRepository.java
@@ -0,0 +1,24 @@
+package roomescape.waiting;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface WaitingRepository extends JpaRepository<Waiting, Long> {
+
+    List<Waiting> findByDateAndTimeIdAndThemeId(String date, Long timeId, Long themeId);
+
+    @Query("SELECT new roomescape.waiting.WaitingWithRank(" +
+            "    w, " +
+            "    (SELECT CAST(COUNT(w2) AS long) " +
+            "     FROM Waiting w2 " +
+            "     WHERE w2.theme = w.theme " +
+            "       AND w2.date = w.date " +
+            "       AND w2.time = w.time " +
+            "       AND w2.id < w.id)) " +
+            "FROM Waiting w " +
+            "WHERE w.member.id = :memberId")
+    List<WaitingWithRank> findWaitingWithRankByMemberId(@Param("memberId") Long memberId);
+}
