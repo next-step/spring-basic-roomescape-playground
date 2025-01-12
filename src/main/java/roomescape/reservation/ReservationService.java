7198357815199c99b7a3450e5ae3855c61731d36
sinji <tobeabestns@naver.com>
2025-01-13 03:27:00 +0900

    refactor(ReservationService): [6단계] 예약 대기 목록 조회 추가(내 예약 목록 조회 시)

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -1,25 +1,33 @@
 package roomescape.reservation;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.w3c.dom.stylesheets.LinkStyle;
 import roomescape.member.Member;
 import roomescape.theme.Theme;
 import roomescape.theme.ThemeRepository;
 import roomescape.time.Time;
 import roomescape.time.TimeRepository;
+import roomescape.waiting.WaitingRepository;
+import roomescape.waiting.WaitingResponse;
+import roomescape.waiting.WaitingWithRank;
 
+import java.lang.invoke.CallSite;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class ReservationService {
     private final ReservationRepository reservationRepository;
     private final TimeRepository timeRepository;
     private final ThemeRepository themeRepository;
+    private final WaitingRepository waitingRepository;
 
-    public ReservationService(ReservationRepository reservationRepository, TimeRepository timeRepository, ThemeRepository themeRepository) {
+    public ReservationService(ReservationRepository reservationRepository, TimeRepository timeRepository, ThemeRepository themeRepository, WaitingRepository waitingRepository) {
         this.reservationRepository = reservationRepository;
         this.timeRepository = timeRepository;
         this.themeRepository = themeRepository;
+        this.waitingRepository = waitingRepository;
     }
 
     public List<ReservationResponse> findAll() {
@@ -29,8 +37,30 @@ public class ReservationService {
     }
 
     public List<MyReservationResponse> findMyReservationsAll(Member loginMember) {
-        return reservationRepository.findByName(loginMember.getName()).stream()
-                .map(it -> new MyReservationResponse(it.getId(), it.getTheme().getName(), it.getDate(), it.getTime().getTime(), "예약"))
+
+        List<MyReservationResponse> myReservationResponses = reservationRepository.findByName(loginMember.getName()).stream()
+                .map(it -> new MyReservationResponse(
+                        it.getId(),
+                        it.getTheme().getName(),
+                        it.getDate(),
+                        it.getTime().getTime(),
+                        "예약"))
+                .toList();
+
+        List<WaitingWithRank> waitingWithRanks = waitingRepository.findWaitingWithRankByMemberId(loginMember.getId());
+
+        List<MyReservationResponse> myReservationResponsesFromWaiting = waitingWithRanks.stream()
+                .map((it -> new MyReservationResponse(
+                        it.getWaiting().getId(),
+                        it.getWaiting().getTheme().getName(),
+                        it.getWaiting().getDate(),
+                        it.getWaiting().getTime().getTime(),
+                        it.getRank() + 1 + "번째 예약대기"
+                )))
+                .toList();
+
+        return Stream.concat(myReservationResponses.stream(),
+                myReservationResponsesFromWaiting.stream())
                 .toList();
     }
 
