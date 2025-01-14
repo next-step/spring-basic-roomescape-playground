91900cab2fc08b0eca757e41a6f72631cd8662f8
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2025-01-15 01:51:25 +0900

    feat(auth): Cookie의 "token"값인 토큰에서 subject 추출

--- src/main/java/roomescape/auth/AuthController.java
+++ src/main/java/roomescape/auth/AuthController.java
@@ -1,8 +1,12 @@
 package roomescape.auth;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +29,20 @@ public class AuthController {
         response.addCookie(cookie);
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity check(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        String subject = null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                subject = jwtTokenProvider.getSubject(token);
+            }
+        }
+        Map<String, String> response = new HashMap<>();
+        response.put("subject", subject);
+        return ResponseEntity.ok().body(response);
     }
 }
