ae219d45e91c583c513a9d0c666d8c8765fce94d
mingking2 <mingee641@gmail.com>
2025-01-16 23:19:48 +0900

    feat: 토큰으로부터 사용자 정보추출 메서드 추가

--- src/main/java/roomescape/auth/TokenService.java
+++ src/main/java/roomescape/auth/TokenService.java
@@ -2,13 +2,15 @@ package roomescape.auth;
 
 import org.springframework.stereotype.Service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import roomescape.member.Member;
+import roomescape.member.MemberResponse;
 
 @Service
 public class TokenService {
-	private	final String secrectKey = "EwordIsMyBestTeacherEwordIsMyBestTeacherEwordIsMyBestTeacherEwordIsMyBestTeacherEwordIsMyBestTeacher";
+	private final String secrectKey = "EwordIsMyBestTeacherEwordIsMyBestTeacherEwordIsMyBestTeacherEwordIsMyBestTeacherEwordIsMyBestTeacher";
 
 	public String createAccessToken(Member member) {
 		return Jwts.builder()
@@ -17,5 +19,16 @@ public class TokenService {
 			.claim("role", member.getRole())
 			.signWith(Keys.hmacShaKeyFor(secrectKey.getBytes()))
 			.compact();
+	}
+
+	public MemberResponse extractMemberResponseFromToken(String token) {
+		Claims claims = Jwts.parserBuilder()
+			.setSigningKey(Keys.hmacShaKeyFor(secrectKey.getBytes()))
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
+
+		return new MemberResponse(Long.valueOf(claims.getSubject()), String.valueOf(claims.get("name")),
+			String.valueOf(claims.get("role")));
 	}
 }
