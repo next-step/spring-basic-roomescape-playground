7c7ee834ad1d4ac8b17200e14781ee81544a4959
garden-zero <oneyoung0623@gmail.com>
2025-01-18 14:58:37 +0900

    fix: Controller를 RestController로 변경

--- src/main/java/roomescape/auth/AuthController.java
+++ src/main/java/roomescape/auth/AuthController.java
@@ -5,12 +5,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class AuthController {
 
     private static final String ACCESS_TOKEN_NAME = "token";
