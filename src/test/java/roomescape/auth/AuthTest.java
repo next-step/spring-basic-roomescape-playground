43583e7eb523635cdf2b88ae6fc73de390150195
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2025-01-19 02:21:22 +0900

    test(auth): 로그인 성공 테스트 추가

--- src/test/java/roomescape/auth/AuthTest.java
+++ src/test/java/roomescape/auth/AuthTest.java
@@ -15,8 +15,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AuthTest {
     private static final String USERNAME_FIELD = "email";
     private static final String PASSWORD_FIELD = "password";
+    private static final String VALID_EMAIL = "admin@email.com";
+    private static final String VALID_PASSWORD = "password";
     private static final String INVALID_EMAIL = "email@email.com";
     private static final String INVALID_PASSWORD = "1234";
+
+    @Test
+    void 로그인_성공() {
+        Map<String, String> params = new HashMap<>();
+        params.put(USERNAME_FIELD, VALID_EMAIL);
+        params.put(PASSWORD_FIELD, VALID_PASSWORD);
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/login")
+                .then().log().all()
+                .extract();
+
+        String token = response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
+
+        assertThat(response.statusCode()).isEqualTo(200);
+        assertThat(token).isNotBlank();
+    }
 
     @Test
     void 이메일과_비밀번호가_일치하지_않는_경우_예외가_발생한다() {
