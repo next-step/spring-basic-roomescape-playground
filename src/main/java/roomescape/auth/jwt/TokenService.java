c62fcd6d0d2a92cbf3e1acdabd33ed4d68497ce0
mingking2 <mingee641@gmail.com>
2025-01-21 22:07:08 +0900

    refactor: Claims 조회 메서드 로직 분리

--- src/main/java/roomescape/auth/jwt/TokenService.java
+++ src/main/java/roomescape/auth/jwt/TokenService.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import roomescape.auth.TokenResponse;
-import roomescape.member.MemberResponse;
 
 @Service
 public class TokenService {
@@ -30,18 +32,37 @@ public class TokenService {
 			.compact());
 	}
 
-	public MemberTokenDto extractMemberResponseFromToken(String token) {
-		Claims claims = Jwts.parserBuilder()
-			.setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
-			.build()
-			.parseClaimsJws(token)
-			.getBody();
+	public boolean validToken(String token) {
+		try {
+			getClaimsFromToken(token);
+			return true;
+		} catch (ExpiredJwtException e) {
+			throw new IllegalArgumentException("만료된 토큰입니다.");
+		} catch (MalformedJwtException e) {
+			throw new IllegalArgumentException("유효하지 않은 토큰입니다.");
+		} catch (SignatureException e) {
+			throw new IllegalArgumentException("조작된 토큰입니다.");
+		}
+	}
 
-		return new MemberTokenDto(Long.valueOf(claims.getSubject()), String.valueOf(claims.get("name")),
-			String.valueOf(claims.get("role")));
+	public MemberTokenDto extractMemberResponseFromToken(String token) {
+		Claims claims = getClaimsFromToken(token);
+		return new MemberTokenDto(
+			Long.valueOf(claims.getSubject()),
+			String.valueOf(claims.get("name")),
+			String.valueOf(claims.get("role"))
+		);
 	}
 
 	private Date createExpiration(Date now, Long expiration) {
 		return new Date(now.getTime() + expiration);
+	}
+
+	private Claims getClaimsFromToken(String token) {
+		return Jwts.parserBuilder()
+			.setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
 	}
 }
