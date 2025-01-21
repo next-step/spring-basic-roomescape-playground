b6b787730b8014dc4b29f96a387a4ff19c53dfaa
mingking2 <mingee641@gmail.com>
2025-01-21 22:14:33 +0900

    test: CookieUtil 테스트코드 추가

---
+++ src/test/java/roomescape/auth/util/CookieUtilTest.java
@@ -0,0 +1,32 @@
+package roomescape.auth.util;
+
+
+
+
+
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.Test;
+
+import jakarta.servlet.http.Cookie;
+
+class CookieUtilTest {
+	private final String token = "testCookieToken";
+
+	@Test
+	void 쿠키를_생성할_수_있다() {
+		Cookie cookie = CookieUtil.createCookie(token);
+
+		assertThat(cookie.getValue()).isEqualTo(token);
+	}
+
+	@Test
+	void 쿠키로부터_토큰을_가져올_수_있다() {
+		Cookie[] cookies = new Cookie[] {CookieUtil.createCookie(token)};
+
+		String tokenFromCookie = CookieUtil.extractTokenFromCookie(cookies);
+		assertThat(tokenFromCookie).isEqualTo(token);
+	}
+}
