c0130b66f407c68e7f921ae00d227aa1825877d6
mingking2 <mingee641@gmail.com>
2025-01-21 17:09:13 +0900

    refactor: 토큰 만료기간 추가

--- src/main/java/roomescape/auth/jwt/TokenService.java
+++ src/main/java/roomescape/auth/jwt/TokenService.java
@@ -1,4 +1,5 @@
-package roomescape.auth;
+package roomescape.auth.jwt;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -6,22 +7,27 @@ import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import roomescape.member.Member;
+import roomescape.auth.TokenResponse;
 import roomescape.member.MemberResponse;
 
 @Service
 public class TokenService {
-	
-	@Value("${roomescape.auth.jwt.secret}")
+
+	@Value("${roomescape.auth.jwt.secret.key}")
 	private String secretKey;
 
-	public String createAccessToken(Member member) {
-		return Jwts.builder()
-			.setSubject(member.getId().toString())
-			.claim("name", member.getName())
-			.claim("role", member.getRole())
+	@Value("${roomescape.auth.jwt.secret.expiration}")
+	private Long expiration;
+
+	public TokenResponse createAccessToken(MemberTokenDto memberTokenDto) {
+		Date now = new Date();
+		return new TokenResponse(Jwts.builder()
+			.setSubject(memberTokenDto.id().toString())
+			.claim("name", memberTokenDto.name())
+			.claim("role", memberTokenDto.role())
+			.setExpiration(createExpiration(now, expiration))
 			.signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
-			.compact();
+			.compact());
 	}
 
 	public MemberResponse extractMemberResponseFromToken(String token) {
@@ -33,5 +39,9 @@ public class TokenService {
 
 		return new MemberResponse(Long.valueOf(claims.getSubject()), String.valueOf(claims.get("name")),
 			String.valueOf(claims.get("role")));
+	}
+
+	private Date createExpiration(Date now, Long expiration) {
+		return new Date(now.getTime() + expiration);
 	}
 }
