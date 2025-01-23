188e66edac55c639daad81b2b9121d04e0ed3dd1
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2025-01-24 04:47:32 +0900

    test(reservation): 예약 기능 테스트 추가

---
+++ src/test/java/roomescape/reservation/CreateReservations.java
@@ -0,0 +1,100 @@
+package roomescape.reservation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class CreateReservations {
+
+    @Test
+    void 로그인_상태에서_예약자명이_존재하지않는_경우_로그인_정보명으로_예약된다() {
+        //given
+        String token = createToken("admin@email.com", "password");
+
+        Map<String, String> reservationRequest = new HashMap<>();
+        reservationRequest.put("date", "2024-03-01");
+        reservationRequest.put("time", "1");
+        reservationRequest.put("theme", "1");
+
+        //when
+        ReservationResponse reservationResponse = RestAssured.given().log().all()
+                .body(reservationRequest)
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .post("/reservations")
+                .then().log().all()
+                .extract().as(ReservationResponse.class);
+
+        //then
+        assertThat(reservationResponse.getName()).isEqualTo("어드민");
+    }
+
+    @Test
+    void 로그인_상태에서_예약자명이_존재하는_경우_예약자명으로_예약된다() {
+        //given
+        String token = createToken("admin@email.com", "password");
+
+        Map<String, String> reservationRequest = new HashMap<>();
+        reservationRequest.put("date", "2024-03-01");
+        reservationRequest.put("name", "브라운");
+        reservationRequest.put("time", "1");
+        reservationRequest.put("theme", "1");
+
+        //when
+        ReservationResponse reservationResponse = RestAssured.given().log().all()
+                .body(reservationRequest)
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .post("/reservations")
+                .then().log().all()
+                .extract().as(ReservationResponse.class);
+
+        //then
+        assertThat(reservationResponse.getName()).isEqualTo("브라운");
+    }
+
+    @Test
+    void 비로그인_상태에서_예약할_경우_예약에_실패한다() {
+        //given
+        Map<String, String> reservationRequest = new HashMap<>();
+        reservationRequest.put("date", "2024-03-01");
+        reservationRequest.put("name", "브라운");
+        reservationRequest.put("time", "1");
+        reservationRequest.put("theme", "1");
+
+        //when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(reservationRequest)
+                .contentType(ContentType.JSON)
+                .post("/reservations")
+                .then().log().all()
+                .extract();
+
+        //then
+        assertThat(response.statusCode()).isEqualTo(400);
+    }
+
+    private String createToken(String email, String password) {
+        Map<String, String> params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/login")
+                .then().log().all()
+                .statusCode(200)
+                .extract();
+
+        return response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
+    }
+}
