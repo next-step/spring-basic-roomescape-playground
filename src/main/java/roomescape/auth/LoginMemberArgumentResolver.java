8f960529f02a0b84925af7daa9e7e88d788a4d8e
garden-zero <oneyoung0623@gmail.com>
2025-01-23 17:37:19 +0900

    feat: 토큰이 없는 경우 예외 발생 구현

--- src/main/java/roomescape/auth/LoginMemberArgumentResolver.java
+++ src/main/java/roomescape/auth/LoginMemberArgumentResolver.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import org.jetbrains.annotations.Nullable;
 import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -29,6 +30,11 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
         String token = extractToken(request);
+
+        if (token == null) {
+            throw new MissingRequestCookieException(TOKEN_NAME, parameter);
+        }
+
         MemberDetailResponse member = authService.loginCheckWithToken(token);
 
         return new LoginMember(member.id(), member.name(), member.email(), member.role());
