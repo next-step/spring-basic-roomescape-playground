ec3f8cff3ab148cc8c8bdd91519c425ca50c36c4
garden-zero <oneyoung0623@gmail.com>
2025-01-24 20:43:35 +0900

    feat: ReservationRequest 및 쿠키를 이용하도록 변경

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -1,12 +1,8 @@
 package roomescape.reservation;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import roomescape.auth.LoginMember;
 
 import java.net.URI;
 import java.util.List;
@@ -26,14 +22,22 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity create(@RequestBody ReservationRequest reservationRequest) {
-        if (reservationRequest.getName() == null
-                || reservationRequest.getDate() == null
-                || reservationRequest.getTheme() == null
-                || reservationRequest.getTime() == null) {
+    public ResponseEntity create(@RequestBody ReservationRequest reservationReq, LoginMember member) {
+        if (reservationReq.getDate() == null
+                || reservationReq.getTheme() == null
+                || reservationReq.getTime() == null) {
             return ResponseEntity.badRequest().build();
         }
-        ReservationResponse reservation = reservationService.save(reservationRequest);
+
+        if (reservationReq.getName() == null) {
+            reservationReq = new ReservationRequest(
+                    member.name(),
+                    reservationReq.getDate(),
+                    reservationReq.getTheme(),
+                    reservationReq.getTime());
+        }
+
+        ReservationResponse reservation = reservationService.save(reservationReq);
 
         return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).body(reservation);
     }
