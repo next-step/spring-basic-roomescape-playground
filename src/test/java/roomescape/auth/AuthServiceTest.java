c59ae6302c1e3f403c30be16eee830a9a03a4194
garden-zero <oneyoung0623@gmail.com>
2025-01-25 17:52:56 +0900

    test: Mock 없이 AuthServiceTest 수정

--- src/test/java/roomescape/auth/AuthServiceTest.java
+++ src/test/java/roomescape/auth/AuthServiceTest.java
@@ -3,118 +3,69 @@ package roomescape.auth;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import roomescape.auth.jwt.MemberTokenDto;
-import roomescape.auth.jwt.TokenService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.member.Member;
 import roomescape.member.MemberDao;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SpringBootTest
+@Transactional
 class AuthServiceTest {
 
-    @Mock
+    @Autowired
     private MemberDao memberDao;
-
-    @Mock
-    private TokenService tokenService;
-
-    @InjectMocks
+    @Autowired
     private AuthService authService;
 
-    private long id;
-    private String name;
-    private String email;
-    private String password;
-    private String role;
     private Member member;
-
-    public AuthServiceTest() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @BeforeEach
     void setUp() {
-        id = 1L;
-        name = "testName";
-        email = "email@test.com";
-        password = "testPassword";
-        member = new Member(id, name, email, role);
+        member = new Member("testName", "test@email.com", "testPassword", "user");
+        memberDao.save(member);
     }
 
     @Test
     @DisplayName("이메일 및 비밀번호로 로그인 성공")
     void loginWithEmailAndPassword_Success() {
-        //given
-        String expectedToken = "test_token";
+        String actualToken = authService.loginWithEmailAndPassword(member.getEmail(), member.getPassword());
 
-        when(memberDao.findByEmailAndPassword(email, password)).thenReturn(member);
-        when(tokenService.createToken(any(MemberTokenDto.class))).thenReturn(expectedToken);
-
-        //when
-        String actualToken = authService.loginWithEmailAndPassword(email, password);
-
-        //then
-        assertThat(actualToken).isEqualTo(expectedToken);
-        verify(memberDao, times(1)).findByEmailAndPassword(email, password);
-        verify(tokenService, times(1)).createToken(any(MemberTokenDto.class));
+        assertThat(actualToken).isNotBlank();
     }
 
     @Test
     @DisplayName("유효하지 않은 이메일과 비밀번호")
     void loginWithEmailAndPassword_Failure_InvalidCredentials() {
-        //given
-        String expectedToken = "test_token";
-
-        when(memberDao.findByEmailAndPassword(email, password)).thenReturn(null);
-
-        //when, then
-        assertThatThrownBy(() -> authService.loginWithEmailAndPassword(email, password))
+        assertThatThrownBy(() -> authService.loginWithEmailAndPassword("invalid_email", "invalid_password"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 이메일 또는 비밀번호입니다.");
-        verify(memberDao, times(1)).findByEmailAndPassword(email, password);
-        verify(tokenService, never()).createToken(any(MemberTokenDto.class));
     }
 
     @Test
     @DisplayName("토큰으로 로그인 성공")
     void loginCheckWithToken_Success() {
-        //given
-        String token = "test_token";
-        MemberTokenDto memberTokenDto = new MemberTokenDto(id, name, email, role);
+        String token = authService.loginWithEmailAndPassword(member.getEmail(), member.getPassword());
 
-        when(tokenService.checkValidToken(token)).thenReturn(true);
-        when(tokenService.getMemberClaims(token)).thenReturn(memberTokenDto);
-
-        //when
         MemberDetailResponse response = authService.loginCheckWithToken(token);
 
-        //then
         assertThat(response).isNotNull();
-        assertThat(response.id()).isEqualTo(memberTokenDto.id());
-        assertThat(response.name()).isEqualTo(memberTokenDto.name());
-        assertThat(response.email()).isEqualTo(memberTokenDto.email());
-        assertThat(response.role()).isEqualTo(memberTokenDto.role());
-        verify(tokenService, times(1)).checkValidToken(token);
-        verify(tokenService, times(1)).getMemberClaims(token);
+        assertThat(response.id()).isNotNull();
+        assertThat(response.name()).isEqualTo(member.getName());
+        assertThat(response.email()).isEqualTo(member.getEmail());
+        assertThat(response.role()).isEqualTo(member.getRole());
     }
 
     @Test
     @DisplayName("잘못된 토큰으로 로그인시 실패")
     void loginCheckWithToken_Failure_InvalidToken() {
-        //given
         String token = "invalid_token";
 
-        when(tokenService.checkValidToken(token)).thenReturn(false);
-
-        //when, then
         assertThatThrownBy(() -> authService.loginCheckWithToken(token))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("잘못된 토큰입니다.");
-        verify(tokenService, times(1)).checkValidToken(token);
-        verify(tokenService, never()).getMemberClaims(token);
     }
 }
