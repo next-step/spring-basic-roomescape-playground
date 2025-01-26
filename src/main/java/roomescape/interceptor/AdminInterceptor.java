dd4df6f195be3b081cf51ab3c51566a29680bfe2
garden-zero <oneyoung0623@gmail.com>
2025-01-26 13:25:26 +0900

    feat: 어드민 페이지 interceptor 구현

---
+++ src/main/java/roomescape/interceptor/AdminInterceptor.java
@@ -0,0 +1,46 @@
+package roomescape.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.auth.AuthService;
+import roomescape.auth.MemberDetailResponse;
+
+import java.util.Arrays;
+
+public class AdminInterceptor implements HandlerInterceptor {
+
+    private final AuthService authService;
+
+    public AdminInterceptor(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = extractToken(request);
+
+        if (token == null) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return false;
+        }
+
+        MemberDetailResponse member = authService.loginCheckWithToken(token);
+        if (!member.role().equals("ADMIN")) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+
+        return true;
+    }
+
+    private String extractToken(HttpServletRequest request) {
+        String token = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals("token"))
+                .map(cookie -> cookie.getValue())
+                .findFirst()
+                .orElse(null);
+
+        return token;
+    }
+}
