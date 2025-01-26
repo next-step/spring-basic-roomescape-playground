20e888c209e0269ac303403fa04a32f4736a02e3
garden-zero <oneyoung0623@gmail.com>
2025-01-26 14:47:54 +0900

    refactor: Configuration 어노테이션을 Component로 변경

--- src/main/java/roomescape/auth/jwt/JwtConfig.java
+++ src/main/java/roomescape/auth/jwt/JwtConfig.java
@@ -1,9 +1,9 @@
 package roomescape.auth.jwt;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
-@Configuration
+@Component
 @ConfigurationProperties("roomescape.auth.jwt")
 public class JwtConfig {
     private String secret;
