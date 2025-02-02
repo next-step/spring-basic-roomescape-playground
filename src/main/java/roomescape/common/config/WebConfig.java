294288a0103539ac8e916b1d95dd36fcb128ac9f
mingking2 <mingee641@gmail.com>
2025-02-02 23:48:54 +0900

    chore: AdminInterceptor 등록

--- src/main/java/roomescape/common/config/WebConfig.java
+++ src/main/java/roomescape/common/config/WebConfig.java
@@ -4,21 +4,33 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import roomescape.auth.resolver.LoginMemberArgumentResolver;
+import roomescape.common.interceptor.AdminInterceptor;
+import roomescape.common.resolver.LoginMemberArgumentResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+	private final AdminInterceptor adminInterceptor;
 
-	public WebConfig(LoginMemberArgumentResolver loginMemberArgumentResolver) {
+	public WebConfig(LoginMemberArgumentResolver loginMemberArgumentResolver, AdminInterceptor adminInterceptor) {
 		this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+		this.adminInterceptor = adminInterceptor;
 	}
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
 		argumentResolvers.add(loginMemberArgumentResolver);
 	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(adminInterceptor)
+			.addPathPatterns("/admin/**")
+			.order(1);
+	}
+
 }
