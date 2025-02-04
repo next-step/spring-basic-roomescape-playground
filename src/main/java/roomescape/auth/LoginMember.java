35313c4048eabcdbac3004c060d085d8159de659
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2025-02-04 10:40:03 +0900

    refactor(auth): 인증 객체 유효성 검사 추가

--- src/main/java/roomescape/auth/LoginMember.java
+++ src/main/java/roomescape/auth/LoginMember.java
@@ -5,4 +5,30 @@ public record LoginMember(
         String name,
         Role role
 ) {
+    public LoginMember(String email, String name, Role role) {
+        validateEmail(email);
+        validateName(name);
+        validateRole(role);
+        this.email = email;
+        this.name = name;
+        this.role = role;
+    }
+
+    private void validateEmail(String email) {
+        if (email == null || email.isBlank()) {
+            throw new IllegalArgumentException("Invalid email");
+        }
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Invalid name");
+        }
+    }
+
+    private void validateRole(Role role) {
+        if (role == null) {
+            throw new IllegalArgumentException("Invalid role");
+        }
+    }
 }
