aad3799cbd4c9e187e679cb47165156801644afd
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2025-02-16 12:12:15 +0900

    feat(waiting): 예약 대기 엔티티 추가

---
+++ src/main/java/roomescape/waiting/Waiting.java
@@ -0,0 +1,57 @@
+package roomescape.waiting;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import roomescape.member.Member;
+import roomescape.theme.Theme;
+import roomescape.time.Time;
+
+@Entity
+public class Waiting {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String date;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Time time;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Theme theme;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    public Waiting() {
+    }
+
+    public Waiting(String name, String date, Time time, Theme theme, Member member) {
+        this.name = name;
+        this.date = date;
+        this.time = time;
+        this.theme = theme;
+        this.member = member;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public Theme getTheme() {
+        return theme;
+    }
+}
