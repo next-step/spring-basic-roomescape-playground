05653736ce6cefc7813102db6f7fe2eb18744481
garden-zero <oneyoung0623@gmail.com>
2025-02-17 15:35:37 +0900

    style: repository 어노테이션 삭제

--- src/main/java/roomescape/member/MemberRepository.java
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -3,9 +3,7 @@ package roomescape.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member save(Member member);
 
--- src/main/java/roomescape/time/TimeRepository.java
+++ src/main/java/roomescape/time/TimeRepository.java
@@ -1,11 +1,9 @@
 package roomescape.time;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface TimeRepository extends JpaRepository<Time, Long> {
     List<Time> findAll();
 
