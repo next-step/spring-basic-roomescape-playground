9313fb67aaaf6255a27b045f2c734ac59565e5c2
iampingu99 <154869950+iampingu99@users.noreply.github.com>
2025-02-18 12:12:32 +0900

    refactor(waiting): 예약 대기 생성 테스트 리팩토링
    - 요청 객체 생성 로직 분리
    - 요청 로직 분리

--- src/test/java/roomescape/waiting/WaitingControllerTest.java
+++ src/test/java/roomescape/waiting/WaitingControllerTest.java
@@ -18,55 +18,50 @@ class WaitingControllerTest {
 
     @Test
     void 예약_대기_생성_성공() {
+        // given
         String token = createToken("admin@email.com", "password");
+        Map<String, String> request = createPostWaitingRequest();
 
-        Map<String, String> param = new HashMap<>();
-        param.put("name", "어드민");
-        param.put("date", "2024-03-01");
-        param.put("timeId", "1");
-        param.put("themeId", "1");
+        // when
+        WaitingResponse response = sendPostWaitingRequest(token, request).as(WaitingResponse.class);
 
-        WaitingResponse waitingResponse = RestAssured.given().log().all()
-                .cookie("token", token)
-                .body(param)
-                .contentType(ContentType.JSON)
-                .post("/waitings")
-                .then().log().all()
-                .statusCode(201)
-                .extract().as(WaitingResponse.class);
-
-        assertThat(waitingResponse.name()).isEqualTo("어드민");
-        assertThat(waitingResponse.date()).isEqualTo("2024-03-01");
-        assertThat(waitingResponse.time()).isEqualTo("10:00");
-        assertThat(waitingResponse.theme()).isEqualTo("테마1");
+        // then
+        assertThat(response.name()).isEqualTo("어드민");
+        assertThat(response.date()).isEqualTo("2024-03-01");
+        assertThat(response.time()).isEqualTo("10:00");
+        assertThat(response.theme()).isEqualTo("테마1");
     }
 
     @Test
     void 같은_예약_대기가_존재하는_경우_예약_대기_생성에_실패한다() {
+        // given
         String token = createToken("admin@email.com", "password");
+        Map<String, String> request = createPostWaitingRequest();
+        ExtractableResponse<Response> response = sendPostWaitingRequest(token, request);
 
+        // when
+        ExtractableResponse<Response> duplicateResponse = sendPostWaitingRequest(token, request);
+
+        // then
+        assertThat(duplicateResponse.statusCode()).isEqualTo(400);
+    }
+
+    private Map<String, String> createPostWaitingRequest() {
         Map<String, String> param = new HashMap<>();
         param.put("name", "어드민");
         param.put("date", "2024-03-01");
         param.put("timeId", "1");
         param.put("themeId", "1");
+        return param;
+    }
 
-        WaitingResponse waitingResponse = RestAssured.given().log().all()
+    private ExtractableResponse<Response> sendPostWaitingRequest(String token, Map<String, String> param) {
+        return RestAssured.given().log().all()
                 .cookie("token", token)
                 .body(param)
                 .contentType(ContentType.JSON)
                 .post("/waitings")
-                .then().log().all()
-                .statusCode(201)
-                .extract().as(WaitingResponse.class);
-
-        RestAssured.given().log().all()
-                .cookie("token", token)
-                .body(param)
-                .contentType(ContentType.JSON)
-                .post("/waitings")
-                .then().log().all()
-                .statusCode(400);
+                .then().log().all().extract();
     }
 
     private String createToken(String email, String password) {
