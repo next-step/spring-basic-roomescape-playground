7c3c9ab1d0dc2804852140aa079c8933a389e3d0
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-07 16:12:22 +0900

    Refactor 기존의 유저를 찾고 토큰을 생성하는 로직을 서비스 레이어로 이관
    컨트롤러에서 해당 작업을 처리하는 것이 적절하지 않다고 판단.

--- src/main/java/roomescape/member/MemberController.java
+++ src/main/java/roomescape/member/MemberController.java
@@ -1,12 +1,6 @@
 package roomescape.member;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 import java.net.URI;
-import java.sql.Date;
-import java.time.LocalDate;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
@@ -17,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
-    public static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
     private MemberService memberService;
 
     public MemberController(MemberService memberService) {
@@ -32,39 +25,18 @@ public class MemberController {
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@RequestBody LoginRequest request) {
-        MemberResponse memberResponse = memberService.findByEmailAndPassword(request);
+        String accessToken = memberService.authenticateAndGetToken(request);
 
-        String accessToken = createToken(memberResponse);
-
-        ResponseCookie cookie = createCookie(accessToken);
-
+        ResponseCookie cookie = createCookie(accessToken); // 생성된 토큰을 쿠키에 담기
         return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
     }
 
     @NotNull
-    private static ResponseCookie createCookie(String accessToken) {
+    private ResponseCookie createCookie(String accessToken) {
         return ResponseCookie.from("token", accessToken)
                 .path("/")
                 .httpOnly(true)
                 .build();
     }
 
-    private static String createToken(MemberResponse memberResponse) {
-        return Jwts.builder()
-                .setSubject(memberResponse.getId().toString())
-                .claim("name", memberResponse.getName())
-                .claim("email", memberResponse.getEmail())
-                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
-                .compact();
-    }
-
-    @PostMapping("/logout")
-    public ResponseEntity logout(HttpServletResponse response) {
-        Cookie cookie = new Cookie("token", "");
-        cookie.setHttpOnly(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(0);
-        response.addCookie(cookie);
-        return ResponseEntity.ok().build();
-    }
 }
--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -1,9 +1,12 @@
 package roomescape.member;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+    public static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
     private MemberDao memberDao;
 
     public MemberService(MemberDao memberDao) {
@@ -11,12 +14,27 @@ public class MemberService {
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
-        Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
+        Member member = memberDao.save(
+                new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
     }
 
-    public MemberResponse findByEmailAndPassword(LoginRequest request) {
-        Member member = memberDao.findByEmailAndPassword(request.email(), request.password());
+    public String authenticateAndGetToken(LoginRequest loginRequest) {
+        MemberResponse memberResponse = findByEmailAndPassword(loginRequest.email(), loginRequest.password());
+        return createToken(memberResponse);
+    }
+
+    private MemberResponse findByEmailAndPassword(String email, String password) {
+        Member member = memberDao.findByEmailAndPassword(email, password);
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
+    }
+
+    private String createToken(MemberResponse memberResponse) {
+        return Jwts.builder()
+                .setSubject(memberResponse.getId().toString())
+                .claim("name", memberResponse.getName())
+                .claim("email", memberResponse.getEmail())
+                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .compact();
     }
 }
