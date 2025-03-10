b93fe78c4b5b7a705767eafdb08220b77452bdf5
JMJ <ghtntkdnsk@naver.com>
2025-03-11 01:04:56 +0900

    feat: 로그인이 되어 있지 않은 경우에 대하여 예외 메시지를 설정

--- src/main/java/roomescape/login/AuthController.java
+++ src/main/java/roomescape/login/AuthController.java
@@ -34,7 +34,17 @@ public class AuthController {
     @GetMapping("/login/check")
     public ResponseEntity<MemberResponse> checkLogin(HttpServletRequest request) {
         Cookie[] cookies = request.getCookies();
+
+        if (cookies == null) {
+            throw new IllegalArgumentException("로그인을 해주세요.");
+        }
+
         String token = extractTokenFromCookie(cookies);
+
+        if (token.isEmpty()) {
+            throw new IllegalArgumentException("로그인을 해주세요.");
+        }
+
         MemberResponse memberResponse = authService.checkLogin(token);
 
         return ResponseEntity.ok(memberResponse);
