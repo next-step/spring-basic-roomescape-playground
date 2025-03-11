7bbf8658258f39e476c668966d07b44dd937ce0a
bamsanchaeg <149764776+bamsanchaeg@users.noreply.github.com>
2025-03-12 04:31:48 +0900

    feat(Test) : JwtToken 테스트 추가 및 MemberController 컨벤션 수정

--- src/test/java/roomescape/MemberControllerTest.java
+++ src/test/java/roomescape/MemberControllerTest.java
@@ -2,7 +2,6 @@ package roomescape;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
