0cdb1b51569a55e9fb8293d275cc5c9ce1f1deec
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-11 18:47:43 +0900

    Feature: 권한이 없을 경우에 401 에러를 반환하기 위해서 ExceptionController 에 에러 핸들링 handleRuntimeException 생성

--- src/main/java/roomescape/ExceptionController.java
+++ src/main/java/roomescape/ExceptionController.java
@@ -1,14 +1,23 @@
 package roomescape;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(RuntimeException.class)
+    protected ResponseEntity<String> handleRuntimeException(RuntimeException e) {
+        e.printStackTrace();
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> handleRuntimeException(Exception e) {
         e.printStackTrace();
         return ResponseEntity.badRequest().build();
     }
+
 }
