77ca381e90fa1dfff3792506d13adf9cc33d1e9c
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-11 16:42:59 +0900

    Create : HandlerMethodArgumentResolver 로직 세부사항 주석으로 순서도 정리

--- src/main/java/roomescape/config/LoginMemberArgumentResolver.java
+++ src/main/java/roomescape/config/LoginMemberArgumentResolver.java
@@ -1,5 +1,7 @@
 package roomescape.config;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -19,6 +21,15 @@ public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolve
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        // 요청에서 쿠키 가지고 온다.
+        // 쿠키에서 이름이 token 인 값을 가지고 온다.
+        // 쿠키가 있으면 jwtProvider 로 파싱한다.
+        // 파싱한 객체로 memberRepository 에서 조회해온다.
+        // 반환하려는 LoginMember 타입의 객체를 초기화해서 반환한다.
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        Cookie[] cookies = request.getCookies();
+
+
         return null;
     }
 
