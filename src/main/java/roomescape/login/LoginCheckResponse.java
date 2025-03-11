555bf30256a00a8f49a482e987791a5ab89b3477
Seong Min Kim <166149819+seongm1n@users.noreply.github.com>
2025-03-12 08:45:48 +0900

    feat: JPA 설정 변경

---
+++ src/main/java/roomescape/login/LoginCheckResponse.java
@@ -0,0 +1,4 @@
+package roomescape.login;
+
+public record LoginCheckResponse(String name) {
+}
---
+++ src/main/java/roomescape/login/LoginController.java
@@ -0,0 +1,33 @@
+package roomescape.login;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoginController {
+
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        String token = loginService.login(loginRequest.email(), loginRequest.password());
+
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<LoginCheckResponse> checkLogin(@CookieValue("token") String token) {
+        return ResponseEntity.ok(loginService.getUserInfoFromToken(token));
+    }
+}
