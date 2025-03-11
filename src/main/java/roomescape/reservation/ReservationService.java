cc147c43772422bc50b06ad869fcc6bad0e014f2
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-11 18:16:39 +0900

    Refactor: Reservation Service save 메소드 오버로딩
    클라이언트의 요청에 이름이 null 일 경우에 다른 매개변수로 저장할 수 있도록 구현

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -1,21 +1,36 @@
 package roomescape.reservation;
 
-import org.springframework.stereotype.Service;
-
 import java.util.List;
+import org.springframework.stereotype.Service;
+import roomescape.member.LoginMember;
+import roomescape.member.Member;
+import roomescape.member.MemberDao;
 
 @Service
 public class ReservationService {
-    private ReservationDao reservationDao;
 
-    public ReservationService(ReservationDao reservationDao) {
+    private final ReservationDao reservationDao;
+    private final MemberDao memberDao;
+
+    public ReservationService(ReservationDao reservationDao, MemberDao memberDao) {
         this.reservationDao = reservationDao;
+        this.memberDao = memberDao;
     }
 
     public ReservationResponse save(ReservationRequest reservationRequest) {
         Reservation reservation = reservationDao.save(reservationRequest);
 
-        return new ReservationResponse(reservation.getId(), reservationRequest.getName(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
+        return new ReservationResponse(reservation.getId(), reservationRequest.getName(),
+                reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
+    }
+
+    public ReservationResponse save(ReservationRequest reservationRequest, LoginMember loginMember) {
+        Member foundMember = memberDao.findById(loginMember.getId())
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        Reservation reservation = reservationDao.save(reservationRequest.getDate(), foundMember.getName(),
+                reservationRequest.getTheme(), reservationRequest.getTime());
+        return new ReservationResponse(reservation.getId(), reservation.getName(),
+                reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
     }
 
     public void deleteById(Long id) {
@@ -24,7 +39,8 @@ public class ReservationService {
 
     public List<ReservationResponse> findAll() {
         return reservationDao.findAll().stream()
-                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
+                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(),
+                        it.getTime().getValue()))
                 .toList();
     }
 }
