d10f3eec95ca0c02ee382fdf74329fd9e363a0e8
JMJ <ghtntkdnsk@naver.com>
2025-03-12 03:00:20 +0900

    feat: 로그인 사용자 정보 자동 주입 기능 추가

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -1,4 +1,22 @@
 package roomescape.config;
 
-public class WebConfig {
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.AuthService;
+import roomescape.auth.LoginMemberArgumentResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final AuthService authService;
+
+    public WebConfig(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver(authService));
+    }
 }
