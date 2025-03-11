d41fb8ccd7c1fd54926094b0d950245bc895b763
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-03-11 18:47:16 +0900

    Feature: Configuration 클래스에 인터셉터RoleInterceptor 등록

--- src/main/java/roomescape/config/WebMvcConfiguration.java
+++ src/main/java/roomescape/config/WebMvcConfiguration.java
@@ -3,19 +3,29 @@ package roomescape.config;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
     private final LoginMemberArgumentResolver loginMemberArgumentResolver;
+    private final RoleInterceptor roleInterceptor;
 
-    public WebMvcConfiguration(LoginMemberArgumentResolver loginMemberArgumentResolver) {
+    public WebMvcConfiguration(LoginMemberArgumentResolver loginMemberArgumentResolver,
+                               RoleInterceptor roleInterceptor) {
         this.loginMemberArgumentResolver = loginMemberArgumentResolver;
+        this.roleInterceptor = roleInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginMemberArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(roleInterceptor)
+                .addPathPatterns("/admin/**");
     }
 }
