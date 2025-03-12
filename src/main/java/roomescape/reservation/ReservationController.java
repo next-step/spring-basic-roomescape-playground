bca1aa1be75edbbd2595def4291eee2ca2af0eec
백재우 <9712jw@gmail.com>
2025-03-13 02:51:30 +0900

    Refactor: ReservationController 예약 생성하는 로직의 if 분기처리를 Optional과 스트림으로 변경

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -2,6 +2,7 @@ package roomescape.reservation;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,28 +27,18 @@ public class ReservationController {
     }
 
     @PostMapping("/reservations")
-    public ResponseEntity<ReservationResponse> create(@RequestBody ReservationRequest reservationRequest, LoginMember loginMember) {
-        /**
-         * 예약 생성 시 ReservationReqeust의 name이 없는 경우 Cookie에 담긴 정보를 활용하도록 리팩터링 합니다.
-         * ReservationReqeust에 name값이 있으면 name으로 Member를 찾고
-         * 없으면 로그인 정보를 활용해서 Member를 찾도록 수정합니다.
-         */
-        ReservationResponse reservation = null;
-        if (reservationRequest.getName() == null && loginMember != null) {
-             reservation = reservationService.save(reservationRequest, loginMember);
-            return getReservationResponseResponseEntity(reservation);
-        }
-         reservation = reservationService.save(reservationRequest);
-        return getReservationResponseResponseEntity(reservation);
-    }
+    public ResponseEntity<ReservationResponse> create(@RequestBody ReservationRequest reservationRequest,
+                                                      LoginMember loginMember) {
 
-    private static ResponseEntity<ReservationResponse> getReservationResponseResponseEntity(
-            ReservationResponse reservation) {
+        ReservationResponse reservation = Optional.ofNullable(reservationRequest.getName())
+                .map(name -> reservationService.save(reservationRequest))
+                .orElseGet(() -> reservationService.save(reservationRequest, loginMember));
+
         return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).body(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
-    public ResponseEntity delete(@PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         reservationService.deleteById(id);
         return ResponseEntity.noContent().build();
     }
