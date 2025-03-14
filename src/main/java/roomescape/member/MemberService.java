67c40a5d421042209fabf69b3f4e3b499fd9254a
seongm1n <0411tjdals34@gmail.com>
2025-03-14 14:58:28 +0900

    feat: 토큰으로 멤버를 찾는 메서드 추가

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -1,17 +1,34 @@
 package roomescape.member;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-    private MemberDao memberDao;
+    private final MemberDao memberDao;
+    private final String secretKey;
 
-    public MemberService(MemberDao memberDao) {
+    public MemberService(MemberDao memberDao, @Value("${roomescape.auth.jwt.secret}") String secretKey) {
         this.memberDao = memberDao;
+        this.secretKey = secretKey;
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
         Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
+    }
+
+    public Member findMemberByToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        Long id = claims.get("id", Long.class);
+        return memberDao.findById(id);
     }
 }
