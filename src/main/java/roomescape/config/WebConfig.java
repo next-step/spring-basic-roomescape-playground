40a67dea9ba017dfdc67f2938d47dff1378abaa6
JMJ <ghtntkdnsk@naver.com>
2025-03-17 00:12:52 +0900

    refactor: 불필요한 order 메서드 제거

--- src/main/java/roomescape/config/WebConfig.java
+++ src/main/java/roomescape/config/WebConfig.java
@@ -25,7 +25,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new AdminAuthInterceptor(authService))
-                .addPathPatterns("/admin/**")
-                .order(1);
+                .addPathPatterns("/admin/**");
     }
 }
