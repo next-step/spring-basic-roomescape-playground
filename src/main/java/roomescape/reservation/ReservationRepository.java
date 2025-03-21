af5c10e3383469d8c766db88c19d1d45419c6411
JMJ <ghtntkdnsk@naver.com>
2025-03-22 02:42:50 +0900

    feat: Reservation을 JPA 엔티티로 매핑 및 ReservationDAO를 ReservationRepository로 변경

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -1,4 +1,11 @@
 package roomescape.reservation;
 
-public interface ReservationRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import roomescape.theme.Theme;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByDateAndTheme(String date, Theme theme);
 }
--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -6,13 +6,21 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import org.springframework.web.server.ResponseStatusException;
 import roomescape.auth.domain.LoginMember;
+import roomescape.theme.Theme;
+import roomescape.theme.ThemeRepository;
+import roomescape.time.Time;
+import roomescape.time.TimeRepository;
 
 @Service
 public class ReservationService {
-    private ReservationDao reservationDao;
+    private final ReservationRepository reservationRepository;
+    private final TimeRepository timeRepository;
+    private final ThemeRepository themeRepository;
 
-    public ReservationService(ReservationDao reservationDao) {
-        this.reservationDao = reservationDao;
+    public ReservationService(ReservationRepository reservationRepository, TimeRepository timeRepository, ThemeRepository themeRepository) {
+        this.reservationRepository = reservationRepository;
+        this.timeRepository = timeRepository;
+        this.themeRepository = themeRepository;
     }
 
     public ReservationResponse save(ReservationRequest reservationRequest, LoginMember loginMember) {
@@ -20,25 +28,31 @@ public class ReservationService {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인 이름으로만 예약할 수 있습니다.");
         }
 
-        Reservation reservation = reservationDao.save(reservationRequest);
+        Time time = timeRepository.findById(reservationRequest.getTime())
+                .orElseThrow(() -> new IllegalArgumentException("없는 시간입니다."));
+        Theme theme = themeRepository.findById(reservationRequest.getTheme())
+                .orElseThrow(() -> new IllegalArgumentException("없는 테마입니다."));
 
-        return new ReservationResponse(reservation.getId(), reservationRequest.getName(),
-                reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
+        Reservation reservation = new Reservation(reservationRequest.getName(), reservationRequest.getDate(), time, theme);
+        Reservation savedReservation = reservationRepository.save(reservation);
+
+        return new ReservationResponse(savedReservation.getId(), savedReservation.getName(),
+                savedReservation.getTheme().getName(), savedReservation.getDate(), savedReservation.getTime().getValue());
     }
 
     public void deleteById(Long id, LoginMember loginMember) {
-        Reservation reservation = reservationDao.findById(id)
+        Reservation reservation = reservationRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "예약을 찾을 수 없습니다."));
 
         if (loginMember.isNotAdmin() && loginMember.notHaveName(reservation.getName())) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "자신의 예약만 삭제할 수 있습니다.");
         }
 
-        reservationDao.deleteById(id);
+        reservationRepository.deleteById(id);
     }
 
     public List<ReservationResponse> findAll() {
-        return reservationDao.findAll().stream()
+        return reservationRepository.findAll().stream()
                 .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
                 .toList();
     }
