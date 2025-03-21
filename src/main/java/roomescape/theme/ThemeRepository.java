1787a029484322275a04e244eb8fb70e97fedcdc
JMJ <ghtntkdnsk@naver.com>
2025-03-22 02:42:03 +0900

    feat: Theme을 JPA 엔티티로 매핑 및 ThemeDAO를 ThemeRepository로 변경

--- src/main/java/roomescape/theme/ThemeRepository.java
+++ src/main/java/roomescape/theme/ThemeRepository.java
@@ -1,4 +1,8 @@
 package roomescape.theme;
 
-public interface ThemeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
 }
