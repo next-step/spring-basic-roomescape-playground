d5acaa5941ef546ccb4a3431bc5a2709d8986ff7
백재우 <9712jw@gmail.com>
2025-03-23 13:20:12 +0900

    Feature: JWT 인증 예외 관련 메소드 ExceptionController에 추가

--- src/main/java/roomescape/ExceptionController.java
+++ src/main/java/roomescape/ExceptionController.java
@@ -1,5 +1,6 @@
 package roomescape;
 
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(SecurityException.class)
+    protected ResponseEntity<String> handleSecurityException(SecurityException exception) {
+        exception.printStackTrace();
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    protected ResponseEntity<String> handleNoSuchElementException(NoSuchElementException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    protected ResponseEntity<String> handleIllegalException(IllegalArgumentException exception) {
+        exception.printStackTrace();
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
 
     @ExceptionHandler(RuntimeException.class)
     protected ResponseEntity<String> handleRuntimeException(RuntimeException e) {
