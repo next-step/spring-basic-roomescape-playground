32d92f92f9f1c0127963c621b3a91be6b46301e6
heehunjung <heehunjung3658@gmail.com>
2025-03-24 02:02:12 +0900

    refactor: ReservationRepository 생성 및 Dao 삭제

---
+++ src/main/java/roomescape/reservationTime/ReservationTimeRepository.java
@@ -0,0 +1,6 @@
+package roomescape.reservationTime;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationTimeRepository extends JpaRepository<ReservationTime, Long> {
+}
