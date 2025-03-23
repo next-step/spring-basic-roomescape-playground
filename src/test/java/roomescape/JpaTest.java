43ed6e14adbae4a7991888e61251db090c310735
heehunjung <heehunjung3658@gmail.com>
2025-03-24 00:32:23 +0900

    refactor: jap 관련 설정 추가

---
+++ src/test/java/roomescape/JpaTest.java
@@ -0,0 +1,26 @@
+package roomescape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class JpaTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private TimeRepository timeRepository;
+
+    @Test
+    void 사단계() {
+        Time time = new Time("10:00");
+        entityManager.persist(time);
+        entityManager.flush();
+
+        Time persistTime = timeRepository.findById(time.getId()).orElse(null);
+
+        assertThat(persistTime.getTime()).isEqualTo(time.getTime());
+    }
+}
