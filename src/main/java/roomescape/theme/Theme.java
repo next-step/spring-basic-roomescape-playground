8b3547b707dffb040e30f71b243a51a12c362c4d
JMJ <ghtntkdnsk@naver.com>
2025-03-24 20:08:27 +0900

    refactor: 불필요한 연관관계 제거

--- src/main/java/roomescape/theme/Theme.java
+++ src/main/java/roomescape/theme/Theme.java
@@ -16,9 +16,6 @@ public class Theme {
     private String name;
     private String description;
 
-    @OneToMany(mappedBy = "theme")
-    private List<Reservation> reservations;
-
     protected Theme() {
     }
 
