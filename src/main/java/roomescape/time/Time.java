57cf114ce61442dc904646d946f59e39ba153dd7
seongm1n <0411tjdals34@gmail.com>
2025-03-25 19:48:15 +0900

    feat: TimeDao -> TimeRepository

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,20 +1,23 @@
 package roomescape.time;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Time {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String value;
+
+    protected Time() {
+    }
 
     public Time(Long id, String value) {
         this.id = id;
         this.value = value;
-    }
-
-    public Time(String value) {
-        this.value = value;
-    }
-
-    public Time() {
-
     }
 
     public Long getId() {
