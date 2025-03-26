d9d8f418baddc0845cf489287f89dddfb8800c8d
seongm1n <0411tjdals34@gmail.com>
2025-03-26 23:10:03 +0900

    refector: DTO record로 변환

--- src/main/java/roomescape/reservation/ReservationResponse.java
+++ src/main/java/roomescape/reservation/ReservationResponse.java
@@ -3,38 +3,5 @@ package roomescape.reservation;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
-public class ReservationResponse {
-    private Long id;
-    private String name;
-    private String theme;
-    private LocalDate date;
-    private LocalTime time;
-
-    public ReservationResponse(Long id, String name, String theme, LocalDate date, LocalTime time) {
-        this.id = id;
-        this.name = name;
-        this.theme = theme;
-        this.date = date;
-        this.time = time;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getTheme() {
-        return theme;
-    }
-
-    public LocalDate getDate() {
-        return date;
-    }
-
-    public LocalTime getTime() {
-        return time;
-    }
+public record ReservationResponse(Long id, String name, String theme, LocalDate date, LocalTime time) {
 }
