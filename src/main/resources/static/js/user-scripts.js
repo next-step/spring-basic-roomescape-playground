c35ebf6bec30d8c9faeb31acd05887e63bf22df0
JMJ <ghtntkdnsk@naver.com>
2025-03-29 13:33:28 +0900

    refactor: 로그아웃시 홈으로 리다이렉트

--- src/main/resources/static/js/user-scripts.js
+++ src/main/resources/static/js/user-scripts.js
@@ -10,7 +10,7 @@ document.getElementById('logout-btn').addEventListener('click', function () {
       .then(response => {
         if(response.ok) {
           // 로그아웃 성공, 페이지 새로고침 또는 리다이렉트
-          window.location.reload();
+            window.location.href = '/';
         } else {
           // 로그아웃 실패 처리
           console.error('Logout failed');
