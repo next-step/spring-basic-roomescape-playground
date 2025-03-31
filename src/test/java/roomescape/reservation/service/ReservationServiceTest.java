e49ebb630efd0582b57340a6b68398872ade8f8c
Jiihyun <cwlgusc@gmail.com>
2025-03-31 15:36:37 +0900

    test: ReservationService 테스트 추가

--- src/test/java/roomescape/reservation/service/ReservationServiceTest.java
+++ src/test/java/roomescape/reservation/service/ReservationServiceTest.java
@@ -3,21 +3,29 @@ package roomescape.reservation.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.auth.dto.LoginMember;
 import roomescape.exception.BadRequestException;
 import roomescape.exception.ExceptionMessage;
 import roomescape.member.domain.Member;
 import roomescape.member.domain.Role;
 import roomescape.member.repository.MemberRepository;
+import roomescape.reservation.domain.Reservation;
 import roomescape.reservation.dto.request.ReservationRequest;
+import roomescape.reservation.dto.response.MyReservationResponse;
 import roomescape.reservation.dto.response.ReservationResponse;
+import roomescape.reservation.repository.ReservationRepository;
 import roomescape.theme.domain.Theme;
 import roomescape.theme.repository.ThemeRepository;
 import roomescape.time.domain.Time;
 import roomescape.time.repository.TimeRepository;
+import roomescape.waiting.domain.Status;
+import roomescape.waiting.domain.Waiting;
+import roomescape.waiting.repository.WaitingRepository;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -30,6 +38,9 @@ class ReservationServiceTest {
     private ReservationService reservationService;
 
     @Autowired
+    private ReservationRepository reservationRepository;
+
+    @Autowired
     private MemberRepository memberRepository;
 
     @Autowired
@@ -38,20 +49,22 @@ class ReservationServiceTest {
     @Autowired
     private TimeRepository timeRepository;
 
+    @Autowired
+    private WaitingRepository waitingRepository;
 
     @Test
     void 관리자가_직접_고객의_예약을_생성할_수_있다() {
         // given
         Theme theme = createTheme();
         Time time = createTime();
-        Member member = createMember("멤버", "member@email.com", Role.USER);
+        LoginMember loginMember = createLoginMember("멤버", "member@email.com");
 
-        ReservationRequest request = new ReservationRequest(member.getName(), LocalDate.of(2025, 3, 15), theme.getId(), time.getId());
+        ReservationRequest request = new ReservationRequest(loginMember.name(), LocalDate.of(2025, 3, 15), theme.getId(), time.getId());
         // when
         ReservationResponse response = reservationService.save(request, null);
         // then
         assertAll(
-                () -> assertThat(response.getName()).isEqualTo(member.getName()),
+                () -> assertThat(response.getName()).isEqualTo(loginMember.name()),
                 () -> assertThat(response.getTheme()).isEqualTo(theme.getName()),
                 () -> assertThat(response.getDate()).isEqualTo(request.getDate()),
                 () -> assertThat(response.getTime()).isEqualTo(time.getValue())
@@ -63,10 +76,9 @@ class ReservationServiceTest {
         // given
         Theme theme = createTheme();
         Time time = createTime();
-        Member member = createMember("멤버", "member@email.com", Role.USER);
+        LoginMember loginMember = createLoginMember("멤버", "member@email.com");
 
         ReservationRequest request = new ReservationRequest(null, LocalDate.of(2025, 3, 15), theme.getId(), time.getId());
-        LoginMember loginMember = new LoginMember(member.getId(), member.getName(), member.getEmail(), member.getRole());
         // when
         ReservationResponse response = reservationService.save(request, loginMember);
         // then
@@ -83,19 +95,88 @@ class ReservationServiceTest {
         // given
         Theme theme = createTheme();
         Time time = createTime();
-        Member member1 = createMember("멤버1", "member1@email.com", Role.USER);
-        Member member2 = createMember("멤버2", "member2@email.com", Role.USER);
+        LoginMember loginMember1 = createLoginMember("멤버1", "member1@email.com");
+        LoginMember loginMember2 = createLoginMember("멤버2", "member2@email.com");
 
         ReservationRequest request1 = new ReservationRequest(null, LocalDate.of(2025, 3, 15), theme.getId(), time.getId());
-        LoginMember loginMember1 = new LoginMember(member1.getId(), member1.getName(), member1.getEmail(), member1.getRole());
         reservationService.save(request1, loginMember1);
 
         ReservationRequest request2 = new ReservationRequest(null, LocalDate.of(2025, 3, 15), theme.getId(), time.getId());
-        LoginMember loginMember2 = new LoginMember(member2.getId(), member2.getName(), member2.getEmail(), member2.getRole());
         // when & then
         assertThatThrownBy(() -> reservationService.save(request2, loginMember2))
                 .isInstanceOf(BadRequestException.class)
                 .hasMessage(ExceptionMessage.RESERVATION_ALREADY_EXISTS.getMessage());
+    }
+
+    @Test
+    @Transactional(readOnly = true)
+    void 예약_및_대기_목록을_조회한다() {
+        // given
+        LoginMember loginMember = createLoginMember("멤버", "member@email.com");
+
+        Time time = createTime();
+        Theme theme = createTheme();
+
+        LocalDate date1 = LocalDate.of(2025, 3, 30);
+        Reservation reservation = new Reservation(loginMember.id(), loginMember.name(), date1, time, theme);
+        reservationRepository.save(reservation);
+
+        LocalDate date2 = LocalDate.of(2025, 3, 31);
+        Waiting waiting = new Waiting(loginMember.id(), loginMember.name(), date2, time, theme);
+        waitingRepository.save(waiting);
+        // when
+        List<MyReservationResponse> responses = reservationService.findMyReservations(loginMember);
+        // then
+        assertAll(
+                () -> assertThat(responses).hasSize(2),
+                () -> assertThat(responses).anyMatch(response -> response.id() == (reservation.getId())),
+                () -> assertThat(responses).anyMatch(response -> response.id() == (waiting.getId()))
+        );
+    }
+
+    @Test
+    void 예약을_취소할_수_있다() {
+        // given
+        LoginMember loginMember = createLoginMember("멤버", "member@email.com");
+
+        Time time = createTime();
+        Theme theme = createTheme();
+
+        LocalDate date = LocalDate.of(2025, 3, 30);
+        Reservation reservation = new Reservation(loginMember.id(), loginMember.name(), date, time, theme);
+        reservationRepository.save(reservation);
+
+        // when
+        reservationService.delete(reservation.getId());
+        // then
+        List<MyReservationResponse> responses = reservationService.findMyReservations(loginMember);
+        assertThat(responses).isEmpty();
+    }
+
+    @Test
+    @Transactional
+    void 예약을_취소하면_첫번째_대기멤버가_예약에_성공한다() {
+        // given
+        LoginMember loginMember1 = createLoginMember("멤버1", "member1@email.com");
+        LoginMember loginMember2 = createLoginMember("멤버2", "member2@email.com");
+
+        Time time = createTime();
+        Theme theme = createTheme();
+        LocalDate date = LocalDate.of(2025, 3, 30);
+
+        Reservation reservation = new Reservation(loginMember1.id(), loginMember1.name(), date, time, theme);
+        reservationRepository.save(reservation);
+
+        Waiting waiting = new Waiting(loginMember2.id(), loginMember2.name(), date, time, theme);
+        waitingRepository.save(waiting);
+        // when
+        reservationService.delete(reservation.getId());
+        // then
+        List<MyReservationResponse> responses = reservationService.findMyReservations(loginMember2);
+        assertAll(
+                () -> assertThat(responses).hasSize(1),
+                () -> assertThat(responses).anyMatch(response -> Status.CONFIRMED.getDescription().equals(response.status()))
+                );
     }
 
     private Theme createTheme() {
@@ -108,8 +189,9 @@ class ReservationServiceTest {
         return timeRepository.save(time);
     }
 
-    private Member createMember(String name, String email, Role role) {
-        Member member = new Member(name, email, "password", role);
-        return memberRepository.save(member);
+    private LoginMember createLoginMember(String name, String email) {
+        Member member = new Member(name, email, "password", Role.USER);
+        Member savedMember = memberRepository.save(member);
+        return new LoginMember(savedMember.getId(), savedMember.getName(), savedMember.getEmail(), savedMember.getRole());
     }
 }
