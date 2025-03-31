ecd95bc12d2f571afbc8cbc5f4221de8f31fb4a6
Jiihyun <cwlgusc@gmail.com>
2025-03-31 18:33:39 +0900

    feat: WaitingRequest 데이터 유효성 검증

--- src/main/java/roomescape/waiting/dto/request/WaitingRequest.java
+++ src/main/java/roomescape/waiting/dto/request/WaitingRequest.java
@@ -1,5 +1,7 @@
 package roomescape.waiting.dto.request;
 
+import roomescape.exception.BadRequestException;
+import roomescape.exception.ExceptionMessage;
 import roomescape.theme.domain.Theme;
 import roomescape.time.domain.Time;
 import roomescape.waiting.domain.Waiting;
@@ -11,6 +13,30 @@ public record WaitingRequest(
         long time,
         long theme
 ) {
+    public WaitingRequest {
+        validateDate(date);
+        validateTheme(theme);
+        validateTime(time);
+    }
+
+    private void validateDate(final LocalDate date) {
+        if (date == null) {
+            throw new BadRequestException(ExceptionMessage.INVALID_DATE.getMessage());
+        }
+    }
+
+    private void validateTheme(final Long themeId) {
+        if (themeId == null) {
+            throw new BadRequestException(ExceptionMessage.INVALID_THEME.getMessage());
+        }
+    }
+
+    private void validateTime(final Long timeId) {
+        if (timeId == null) {
+            throw new BadRequestException(ExceptionMessage.INVALID_TIME.getMessage());
+        }
+    }
+
     public Waiting toWaiting(long memberId, String name, Time time, Theme theme) {
         return new Waiting(
                 memberId,
