82b0d5ee12d500927dcf2d50e596ead7db00605d
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-12 09:44:21 +0900

    Refactor: Reservation 사용하지 않는 생성자 삭제

--- src/main/java/roomescape/reservation/Reservation.java
+++ src/main/java/roomescape/reservation/Reservation.java
@@ -43,28 +43,12 @@ public class Reservation {
 
     }
 
-    public Reservation(Long id, String name, String date, Time time, Theme theme, Member member) {
-        this.id = id;
-        this.name = name;
-        this.date = date;
-        this.time = time;
-        this.theme = theme;
-        this.member = member;
-    }
-
     public Reservation(String name, String date, Time time, Theme theme, Member member) {
         this.name = name;
         this.date = date;
         this.time = time;
         this.theme = theme;
         this.member = member;
-    }
-
-    public Reservation(String name, String date, Time time, Theme theme) {
-        this.name = name;
-        this.date = date;
-        this.time = time;
-        this.theme = theme;
     }
 
     public Long getId() {
