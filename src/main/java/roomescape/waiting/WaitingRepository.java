59b42b943409eb0c7a92a6c72f5baf689f47f9cc
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-12 13:09:17 +0900

    Refactor: Waiting Repository 의 쿼리 MultiString 변경

--- src/main/java/roomescape/waiting/WaitingRepository.java
+++ src/main/java/roomescape/waiting/WaitingRepository.java
@@ -3,23 +3,42 @@ package roomescape.waiting;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import roomescape.theme.Theme;
 import roomescape.time.Time;
 
 public interface WaitingRepository extends JpaRepository<Waiting, Long> {
 
-    Long countByThemeAndDateAndTimeAndIdLessThanEqual(Theme theme, String date, Time time, Long id);
+    @Query("""
+            SELECT COUNT(w)
+              FROM Waiting w
+             WHERE w.theme = :theme
+              AND w.date = :date
+              AND w.time = :time
+              AND w.id <= :id
+            """)
+    Long countByConditions(
+            @Param("theme") Theme theme,
+            @Param("date") String date,
+            @Param("time") Time time,
+            @Param("id") Long id
+    );
 
-    @Query("SELECT new roomescape.waiting.WaitingWithRank(" +
-            "    w, " +
-            "    (SELECT COUNT(w2) " +
-            "     FROM Waiting w2 " +
-            "     WHERE w2.theme = w.theme " +
-            "       AND w2.date = w.date " +
-            "       AND w2.time = w.time " +
-            "       AND w2.id < w.id)) " +
-            "FROM Waiting w " +
-            "WHERE w.member.id = :memberId")
+    @Query("""
+            SELECT new roomescape.waiting.WaitingWithRank(
+                w,
+                (
+                    SELECT COUNT(w2)
+                     FROM Waiting w2
+                     WHERE w2.theme = w.theme
+                       AND w2.date = w.date
+                       AND w2.time = w.time
+                       AND w2.id < w.id
+                       )
+               )
+            FROM Waiting w
+            WHERE w.member.id = :memberId
+            """)
     List<WaitingWithRank> findWaitingsWithRankByMemberId(Long memberId);
 
 }
