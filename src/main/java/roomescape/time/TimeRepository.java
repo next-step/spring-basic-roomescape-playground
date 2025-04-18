0f9ce5dadc455487f1a1ba9a07210c7f0e2a8c22
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2025-04-18 14:59:37 +0900

    Refactor: TimeRepository default 메소드로 예외처리 포함하도록 변경

--- src/main/java/roomescape/time/TimeRepository.java
+++ src/main/java/roomescape/time/TimeRepository.java
@@ -6,4 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface TimeRepository extends JpaRepository<Time, Long> {
 
     List<Time> findAllByDeletedFalse();
+
+    default Time getById(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 시간이 존재하지 않습니다."));
+    }
 }
