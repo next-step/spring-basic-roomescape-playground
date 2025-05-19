ddfa18f7a1b1a3c2f2410760ad6135bfcbc7056a
Seokju Hong <iamseokjuhong@gmail.com>
2025-05-19 18:04:17 +0900

    4단계 - JPA 전환

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -2,32 +2,57 @@ package roomescape.reservation;
 
 import org.springframework.stereotype.Service;
 import roomescape.member.LoginMember;
+import roomescape.theme.Theme;
+import roomescape.theme.ThemeRepository;
+import roomescape.time.Time;
+import roomescape.time.TimeRepository;
 
 import java.util.List;
 
 @Service
 public class ReservationService {
-    private ReservationDao reservationDao;
+    private final ReservationRepository reservationRepository;
+    private final ThemeRepository themeRepository;
+    private final TimeRepository timeRepository;
 
-    public ReservationService(ReservationDao reservationDao) {
-        this.reservationDao = reservationDao;
+    public ReservationService(ReservationRepository reservationRepository, ThemeRepository themeRepository, TimeRepository timeRepository) {
+        this.reservationRepository = reservationRepository;
+        this.themeRepository = themeRepository;
+        this.timeRepository = timeRepository;
     }
 
     public ReservationResponse save(ReservationRequest reservationRequest, LoginMember loginMember) {
         reservationRequest = reservationRequest.withUserName(loginMember.getName());
 
-        Reservation reservation = reservationDao.save(reservationRequest);
+        Time time = timeRepository.findById(reservationRequest.getTime())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid time ID"));
+        Theme theme = themeRepository.findById(reservationRequest.getTheme())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid theme ID"));
 
-        return new ReservationResponse(reservation.getId(), reservationRequest.getName(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
+        Reservation reservation = new Reservation(
+                reservationRequest.getName(),
+                reservationRequest.getDate(),
+                time,
+                theme
+        );
+        reservationRepository.save(reservation);
+
+        return new ReservationResponse(
+                reservation.getId(), 
+                reservationRequest.getName(), 
+                theme.getName(),
+                reservation.getDate(), 
+                time.getTime()
+        );
     }
 
     public void deleteById(Long id) {
-        reservationDao.deleteById(id);
+        reservationRepository.deleteById(id);
     }
 
     public List<ReservationResponse> findAll() {
-        return reservationDao.findAll().stream()
-                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
+        return reservationRepository.findAll().stream()
+                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getTime()))
                 .toList();
     }
 }
