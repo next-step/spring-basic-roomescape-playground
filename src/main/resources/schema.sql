6fae5b93aefdbaa28720547ffa12997d5ba48e48
JoungMinJu <alswn9938@gmail.com>
2025-06-04 00:49:14 +0900

    step6 : 의도한대로 동작하도록 scheme.sql 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,23 +4,23 @@ INSERT INTO member (name, email, password, role)
 VALUES ('어드민', 'admin@email.com', 'password', 'ADMIN'),
        ('브라운', 'brown@email.com', 'password', 'USER');
 
-INSERT INTO theme (name, description)
-VALUES ('테마1', '테마1입니다.'),
-       ('테마2', '테마2입니다.'),
-       ('테마3', '테마3입니다.');
+INSERT INTO theme (name, description, deleted)
+VALUES ('테마1', '테마1입니다.', 0),
+       ('테마2', '테마2입니다.', 0),
+       ('테마3', '테마3입니다.', 0);
 
-INSERT INTO "TIME" (time_value)
-VALUES ('10:00'),
-       ('12:00'),
-       ('14:00'),
-       ('16:00'),
-       ('18:00'),
-       ('20:00');
+INSERT INTO "TIME" (time_value, deleted)
+VALUES ('10:00', 0),
+       ('12:00', 0),
+       ('14:00', 0),
+       ('16:00', 0),
+       ('18:00', 0),
+       ('20:00', 0);
 
 INSERT INTO reservation (member_id, name, date, time_id, theme_id)
 VALUES (1, '', '2024-03-01', 1, 1),
        (1, '', '2024-03-01', 2, 2),
        (1, '', '2024-03-01', 3, 3);
 
-INSERT INTO reservation (name, date, time_id, theme_id)
-VALUES ('브라운', '2024-03-01', 1, 2);
+INSERT INTO reservation (member_id, name, date, time_id, theme_id)
+VALUES (2, '브라운', '2024-03-01', 1, 2);
