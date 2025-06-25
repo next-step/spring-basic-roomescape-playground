257f9ead4fb2fe7325f262e9062cd4f51d6a02b1
gjtjrl303 <gjtjrl303@naver.com>
2025-06-25 18:46:43 +0900

    feat: 토큰을 이용해 사용자 정보를 담는 ArgumentResolver 구현

---
+++ src/main/java/roomescape/auth/LoginMemberArgumentResolver.java
@@ -0,0 +1,32 @@
+package roomescape.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.member.MemberResponse;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final AuthService authService;
+
+    public LoginMemberArgumentResolver(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getGenericParameterType().equals(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        MemberResponse response = authService.findMemberByToken(request);
+        return new LoginMember(response.getId(), response.getName(), response.getEmail());
+    }
+}
