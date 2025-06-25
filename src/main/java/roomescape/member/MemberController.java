49060f1fdc5abfc41b31d29d50a278001ee2689a
gjtjrl303 <gjtjrl303@naver.com>
2025-06-25 18:47:48 +0900

    feat: 로그인, 로그인 체크 api 구현

--- src/main/java/roomescape/member/MemberController.java
+++ src/main/java/roomescape/member/MemberController.java
@@ -8,21 +8,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.auth.AuthService;
+import roomescape.auth.LoginRequest;
+import roomescape.auth.TokenResponse;
 
 import java.net.URI;
 
 @RestController
 public class MemberController {
+    private final AuthService authService;
     private MemberService memberService;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, AuthService authService) {
         this.memberService = memberService;
+        this.authService = authService;
     }
 
     @PostMapping("/members")
     public ResponseEntity createMember(@RequestBody MemberRequest memberRequest) {
         MemberResponse member = memberService.createMember(memberRequest);
         return ResponseEntity.created(URI.create("/members/" + member.getId())).body(member);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        TokenResponse tokenResponse = authService.login(loginRequest);
+
+        Cookie cookie = new Cookie("token", tokenResponse.accessToken());
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<MemberResponse> checkLogin(HttpServletRequest request) {
+        MemberResponse response = authService.findMemberByToken(request);
+        return ResponseEntity.ok().body(response);
     }
 
     @PostMapping("/logout")
