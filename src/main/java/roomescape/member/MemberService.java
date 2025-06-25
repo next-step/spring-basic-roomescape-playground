988ad5fe9267b1174f0bb8e1be21332493789a4e
gjtjrl303 <gjtjrl303@naver.com>
2025-06-25 18:54:55 +0900

    feat: 회원가입된 사용자가 있는지 검증하는 authenticate 메서드 구현

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -1,6 +1,8 @@
 package roomescape.member;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import roomescape.auth.LoginRequest;
 
 @Service
 public class MemberService {
@@ -12,6 +14,20 @@ public class MemberService {
 
     public MemberResponse createMember(MemberRequest memberRequest) {
         Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
-        return new MemberResponse(member.getId(), member.getName(), member.getEmail());
+        return new MemberResponse(member.getId(), member.getName(), member.getEmail(), member.getRole());
     }
+
+    public Member authenticate(LoginRequest loginRequest) {
+        try {
+            return memberDao.findByEmailAndPassword(loginRequest.email(), loginRequest.password());
+        } catch (EmptyResultDataAccessException e) {
+            throw new IllegalArgumentException("이메일 또는 비밀번호가 잘못되었습니다");
+        }
+    }
+
+    public MemberResponse findById(Long memberId) {
+        Member findMember = memberDao.findById(memberId);
+        return new MemberResponse(findMember.getId(), findMember.getName(), findMember.getEmail(), findMember.getRole());
+    }
+
 }
