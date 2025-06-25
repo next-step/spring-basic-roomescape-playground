1d95db560267128b997542e4d26c017dc57c5910
gjtjrl303 <gjtjrl303@naver.com>
2025-06-25 18:45:19 +0900

    feat: JwtProvider 구현

---
+++ src/main/java/roomescape/auth/JwtProvider.java
@@ -0,0 +1,31 @@
+package roomescape.auth;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import roomescape.member.Member;
+
+@Component
+public class JwtProvider {
+    @Value("${roomescape.auth.jwt.secret}")
+    private String secretKey;
+
+    public Long extractMemberId(String token) {
+        return Long.valueOf(Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody().getSubject());
+    }
+
+    public String createToken(Member member) {
+        String accessToken = Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+        return accessToken;
+    }
+}
