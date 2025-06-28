f4a07d0081b437d0f7f723cfd8864d2076ecb394
Jiwoo <ziuoo0424@gmail.com>
2025-06-28 19:39:29 +0900

    feat: JWT 토큰 관리 클래스

---
+++ src/main/java/roomescape/member/JwtTokenProvider.java
@@ -0,0 +1,31 @@
+package roomescape.member;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+
+@Component
+public class JwtTokenProvider {
+
+    private final SecretKey secretKey = Keys.hmacShaKeyFor("Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=".getBytes());
+
+    public String createToken(Member member) {
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String getSubject(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
