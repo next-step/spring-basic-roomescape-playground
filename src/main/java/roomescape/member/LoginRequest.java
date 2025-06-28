41948ad54f3c2dfc5b07ec47d1a3dcfada436b68
Jiwoo <ziuoo0424@gmail.com>
2025-06-28 19:38:11 +0900

    feat: 로그인 요청 DTO 생성

---
+++ src/main/java/roomescape/member/LoginRequest.java
@@ -0,0 +1,16 @@
+package roomescape.member;
+
+public class LoginRequest {
+
+    private String email;
+    private String password;
+
+    // Getter 추가
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
