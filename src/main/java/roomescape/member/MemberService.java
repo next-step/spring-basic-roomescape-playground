68e3284f16e9498a9909a49c6893f934aeb838fa
Jiwoo <ziuoo0424@gmail.com>
2025-06-28 19:40:08 +0900

    feat: 로그인 로직 구현

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -4,14 +4,32 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-    private MemberDao memberDao;
 
-    public MemberService(MemberDao memberDao) {
+    private final MemberDao memberDao;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public MemberService(MemberDao memberDao, JwtTokenProvider jwtTokenProvider) {
         this.memberDao = memberDao;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
         Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
     }
+
+    // 로그인 로직
+    public String login(LoginRequest request) {
+        Member member = memberDao.findByEmailAndPassword(request.getEmail(), request.getPassword())
+                .orElseThrow(() -> new IllegalArgumentException("이메일 또는 비밀번호가 일치하지 않습니다."));
+
+        return jwtTokenProvider.createToken(member);
+    }
+
+    // ID로 멤버를 찾는 로직
+    public Member findById(Long id) {
+        return memberDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 사용자를 찾을 수 없습니다."));
+    }
+
 }
