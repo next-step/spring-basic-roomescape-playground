0b223ebb75592e6f0f5b94d91e9b09d1eae08785
gjtjrl303 <gjtjrl303@naver.com>
2025-06-30 09:59:46 +0900

    feat: AuthService에서 쿠키 추출 로직 분리

--- src/main/java/roomescape/auth/AuthService.java
+++ src/main/java/roomescape/auth/AuthService.java
@@ -1,8 +1,7 @@
 package roomescape.auth;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
+import roomescape.exception.RoomEscapeException;
 import roomescape.member.Member;
 import roomescape.member.MemberResponse;
 import roomescape.member.MemberService;
@@ -19,27 +18,13 @@ public class AuthService {
     }
 
     public TokenResponse login(LoginRequest loginRequest) {
-        Member member = memberService.authenticate(loginRequest);
+        Member member = memberService.authenticate(loginRequest.email(), loginRequest.password());
         String token = jwtProvider.createToken(member);
         return new TokenResponse(token);
     }
 
-    public MemberResponse findMemberByToken(HttpServletRequest request) {
-        Cookie[] cookies = request.getCookies();
-        String token = extractTokenFromCookie(cookies);
+    public MemberResponse findMemberByToken(String token) {
         Long memberId = jwtProvider.extractMemberId(token);
         return memberService.findById(memberId);
-    }
-
-    private String extractTokenFromCookie(Cookie[] cookies) {
-        if (cookies == null) {
-            throw new IllegalArgumentException("쿠키가 존재하지 않습니다");
-        }
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                return cookie.getValue();
-            }
-        }
-        throw new IllegalArgumentException("토큰 쿠키가 존재하지 않습니다.");
     }
 }
