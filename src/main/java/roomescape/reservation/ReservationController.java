041ff154ccf457a9236021a554e01ce40aa8b22c
gjtjrl303 <gjtjrl303@naver.com>
2025-07-02 23:27:55 +0900

    feat: /reservation-mine api 구현

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -8,17 +8,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import roomescape.auth.LoginMember;
+import roomescape.waiting.WaitingResponse;
+import roomescape.waiting.WaitingService;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Stream;
 
 @RestController
 public class ReservationController {
 
     private final ReservationService reservationService;
+    private final WaitingService waitingService;
 
-    public ReservationController(ReservationService reservationService) {
+    public ReservationController(ReservationService reservationService, WaitingService waitingService) {
         this.reservationService = reservationService;
+        this.waitingService = waitingService;
     }
 
     @GetMapping("/reservations")
@@ -30,8 +35,8 @@ public class ReservationController {
     public ResponseEntity create(@RequestBody ReservationRequest reservationRequest, LoginMember loginMember) {
         if (
                 reservationRequest.getDate() == null
-                || reservationRequest.getTheme() == null
-                || reservationRequest.getTime() == null) {
+                        || reservationRequest.getTheme() == null
+                        || reservationRequest.getTime() == null) {
             return ResponseEntity.badRequest().build();
         }
 
@@ -44,5 +49,23 @@ public class ReservationController {
     public ResponseEntity delete(@PathVariable Long id) {
         reservationService.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/reservations-mine")
+    public ResponseEntity<List<MyReservationResponse>> findReservationsByMember(LoginMember loginMember) {
+        List<MyReservationResponse> reservationByMember = reservationService.findReservationByMember(loginMember);
+        List<WaitingResponse> waitingByMember = waitingService.findWaitingWithRankByMember(loginMember);
+
+        List<MyReservationResponse> waiting = waitingByMember.stream()
+                .map(MyReservationResponse::from)
+                .toList();
+
+        List<MyReservationResponse> all = Stream.concat(
+                        reservationByMember.stream(),
+                        waiting.stream())
+                .toList();
+
+
+        return ResponseEntity.ok().body(all);
     }
 }
