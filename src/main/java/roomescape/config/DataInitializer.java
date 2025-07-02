4b8116ceb5facbf1e13f87abde0e2c8a954ca395
Jiwoo <ziuoo0424@gmail.com>
2025-07-02 23:04:15 +0900

    test: 내 예약 목록 조회 기능 테스트 코드 추가 - DataInitializer_테스트용 초기 예약 데이터 추가 - MissionStepTest_5단계() 추가

--- src/main/java/roomescape/config/DataInitializer.java
+++ src/main/java/roomescape/config/DataInitializer.java
@@ -5,10 +5,14 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.member.Member;
 import roomescape.member.MemberRepository;
+import roomescape.reservation.Reservation;
+import roomescape.reservation.ReservationRepository;
 import roomescape.theme.Theme;
 import roomescape.theme.ThemeRepository;
 import roomescape.time.Time;
 import roomescape.time.TimeRepository;
+
+import java.time.LocalDate;
 
 @Component
 public class DataInitializer {
@@ -16,27 +20,35 @@ public class DataInitializer {
     private final MemberRepository memberRepository;
     private final ThemeRepository themeRepository;
     private final TimeRepository timeRepository;
+    private final ReservationRepository reservationRepository;
 
-    public DataInitializer(MemberRepository memberRepository, ThemeRepository themeRepository, TimeRepository timeRepository) {
+    public DataInitializer(MemberRepository memberRepository, ThemeRepository themeRepository, TimeRepository timeRepository,
+                           ReservationRepository reservationRepository) {
         this.memberRepository = memberRepository;
         this.themeRepository = themeRepository;
         this.timeRepository = timeRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @PostConstruct
     @Transactional
     public void init() {
         // Member 데이터
-        memberRepository.save(new Member("어드민", "admin@email.com", "password", "ADMIN"));
+        Member admin = memberRepository.save(new Member("어드민", "admin@email.com", "password", "ADMIN"));
         memberRepository.save(new Member("클로이", "chloe@email.com", "password", "USER"));
 
         // Theme 데이터
-        themeRepository.save(new Theme("공포", "매우 무서운 테마", "https://i.imgur.com/1.jpg"));
-        themeRepository.save(new Theme("코믹", "매우 웃긴 테마", "https://i.imgur.com/2.jpg"));
+        Theme theme1 = themeRepository.save(new Theme("공포", "매우 무서운 테마", "https://i.imgur.com/1.jpg"));
+        Theme theme2 = themeRepository.save(new Theme("코믹", "매우 웃긴 테마", "https://i.imgur.com/2.jpg"));
+        Theme theme3 = themeRepository.save(new Theme("어드벤처", "신나는 모험 테마", "https://i.imgur.com/3.jpg"));
 
         // Time 데이터
-        timeRepository.save(new Time("10:00"));
-        timeRepository.save(new Time("13:00"));
-        timeRepository.save(new Time("15:00"));
+        Time time1 = timeRepository.save(new Time("10:00"));
+        Time time2 = timeRepository.save(new Time("13:00"));
+        Time time3 = timeRepository.save(new Time("15:00"));
+
+        reservationRepository.save(new Reservation(LocalDate.parse("2024-03-01"), time1, theme1, admin));
+        reservationRepository.save(new Reservation(LocalDate.parse("2024-03-01"), time2, theme2, admin));
+        reservationRepository.save(new Reservation(LocalDate.parse("2024-03-01"), time3, theme3, admin));
     }
 }
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -10,11 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.dto.MyReservationResponse;
 import roomescape.reservation.dto.ReservationResponse;
 import roomescape.time.Time;
 import roomescape.time.TimeRepository;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -102,5 +104,19 @@ public class MissionStepTest {
                 .get("/admin")
                 .then().log().all()
                 .statusCode(200);
+    }
+
+    @Test
+    void 오단계() {
+        String adminToken = createToken("admin@email.com", "password");
+
+        List<MyReservationResponse> reservations = RestAssured.given().log().all()
+                .cookie("token", adminToken)
+                .get("/reservations-mine")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList(".", MyReservationResponse.class);
+
+        assertThat(reservations).hasSize(3);
     }
 }
