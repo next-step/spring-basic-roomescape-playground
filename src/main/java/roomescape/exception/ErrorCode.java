d03495244dae910b554f9660d9ccb21853615d67
gjtjrl303 <gjtjrl303@naver.com>
2025-07-02 23:23:26 +0900

    feat: errorCode 추가

--- src/main/java/roomescape/exception/ErrorCode.java
+++ src/main/java/roomescape/exception/ErrorCode.java
@@ -4,12 +4,18 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 사용자를 찾을 수 없습니다."),
+    MEMBER_NOT_FOUND(HttpStatus.UNAUTHORIZED, "해당 사용자를 찾을 수 없습니다."),
     INVALID_LOGIN(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 잘못되었습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     INVALID_TOKEN_SIGNATURE(HttpStatus.UNAUTHORIZED, "토큰의 서명이 유효하지 않습니다."),
-    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다.");
-
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED, "관리자 권한이 필요합니다."),
+    DUPLICATE_RESERVATION(HttpStatus.BAD_REQUEST, "이미 예약을 했습니다."),
+    DUPLICATE_WAITING(HttpStatus.BAD_REQUEST, "이미 예약 대기를 했습니다"),
+    TIME_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 시간 정보를 찾을 수 없습니다."),
+    THEME_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 테마 정보를 찾을 수 없습니다."),
+    WAITING_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 예약 대기 정보를 찾을 수 없습니다"),
+    RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 예약 정보를 찾을 수 없습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
