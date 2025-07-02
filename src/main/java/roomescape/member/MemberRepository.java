ee19a297ce6f37393869047419c99d417d95bf1c
gjtjrl303 <gjtjrl303@naver.com>
2025-07-02 23:21:11 +0900

    refactor: MemberDao → MemberRepository로 변경 및 EntityManager 적용

---
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -0,0 +1,32 @@
+package roomescape.member;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public Member save(Member member) {
+        entityManager.persist(member);
+        return member;
+    }
+
+    public Member getByEmailAndPassword(String email, String password) {
+        return entityManager.createQuery(
+                        "SELECT m FROM Member m WHERE m.email = :email AND m.password = :password", Member.class)
+                .setParameter("email", email)
+                .setParameter("password", password)
+                .getSingleResult();
+    }
+
+    public Optional<Member> findById(Long memberId) {
+        Member member = entityManager.find(Member.class, memberId);
+        return Optional.ofNullable(member);
+    }
+}
