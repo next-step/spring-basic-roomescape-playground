a89dc12812eef2cfe3621dfba3ced12cebf0263b
gjtjrl303 <gjtjrl303@naver.com>
2025-07-02 23:26:50 +0900

    refactor: service dao에서 repository쓰도록 변경 및 transactional 추가

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -1,33 +1,35 @@
 package roomescape.member;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import jakarta.persistence.NoResultException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.exception.ErrorCode;
 import roomescape.exception.RoomEscapeException;
 
 @Service
+@Transactional
 public class MemberService {
-    private MemberDao memberDao;
+    private MemberRepository memberRepository;
 
-    public MemberService(MemberDao memberDao) {
-        this.memberDao = memberDao;
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
-        Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
+        Member member = memberRepository.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail(), member.getRole());
     }
 
     public Member authenticate(String email, String password) {
         try {
-            return memberDao.findByEmailAndPassword(email, password);
-        } catch (EmptyResultDataAccessException e) {
+            return memberRepository.getByEmailAndPassword(email, password);
+        } catch (NoResultException e) {
             throw new RoomEscapeException(ErrorCode.INVALID_LOGIN);
         }
     }
 
     public MemberResponse findById(Long memberId) {
-        Member findMember = memberDao.findById(memberId);
+        Member findMember = memberRepository.findById(memberId).orElseThrow(() ->new RoomEscapeException(ErrorCode.MEMBER_NOT_FOUND));
         return new MemberResponse(findMember.getId(), findMember.getName(), findMember.getEmail(), findMember.getRole());
     }
 
