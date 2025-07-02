f312601b64f54a277f71bd0cef1e159008bac08d
Jiwoo <ziuoo0424@gmail.com>
2025-07-02 22:18:12 +0900

    style: 필드에 @Getter 추가

--- src/main/java/roomescape/time/AvailableTime.java
+++ src/main/java/roomescape/time/AvailableTime.java
@@ -1,22 +1,20 @@
 package roomescape.time;
 
+import lombok.Getter;
+
 public class AvailableTime {
+
+    @Getter
     private Long timeId;
+    @Getter
     private String time;
+    @Getter
     private boolean booked;
 
     public AvailableTime(Long timeId, String time, boolean booked) {
         this.timeId = timeId;
         this.time = time;
         this.booked = booked;
-    }
-
-    public Long getTimeId() {
-        return timeId;
-    }
-
-    public String getTime() {
-        return time;
     }
 
     public boolean isBooked() {
