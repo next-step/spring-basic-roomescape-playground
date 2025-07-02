362bc284dad4ed08d5e48985119631a5abc4375e
gjtjrl303 <gjtjrl303@naver.com>
2025-07-02 23:26:50 +0900

    refactor: service dao에서 repository쓰도록 변경 및 transactional 추가

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -1,34 +1,74 @@
 package roomescape.reservation;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.auth.LoginMember;
+import roomescape.exception.RoomEscapeException;
+import roomescape.member.MemberRepository;
+import roomescape.theme.ThemeRepository;
+import roomescape.time.TimeRepository;
 
 import java.util.List;
 
-@Service
-public class ReservationService {
-    private ReservationDao reservationDao;
+import static roomescape.exception.ErrorCode.*;
 
-    public ReservationService(ReservationDao reservationDao) {
-        this.reservationDao = reservationDao;
+@Service
+@Transactional
+public class ReservationService {
+
+    private final ReservationRepository reservationRepository;
+    private final MemberRepository memberRepository;
+    private final ThemeRepository themeRepository;
+    private final TimeRepository timeRepository;
+
+    public ReservationService(ReservationRepository reservationRepository, MemberRepository memberRepository, ThemeRepository themeRepository, TimeRepository timeRepository) {
+        this.reservationRepository = reservationRepository;
+        this.memberRepository = memberRepository;
+        this.themeRepository = themeRepository;
+        this.timeRepository = timeRepository;
     }
 
     public ReservationResponse save(ReservationRequest reservationRequest, LoginMember loginMember) {
         if (reservationRequest.getName() == null || reservationRequest.getName().isBlank()) {
-            reservationRequest = new ReservationRequest(loginMember.name(), reservationRequest.getDate(),reservationRequest.getTheme(),reservationRequest.getTime());
+            reservationRequest = new ReservationRequest(loginMember.name(), reservationRequest.getDate(), reservationRequest.getTheme(), reservationRequest.getTime());
         }
-        Reservation reservation = reservationDao.save(reservationRequest);
 
-        return new ReservationResponse(reservation.getId(), reservationRequest.getName(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
+        validateDuplicatedReservation(reservationRequest, loginMember);
+
+        Reservation reservation = new Reservation(reservationRequest.getName(),
+                reservationRequest.getDate(),
+                timeRepository.findById(reservationRequest.getTime()).orElseThrow(()->new RoomEscapeException(TIME_NOT_FOUND)),
+                themeRepository.findById(reservationRequest.getTheme()).orElseThrow(()->new RoomEscapeException(THEME_NOT_FOUND)),
+                memberRepository.findById(loginMember.id()).orElseThrow(() -> new RoomEscapeException(MEMBER_NOT_FOUND)));
+
+        reservationRepository.save(reservation);
+        return new ReservationResponse(reservation.getId(), reservationRequest.getName(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getTime());
     }
 
     public void deleteById(Long id) {
-        reservationDao.deleteById(id);
+        reservationRepository.deleteById(id);
     }
 
     public List<ReservationResponse> findAll() {
-        return reservationDao.findAll().stream()
-                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
+        return reservationRepository.findAll().stream()
+                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getTime()))
                 .toList();
+    }
+
+    public List<MyReservationResponse> findReservationByMember(LoginMember loginMember) {
+        return reservationRepository.findByMemberId(loginMember.id()).stream()
+                .map(MyReservationResponse::from)
+                .toList();
+    }
+
+    private void validateDuplicatedReservation(ReservationRequest reservationRequest, LoginMember loginMember) {
+        boolean alreadyReserved = reservationRepository.existsByMemberIdAndThemeIdAndDateAndTimeId(
+                loginMember.id(),
+                reservationRequest.getTheme(),
+                reservationRequest.getDate(),
+                reservationRequest.getTime());
+        if (alreadyReserved) {
+            throw new RoomEscapeException(DUPLICATE_RESERVATION);
+        }
     }
 }
