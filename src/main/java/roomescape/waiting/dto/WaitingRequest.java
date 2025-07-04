4b4cfc3015f570b2576187e05008343517ed4b95
Jiwoo <ziuoo0424@gmail.com>
2025-07-05 00:55:42 +0900

    refactor(dto): WaitingRequest 필드명 변경 - timeId → time, themeId → theme

--- src/main/java/roomescape/waiting/dto/WaitingRequest.java
+++ src/main/java/roomescape/waiting/dto/WaitingRequest.java
@@ -1,23 +1,18 @@
 package roomescape.waiting.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor
 public class WaitingRequest {
 
-    @Getter
     private LocalDate date;
-    @Getter
-    private Long timeId;
-    @Getter
-    private Long themeId;
-
-    private WaitingRequest() {}
-
-    public WaitingRequest(LocalDate date, Long timeId, Long themeId) {
-        this.date = date;
-        this.timeId = timeId;
-        this.themeId = themeId;
-    }
+    private Long time;
+    private Long theme;
 }
