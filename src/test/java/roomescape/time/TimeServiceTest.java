ae15d001a83ed9acf08eefe1bd499c6d26b35b60
gjtjrl303 <gjtjrl303@naver.com>
2025-07-07 10:32:42 +0900

    feat: time 소프트 딜리트 테스트 코드 구현

--- src/test/java/roomescape/time/TimeServiceTest.java
+++ src/test/java/roomescape/time/TimeServiceTest.java
@@ -3,6 +3,7 @@ package roomescape.time;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.exception.RoomEscapeException;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class TimeServiceTest {
 
     @Autowired
@@ -38,6 +40,35 @@ class TimeServiceTest {
         assertThatThrownBy(() -> timeService.deleteById(response.id()))
                 .isInstanceOf(RoomEscapeException.class)
                 .hasMessage("시간이 다른 자원에서 사용중입니다.");
+    }
+
+    @Test
+    void 중복된_시간이_들어가면_안된다() {
+        //given
+        TimeRequest timeRequest1 = new TimeRequest("13:00");
+        TimeRequest timeRequest2 = new TimeRequest("13:00");
+        timeService.save(timeRequest1);
+
+        //then
+        assertThatThrownBy(() -> timeService.save(timeRequest2))
+                .isInstanceOf(RoomEscapeException.class)
+                .hasMessage("이미 존재하는 시간입니다.");
+    }
+
+    @Test
+    void 소프트_삭제후_다시넣기_가능해야한다() {
+        //given
+        TimeRequest timeRequest1 = new TimeRequest("13:00");
+        TimeRequest timeRequest2 = new TimeRequest("13:00");
+        TimeResponse saved1 = timeService.save(timeRequest1);
+        timeService.deleteById(saved1.id());
+
+        //when
+        TimeResponse saved2 = timeService.save(timeRequest2);
+
+        //then
+        assertThat(saved2.id()).isGreaterThan(0L);
+        assertThat(saved2.value()).isEqualTo(timeRequest2.value());
     }
 
 }
