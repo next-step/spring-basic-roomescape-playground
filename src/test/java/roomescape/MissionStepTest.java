d59bd4a299d20e150d7b73ab287dacc731bd2696
gjtjrl303 <gjtjrl303@naver.com>
2025-07-07 12:01:54 +0900

    refactor: 기본 데이터 대신 독립적인 테스트 데이터를 사용

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -130,7 +130,7 @@ public class MissionStepTest {
 
     @Test
     void 사단계() {
-        Time time = new Time("10:00");
+        Time time = new Time("11:00");
         entityManager.persist(time);
         entityManager.flush();
         entityManager.clear();
