c7268782027b0c32a81b670d1d97cebf564d8bdb
gjtjrl303 <gjtjrl303@naver.com>
2025-07-07 10:31:01 +0900

    feat: theme 테스트에 transactional 추가

--- src/test/java/roomescape/theme/ThemeServiceTest.java
+++ src/test/java/roomescape/theme/ThemeServiceTest.java
@@ -3,6 +3,7 @@ package roomescape.theme;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import roomescape.exception.RoomEscapeException;
 
 import java.util.List;
@@ -10,6 +11,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class ThemeServiceTest {
 
     @Autowired
