c05119599299df38dbc78202fc85e9f4e7c3ac44
gjtjrl303 <gjtjrl303@naver.com>
2025-07-09 17:43:54 +0900

    refactor: MemberRepository의 EntityManager 사용 로직을 JpaRepository로 전환

--- src/main/java/roomescape/member/MemberRepository.java
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -1,39 +1,15 @@
 package roomescape.member;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    Optional<Member> findByEmail(String email);
 
-    public Member save(Member member) {
-        entityManager.persist(member);
-        return member;
-    }
+    boolean existsByEmail(String email);
 
-    public Member getByEmail(String email) {
-        return entityManager.createQuery(
-                        "SELECT m FROM Member m WHERE m.email = :email", Member.class)
-                .setParameter("email", email)
-                .getSingleResult();
-    }
-
-    public Optional<Member> findById(Long memberId) {
-        Member member = entityManager.find(Member.class, memberId);
-        return Optional.ofNullable(member);
-    }
-
-    public Optional<Member> findByEmail(String email) {
-        return entityManager.createQuery(
-                        "SELECT m FROM Member m WHERE m.email =:email", Member.class)
-                .setParameter("email", email)
-                .getResultStream()
-                .findFirst();
-    }
 }
