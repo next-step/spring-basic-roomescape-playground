0f926bb1c54326fdc37d376774649c4243c2fdda
jihwanyeom <yum020428@gmail.com>
2025-07-09 21:20:10 +0900

    refactor : ThemeRepository 에 JpaRepository 적용

--- src/main/java/roomescape/theme/Theme.java
+++ src/main/java/roomescape/theme/Theme.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@SQLDelete(sql = "UPDATE theme SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
 public class Theme {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
