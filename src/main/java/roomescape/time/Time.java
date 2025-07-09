9a3996600107258f85419582f79f29474fb68427
jihwanyeom <yum020428@gmail.com>
2025-07-09 21:21:24 +0900

    refactor : TimeRepository 에 JpaRepository 적용

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
+@SQLDelete(sql = "UPDATE time SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
 public class Time {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
