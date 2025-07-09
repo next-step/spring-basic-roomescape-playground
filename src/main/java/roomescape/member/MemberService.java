681eb9af2a0859433466f132f076330833f99b88
gjtjrl303 <gjtjrl303@naver.com>
2025-07-09 17:43:54 +0900

    refactor: MemberRepository의 EntityManager 사용 로직을 JpaRepository로 전환

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -1,6 +1,5 @@
 package roomescape.member;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import roomescape.exception.ErrorCode;
@@ -9,7 +8,8 @@ import roomescape.exception.RoomEscapeException;
 @Service
 @Transactional
 public class MemberService {
-    private MemberRepository memberRepository;
+
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
@@ -22,24 +22,22 @@ public class MemberService {
     }
 
     public Member authenticate(String email, String password) {
-        try {
-            Member findMember = memberRepository.getByEmail(email);
-            if (!findMember.getPassword().equals(password)) {
-                throw new RoomEscapeException(ErrorCode.INVALID_LOGIN,"비밀번호가 틀렸습니다.");
-            }
-            return findMember;
-        } catch (EmptyResultDataAccessException e) {
-            throw new RoomEscapeException(ErrorCode.INVALID_LOGIN,"이메일이 잘못되었습니다.");
+        Member findMember = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new RoomEscapeException(ErrorCode.INVALID_LOGIN, "이메일이 잘못되었습니다."));
+        if (!findMember.getPassword().equals(password)) {
+            throw new RoomEscapeException(ErrorCode.INVALID_LOGIN, "비밀번호가 틀렸습니다.");
         }
+        return findMember;
     }
 
     public MemberResponse getById(Long memberId) {
-        Member findMember = memberRepository.findById(memberId).orElseThrow(() ->new RoomEscapeException(ErrorCode.MEMBER_NOT_FOUND));
+        Member findMember = memberRepository
+                .findById(memberId).orElseThrow(() -> new RoomEscapeException(ErrorCode.MEMBER_NOT_FOUND));
         return MemberResponse.from(findMember);
     }
 
     private void validateDuplicateEmail(MemberRequest memberRequest) {
-        if (memberRepository.findByEmail(memberRequest.email()).isPresent()) {
+        if (memberRepository.existsByEmail(memberRequest.email())) {
             throw new RoomEscapeException(ErrorCode.DUPLICATE_EMAIL);
         }
     }
