142c771e593eeab5eada8e8c3d0944f695f538de
gjtjrl303 <gjtjrl303@naver.com>
2025-07-11 12:38:17 +0900

    refactor: findByEmail 예외 처리 로직을 default 메서드 getByEmailOrThrow로 분리

--- src/main/java/roomescape/member/MemberRepository.java
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -2,6 +2,8 @@ package roomescape.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import roomescape.exception.ErrorCode;
+import roomescape.exception.RoomEscapeException;
 
 import java.util.Optional;
 
@@ -11,5 +13,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    default Member getByEmailOrThrow(String email) {
+        return findByEmail(email).orElseThrow(
+                () -> new RoomEscapeException(ErrorCode.INVALID_LOGIN, "이메일이 잘못되었습니다."));
+    }
 
 }
