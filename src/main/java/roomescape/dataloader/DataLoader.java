86f2263f37839f6d91c127b8194d177fdb661b8f
gjtjrl303 <gjtjrl303@naver.com>
2025-07-16 20:49:56 +0900

    refactor: 초기 데이터 주입을 schema.sql → DataLoader 클래스 방식으로 변경

---
+++ src/main/java/roomescape/dataloader/DataLoader.java
@@ -0,0 +1,30 @@
+package roomescape.dataloader;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import roomescape.member.Member;
+import roomescape.member.MemberRepository;
+import roomescape.member.Role;
+
+@Profile("prod")
+@Component
+@Transactional
+public class DataLoader implements CommandLineRunner {
+
+    private final MemberRepository memberRepository;
+
+    public DataLoader(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Member member1 = new Member("어드민", "admin@email.com", "password", Role.ADMIN);
+        Member member2 = new Member("브라운", "brown@email.com", "password", Role.USER);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+    }
+}
