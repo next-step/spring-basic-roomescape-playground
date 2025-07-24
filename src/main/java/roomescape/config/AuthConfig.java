716da1f3f9e9407cd041141d5e6d767b84053883
Jiwoo <ziuoo0424@gmail.com>
2025-07-24 15:20:56 +0900

    refactor: AuthConfig 애플리케이션 내부로 이동

--- src/main/java/roomescape/config/AuthConfig.java
+++ src/main/java/roomescape/config/AuthConfig.java
@@ -1,5 +1,6 @@
-package auth;
+package roomescape.config;
 
+import auth.JwtUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
