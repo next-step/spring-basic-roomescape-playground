ba75e000aab36baeac21fc1f5c3d6d14a87abe47
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2025-12-30 17:46:30 +0900

    chore: 사용하지 않는 import 제거

--- src/main/java/roomescape/controller/ReservationController.java
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -1,6 +1,5 @@
 package roomescape.controller;
 
-import java.util.Optional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
