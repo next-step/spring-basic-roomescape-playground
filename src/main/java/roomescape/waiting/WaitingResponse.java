1c30a057d87745e213f454fea67a34851eca5c22
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-01-01 20:26:29 +0900

    refactor: waitingResponse의 setter 추가(테스트코드용)

--- src/main/java/roomescape/waiting/WaitingResponse.java
+++ src/main/java/roomescape/waiting/WaitingResponse.java
@@ -3,12 +3,19 @@ package roomescape.waiting;
 public class WaitingResponse {
     private Long id;
 
+    public WaitingResponse() {
+    }
+
     public WaitingResponse(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
 
