fa017b9ce81bf0cd0e8dd7f97f34fc4e41c631d9
Suhan Ha <10433434+chemistryx@users.noreply.github.com>
2026-01-08 00:39:09 +0900

    chore: 불필요한 코드 제거

--- src/test/java/roomescape/JpaTest.java
+++ src/test/java/roomescape/JpaTest.java
@@ -6,12 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Import;
 import roomescape.model.Time;
 import roomescape.repository.TimeRepository;
 
 @DataJpaTest
-@Import(TimeRepository.class)
 public class JpaTest {
     @Autowired
     private TestEntityManager entityManager;
