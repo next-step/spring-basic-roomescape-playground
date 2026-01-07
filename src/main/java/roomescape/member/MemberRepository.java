739e9dbe59bc8f33a1dfedf81ac4252ffc9ab8c2
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-01-08 01:39:33 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/roomescape/member/MemberRepository.java
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmailAndPassword(String email, String password);
-    Optional<Member> findByName(String name);
 }
 
 
