2afcb4ab47e3e0e2d57d181a8e18e3a5237b3239
ke-62 <6jtgoeun@naver.com>
2026-01-07 17:31:52 +0900

    refactor:nullable 누락된 곳 추가

--- src/main/java/roomescape/waiting/Waiting.java
+++ src/main/java/roomescape/waiting/Waiting.java
@@ -25,7 +25,7 @@ public class Waiting {
     private Theme theme;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     private Member member;
 
     protected Waiting() {
