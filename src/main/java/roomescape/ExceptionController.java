134fb439690b07219b06445e688c7c1a26a8654e
mintcoke123 <115970383+mintcoke123@users.noreply.github.com>
2026-01-08 16:46:08 +0900

    refactor: ApiError 확장 후 적용

--- src/main/java/roomescape/ExceptionController.java
+++ src/main/java/roomescape/ExceptionController.java
@@ -1,14 +1,49 @@
 package roomescape;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import roomescape.common.ApiError;
 
-@ControllerAdvice
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
 public class ExceptionController {
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> handleRuntimeException(Exception e) {
-        e.printStackTrace();
-        return ResponseEntity.badRequest().build();
-    }
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Map<String, Object>> handleValidation(MethodArgumentNotValidException e) {
+		return build(ApiError.BAD_REQUEST_INVALID_INPUT);
+	}
+
+	@ExceptionHandler({IllegalStateException.class})
+	public ResponseEntity<Map<String, Object>> handleIllegalState(IllegalStateException e) {
+		return build(ApiError.BAD_REQUEST_ILLEGAL_STATE);
+	}
+
+	@ExceptionHandler({NoSuchElementException.class, EntityNotFoundException.class})
+	public ResponseEntity<Map<String, Object>> handleNotFound(RuntimeException e) {
+		return build(ApiError.NOT_FOUND_RESOURCE);
+	}
+
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public ResponseEntity<Map<String, Object>> handleNotReadable(HttpMessageNotReadableException e) {
+		return build(ApiError.BAD_REQUEST_INVALID_INPUT);
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<Map<String, Object>> handleUnknown(Exception e) {
+		e.printStackTrace();
+		return build(ApiError.INTERNAL_SERVER_ERROR);
+	}
+
+	private ResponseEntity<Map<String, Object>> build(ApiError apiError) {
+		Map<String, Object> body = new HashMap<>();
+		body.put("code", apiError.getCode());
+		body.put("message", apiError.getMessage());
+		return ResponseEntity.status(apiError.getHttpStatus()).body(body);
+	}
 }
