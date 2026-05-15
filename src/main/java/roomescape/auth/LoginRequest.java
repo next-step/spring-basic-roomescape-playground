6468465dd32c2d4265157f6dec4b02915709dc84
Gopistol <rhrjs0131@naver.com>
2026-05-16 00:37:03 +0900

    feat: Login관련 DTO 추가

---
+++ src/main/java/roomescape/auth/LoginRequest.java
@@ -0,0 +1,15 @@
+package roomescape.auth;
+
+public class LoginRequest {
+
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/roomescape/auth/LoginResponse.java
@@ -0,0 +1,14 @@
+package roomescape.auth;
+
+public class LoginResponse {
+
+    private String accessToken;
+
+    public LoginResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+}
