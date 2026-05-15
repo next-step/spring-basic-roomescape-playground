05934219503e283a8a3998306f4527bd9951b230
Gopistol <rhrjs0131@naver.com>
2026-05-16 02:08:20 +0900

    feat: resolver 설정

---
+++ src/main/java/roomescape/member/LoginMemberArgumentResolver.java
@@ -0,0 +1,30 @@
+package roomescape.member;
+
+import org.jetbrains.annotations.Nullable;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private MemberService memberService;
+
+    public LoginMemberArgumentResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(Member.class);
+    }
+
+    @Nullable
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+            @Nullable ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+            @Nullable WebDataBinderFactory binderFactory) throws Exception {
+        return null;
+    }
+}
