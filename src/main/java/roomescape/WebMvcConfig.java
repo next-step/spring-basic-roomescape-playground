e351798db5defd8836c151ef4c2d2217b5ccf31f
Gopistol <rhrjs0131@naver.com>
2026-05-16 02:08:26 +0900

    feat: resolver 설정

---
+++ src/main/java/roomescape/WebMvcConfig.java
@@ -0,0 +1,26 @@
+package roomescape;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.AuthArgumentResolver;
+import roomescape.auth.AuthService;
+import roomescape.member.MemberService;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthService authService;
+    private final MemberService memberService;
+
+    public WebMvcConfig(AuthService authService, MemberService memberService) {
+        this.authService = authService;
+        this.memberService = memberService;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthArgumentResolver(authService, memberService));
+    }
+}
