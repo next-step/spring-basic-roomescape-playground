26e5717f542673d0ee054218b31d3dfb57fcfbc9
selee1012 <dltjddms1012@gmail.com>
2026-05-17 23:03:38 +0900

    feat : third-step-complete

--- src/main/java/roomescape/PageController.java
+++ src/main/java/roomescape/PageController.java
@@ -1,13 +1,42 @@
 package roomescape;
 
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import roomescape.member.Member;
+import roomescape.member.MemberDao;
 
 @Controller
 public class PageController {
+    MemberDao memberDao;
+
+    public PageController(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
     @GetMapping("/admin")
-    public String admin() {
-        return "admin/index";
+    public String admin(@CookieValue(value = "token", required = false) String token, HttpServletResponse response) {
+        if (token == null) {
+            response.setStatus(401);
+            return null;
+        }
+
+        String memberId = Jwts.parser()
+                .setSigningKey("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789AB")
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        Member member = memberDao.findById(Integer.parseInt(memberId));
+        if (member.getRole().equals("ADMIN"))
+            return "admin/index";
+        else
+            response.setStatus(401);
+            return null;
+
     }
 
     @GetMapping("/admin/reservation")
