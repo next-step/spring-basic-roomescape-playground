b2e2e2c0b70b6a6a14d25aa6c416cb378219b644
selee1012 <dltjddms1012@gmail.com>
2026-05-22 20:48:48 +0900

    feat : modified record ResevationRequest

--- src/main/java/roomescape/reservation/ReservationRequest.java
+++ src/main/java/roomescape/reservation/ReservationRequest.java
@@ -1,29 +1,3 @@
 package roomescape.reservation;
 
-// record로 구현 불가능 -> setName으로 인해 name이 final이 아니라서
-public class ReservationRequest {
-    private String name;
-    private String date;
-    private Long theme;
-    private Long time;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public Long getTheme() {
-        return theme;
-    }
-
-    public Long getTime() {
-        return time;
-    }
-}
+public record ReservationRequest(String name, String date, Long theme, Long time) { }
