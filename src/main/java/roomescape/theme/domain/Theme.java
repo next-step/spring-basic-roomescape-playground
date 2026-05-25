0f5fa37b86489d0341e272fc44028aefd81ee4af
Gopistol <rhrjs0131@naver.com>
2026-05-26 00:57:34 +0900

    feat: Theme 엔티티 전환

--- src/main/java/roomescape/theme/domain/Theme.java
+++ src/main/java/roomescape/theme/domain/Theme.java
@@ -1,10 +1,27 @@
 package roomescape.theme.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.HashSet;
+import java.util.Set;
+import roomescape.reservation.domain.Reservation;
+
+@Entity
 public class Theme {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String description;
+
+    @OneToMany(mappedBy = "theme")
+    private Set<Reservation> reservations = new HashSet<>();
 
     public Theme() {
     }
@@ -30,5 +47,9 @@ public class Theme {
 
     public String getDescription() {
         return description;
+    }
+
+    public Set<Reservation> getReservations() {
+        return reservations;
     }
 }
