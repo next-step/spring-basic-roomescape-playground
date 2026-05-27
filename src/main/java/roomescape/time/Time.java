cbe834ec877eb4124e064de2be975646e1aa3f00
selee1012 <dltjddms1012@gmail.com>
2026-05-28 02:55:01 +0900

    feat : changed entity use JPA

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,8 +1,21 @@
 package roomescape.time;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Time {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String value;
+
+    protected Time() {
+    }
 
     public Time(Long id, String value) {
         this.id = id;
