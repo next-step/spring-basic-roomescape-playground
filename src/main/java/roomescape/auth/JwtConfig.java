37006da899ba824096a85e18bb1efdc76f32cbad
selee1012 <dltjddms1012@gmail.com>
2026-06-21 23:20:16 +0900

    chore : secretKey 변수로 변경

--- src/main/java/roomescape/auth/JwtConfig.java
+++ src/main/java/roomescape/auth/JwtConfig.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 @Configuration
 public class JwtConfig {
 
+    @Value("${roomescape.auth.jwt.secret}") String secretKey
+
     @Bean
-    public JwtUtils jwtUtils(@Value("${jwt.secret}") String secretKey) {
+    public JwtUtils jwtUtils(String secretKey) {
         return new JwtUtils(secretKey);
     }
 }
