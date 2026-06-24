1a1958c42f670dd8a0c0017a77730c3cdfdd11e8
PARK JUNSEONG <jun0829park@gmail.com>
2026-06-24 23:09:30 +0900

    feat: 로그인 요청 DTO 추가

---
+++ src/main/java/roomescape/auth/LoginRequest.java
@@ -0,0 +1,9 @@
+package roomescape.auth;
+
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+}
