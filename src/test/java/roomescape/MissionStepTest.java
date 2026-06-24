2938a045440a5089d057494a659b9a3844ce4c02
PARK JUNSEONG <jun0829park@gmail.com>
2026-06-25 01:05:58 +0900

    feat: 1단계 미션 테스트 내용 추가

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.HashMap;
 import java.util.Map;
+import roomescape.reservation.ReservationResponse;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,4 +36,43 @@ public class MissionStepTest {
 
         assertThat(token).isNotBlank();
     }
+
+    @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+    public class MissionStepTest {
+        @Test
+        void 이단계() {
+            String token = createToken("admin@email.com", "password");  // 일단계에서 토큰을 추출하는 로직을 메서드로 따로 만들어서 활용하세요.
+
+            Map<String, String> params = new HashMap<>();
+            params.put("date", "2024-03-01");
+            params.put("time", "1");
+            params.put("theme", "1");
+
+            ExtractableResponse<Response> response = RestAssured.given().log().all()
+                    .body(params)
+                    .cookie("token", token)
+                    .contentType(ContentType.JSON)
+                    .post("/reservations")
+                    .then().log().all()
+                    .extract();
+
+            assertThat(response.statusCode()).isEqualTo(201);
+            assertThat(response.as(ReservationResponse.class).getName()).isEqualTo("어드민");
+
+            params.put("name", "브라운");
+
+            ExtractableResponse<Response> adminResponse = RestAssured.given().log().all()
+                    .body(params)
+                    .cookie("token", token)
+                    .contentType(ContentType.JSON)
+                    .post("/reservations")
+                    .then().log().all()
+                    .extract();
+
+            assertThat(adminResponse.statusCode()).isEqualTo(201);
+            assertThat(adminResponse.as(ReservationResponse.class).getName()).isEqualTo("브라운");
+        }
+    }
+
 }
