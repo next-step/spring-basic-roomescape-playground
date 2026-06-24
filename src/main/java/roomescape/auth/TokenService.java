82bf547e44e4a8cb45fc326c70ca89d0bffc3a2e
PARK JUNSEONG <jun0829park@gmail.com>
2026-06-24 23:09:22 +0900

    feat: JWT 토큰 생성 및 파싱 서비스 추가

---
+++ src/main/java/roomescape/auth/TokenService.java
@@ -0,0 +1,36 @@
+package roomescape.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import roomescape.member.Member;
+
+@Service
+public class TokenService {
+
+    private final String secretKey;
+
+    public TokenService(@Value("${roomescape.auth.jwt.secret}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String createToken(Member member) {
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .compact();
+    }
+
+    public Long getMemberIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return Long.valueOf(claims.getSubject());
+    }
+}
