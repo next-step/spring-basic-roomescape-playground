65461dacfd799e2f096ce30407e263af5cf1502c
monsaumon <monsaumon@naver.com>
2026-06-25 15:41:37 +0900

    refactor: null 검증 제거 및 HTTP 응답 코드 변경

--- src/main/java/roomescape/admin/AdminInterceptor.java
+++ src/main/java/roomescape/admin/AdminInterceptor.java
@@ -3,6 +3,7 @@ package roomescape.admin;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import roomescape.member.Member;
@@ -21,8 +22,8 @@ public class AdminInterceptor implements HandlerInterceptor {
         Cookie[] cookies = request.getCookies();
         Member member = memberService.extractMemberFromCookie(cookies);
 
-        if (member == null || !member.getRole().equals("ADMIN")) {
-            response.setStatus(401);
+        if (!member.getRole().equals("ADMIN")) {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
             return false;
         }
 
