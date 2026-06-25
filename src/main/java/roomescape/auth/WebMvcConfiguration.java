2f02531bb7df1108062fe80e8d73cc39464e9f46
PARK JUNSEONG <jun0829park@gmail.com>
2026-06-25 09:24:32 +0900

    feat: 어드민 인터셉터 WebMvc 설정에 등록

--- src/main/java/roomescape/auth/WebMvcConfiguration.java
+++ src/main/java/roomescape/auth/WebMvcConfiguration.java
@@ -2,6 +2,7 @@ package roomescape.auth;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import roomescape.member.MemberDao;
 
@@ -21,5 +22,11 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver(memberDao, tokenService));
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AdminInterceptor(memberDao, tokenService))
+                .addPathPatterns("/admin/**");
     }
 }
