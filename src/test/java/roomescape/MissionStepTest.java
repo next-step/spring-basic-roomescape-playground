8d87340828f448dce4dc951f07c12fefaa9a9777
monsaumon <monsaumon@naver.com>
2026-06-26 10:35:31 +0900

    fix: 상태 코드 수정

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -92,7 +92,7 @@ public class MissionStepTest {
                 .cookie("token", brownToken)
                 .get("/admin")
                 .then().log().all()
-                .statusCode(401);
+                .statusCode(403);
 
         String adminToken = memberService.createToken("admin@email.com", "password");
 
