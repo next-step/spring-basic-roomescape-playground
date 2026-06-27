94922ffcac784e6c839a541aad622aab96be0481
chanh <leechan7120@skku.edu>
2026-06-27 15:20:52 +0900

    fix : token 만료 기한 추가

--- src/main/java/roomescape/token/TokenProvider.java
+++ src/main/java/roomescape/token/TokenProvider.java
@@ -4,17 +4,21 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.util.Date;
 import roomescape.member.Member;
 
 public class TokenProvider {
     private static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
     private static final Key SIGNING_KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    private static final long ACCESS_TOKEN_EXPIRATION = 1000 * 60 * 15;
 
     public static String createToken(Member member) {
         return Jwts.builder()
                 .setSubject(member.getId().toString())
                 .claim("name", member.getName())
                 .claim("role", member.getRole())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_EXPIRATION))
                 .signWith(SIGNING_KEY)
                 .compact();
     }
