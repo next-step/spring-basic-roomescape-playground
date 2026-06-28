569db11b05b38f8f9e47b4f97b2d126821757f4b
pce0303 <ivychoen@gmail.com>
2026-06-29 00:15:46 +0900

    refactor: JWT 키 숨기기

--- src/main/java/roomescape/auth/JwtTokenProvider.java
+++ src/main/java/roomescape/auth/JwtTokenProvider.java
@@ -2,35 +2,37 @@ package roomescape.auth;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.member.Member;
 
 @Component
 public class JwtTokenProvider {
-    private static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+    private final Key secretKey;
+
+    public JwtTokenProvider(@Value("${jwt.secret-key}") String secretKey) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
 
     public String createToken(Member member) {
         return Jwts.builder()
                 .setSubject(member.getId().toString())
                 .claim("name", member.getName())
                 .claim("role", member.getRole())
-                .signWith(getSecretKey())
+                .signWith(secretKey)
                 .compact();
     }
 
     public Long extractMemberId(String token) {
         String subject = Jwts.parserBuilder()
-                .setSigningKey(getSecretKey())
+                .setSigningKey(secretKey)
                 .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
 
         return Long.valueOf(subject);
-    }
-
-    private Key getSecretKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
     }
 }
