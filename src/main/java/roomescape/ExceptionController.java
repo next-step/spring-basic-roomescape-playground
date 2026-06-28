41b0627aecbf466598b29565638fca7b283258f8
TheDeveloperKwon <akongwon@gmail.com>
2026-06-28 14:58:11 +0900

    fix: 검증 어노테이션 핸들러 누락 수정

--- src/main/java/roomescape/ExceptionController.java
+++ src/main/java/roomescape/ExceptionController.java
@@ -1,11 +1,18 @@
 package roomescape;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
+        return ResponseEntity.badRequest().body(message);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Void> handleRuntimeException(Exception e) {
         e.printStackTrace();
--- src/main/java/roomescape/login/LoginController.java
+++ src/main/java/roomescape/login/LoginController.java
@@ -2,6 +2,7 @@ package roomescape.login;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody LoginRequest request, HttpServletResponse response){
+    public ResponseEntity<Void> login(@Valid @RequestBody LoginRequest request, HttpServletResponse response){
         Cookie cookie = loginService.createCookieWithToken(request.getEmail(), request.getPassword());
         response.addCookie(cookie);
         return ResponseEntity.ok().build();
