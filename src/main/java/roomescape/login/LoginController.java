7e0c71b1e9a6e88587886d666965964c29896e07
chaehyunL <chaehyun1012@gmail.com>
2026-06-30 13:31:26 +0900

    feat: token에 관한 책임 분리

--- src/main/java/roomescape/login/LoginController.java
+++ src/main/java/roomescape/login/LoginController.java
@@ -1,7 +1,5 @@
 package roomescape.login;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -10,14 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.JwtProvider;
 import roomescape.member.Member;
 import roomescape.member.MemberResponse;
 
 @RestController
 public class LoginController {
     private final LoginService loginService;
+    private final JwtProvider jwtProvider;
 
-    public LoginController(LoginService loginService) {
+    public LoginController(LoginService loginService, JwtProvider jwtProvider) {
+        this.jwtProvider = jwtProvider;
         this.loginService = loginService;
     }
 
@@ -25,14 +26,7 @@ public class LoginController {
     public ResponseEntity<Void> login(@RequestBody LoginRequest loginRequest, HttpServletResponse httpresponse) {
         Member member = loginService.login(loginRequest.email(), loginRequest.password());
 
-        String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
-
-        String token = Jwts.builder()
-                .setSubject(member.getId().toString())
-                .claim("name", member.getName())
-                .claim("role", member.getRole())
-                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
-                .compact();
+        String token = jwtProvider.createToken(member);
 
         Cookie cookie = new Cookie("token", token);
         cookie.setHttpOnly(true);
