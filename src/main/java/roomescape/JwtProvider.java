05a06d3443a10dc8c3637222a0f6e30056550ee4
chaehyunL <chaehyun1012@gmail.com>
2026-06-30 13:30:28 +0900

    feat: createToken 메서드 생성

--- src/main/java/roomescape/JwtProvider.java
+++ src/main/java/roomescape/JwtProvider.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
+import roomescape.member.Member;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -14,6 +15,14 @@ public class JwtProvider {
     private final String secretKeyString = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
     private final Key secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
 
+    public String createToken(Member member){
+        return Jwts.builder()
+                .setSubject(member.getId().toString())
+                .claim("name", member.getName())
+                .claim("role", member.getRole())
+                .signWith(secretKey)
+                .compact();
+    }
 
     public Long getMemberId(String token) {
         Claims claims = parseClaims(token);
