05ba157b00d85863b402a1cf18c6c3e0f44c02c6
dahyn <k0d5h16@gmail.com>
2026-07-02 01:01:05 +0900

    feat: 일반 사용자의 대리 예약 생성 제한

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -43,10 +43,19 @@ public class ReservationService {
     }
 
     private Member resolveReservationMember(ReservationRequest request, LoginMember loginMember) {
+
         if (request.getName() == null) {
             return memberService.findById(loginMember.id());
         }
 
-        return memberService.findByName(request.getName());
+        if (loginMember.role().equals("ADMIN")) {
+            return memberService.findByName(request.getName());
+        }
+
+        if (!loginMember.name().equals(request.getName())) {
+            throw new IllegalArgumentException("다른 사용자의 이름으로 예약할 수 없습니다.");
+        }
+
+        return memberService.findById(loginMember.id());
     }
 }
