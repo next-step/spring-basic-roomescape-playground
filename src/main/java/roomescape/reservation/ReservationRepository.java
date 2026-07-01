38e9a48a08511f0ab72477ca317936f19e7dbbc2
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 02:27:28 +0900

    feat: ReservationRepository 추가

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -1,4 +1,9 @@
 package roomescape.reservation;
 
-public interface ReservationRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    List<Reservation> findByDateAndTheme_Id(String date, Long themeId);
 }
