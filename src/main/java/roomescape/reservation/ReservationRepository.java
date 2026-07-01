ca3c91b7417ad03738d18e3725285dbd63496ed8
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 03:04:44 +0900

    feat: ReservationRepository에 회원별 예약 조회 쿼리 추가

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByDateAndTheme_Id(String date, Long themeId);
+    List<Reservation> findByMember_Id(Long memberId);
 }
