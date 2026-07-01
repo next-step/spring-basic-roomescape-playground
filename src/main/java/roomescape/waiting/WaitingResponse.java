16288de4df6ee82e7375ab8473532baf6c58d889
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 03:35:01 +0900

    feat: WaitingResponse DTO 추가

--- src/main/java/roomescape/waiting/WaitingResponse.java
+++ src/main/java/roomescape/waiting/WaitingResponse.java
@@ -1,4 +1,31 @@
 package roomescape.waiting;
 
-public class WaitingResponce {
+public class WaitingResponse {
+    private Long id;
+    private String theme;
+    private String date;
+    private String time;
+
+    public WaitingResponse(Long id, String theme, String date, String time) {
+        this.id = id;
+        this.theme = theme;
+        this.date = date;
+        this.time = time;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
 }
