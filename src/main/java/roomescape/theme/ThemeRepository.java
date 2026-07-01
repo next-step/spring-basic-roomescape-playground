e6bb1cace7a8fa894b52868f61161074a1e8abd5
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 02:26:47 +0900

    feat: ThemeRepository 추가

--- src/main/java/roomescape/theme/ThemeRepository.java
+++ src/main/java/roomescape/theme/ThemeRepository.java
@@ -1,4 +1,19 @@
 package roomescape.theme;
 
-public class ThemeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+
+    List<Theme> findAllByDeletedFalse();
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Theme t SET t.deleted = true WHERE t.id = :id")
+    void softDeleteById(@Param("id") Long id);
 }
