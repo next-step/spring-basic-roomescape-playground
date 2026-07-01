393a5ecc5506902dd821cdfe9e092c3b187b5147
dahyn <k0d5h16@gmail.com>
2026-07-01 22:08:06 +0900

    refactor: JWT Secret Key 하드코딩 제거

--- src/main/java/roomescape/auth/JwtProvider.java
+++ src/main/java/roomescape/auth/JwtProvider.java
@@ -2,27 +2,29 @@ package roomescape.auth;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.member.Member;
 
 @Component
 public class JwtProvider {
 
-    private static final String SECRET_KEY = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+    @Value("${roomescape.auth.jwt.secret}")
+    private String secretKey;
 
     public String createToken(Member member) {
         return Jwts.builder()
                 .setSubject(member.getId().toString())
                 .claim("name", member.getName())
                 .claim("role", member.getRole())
-                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
                 .compact();
     }
 
     public Long extractMemberId(String token) {
         return Long.valueOf(
                 Jwts.parserBuilder()
-                        .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                        .setSigningKey(Keys.hmacShaKeyFor(secretKey.getBytes()))
                         .build()
                         .parseClaimsJws(token)
                         .getBody()
