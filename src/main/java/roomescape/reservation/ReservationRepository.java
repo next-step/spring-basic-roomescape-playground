eb6d56c8bdee30a83ddc6d02f70673fffe73b45b
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 03:35:01 +0900

    feat: ReservationRepository에 슬롯 중복 및 회원별 중복 검증 쿼리 추가 (요구사항 b, a)

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByDateAndTheme_Id(String date, Long themeId);
     List<Reservation> findByMember_Id(Long memberId);
+    boolean existsByDateAndTime_IdAndTheme_Id(String date, Long timeId, Long themeId);
+    boolean existsByMember_IdAndDateAndTime_IdAndTheme_Id(Long memberId, String date, Long timeId, Long themeId);
 }
