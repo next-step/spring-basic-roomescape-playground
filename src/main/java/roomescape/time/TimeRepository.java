e3a8ca9565cfdc68215c29057b2e307f817d5410
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 02:26:38 +0900

    feat: TimeRepository 추가

--- src/main/java/roomescape/time/TimeRepository.java
+++ src/main/java/roomescape/time/TimeRepository.java
@@ -1,4 +1,19 @@
 package roomescape.time;
 
-public class TimeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface TimeRepository extends JpaRepository<Time, Long> {
+
+    List<Time> findAllByDeletedFalse();
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Time t SET t.deleted = true WHERE t.id = :id")
+    void softDeleteById(@Param("id") Long id);
 }
