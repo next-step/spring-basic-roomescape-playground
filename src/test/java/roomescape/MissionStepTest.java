e486cc8cfc7e06a1a23833e568ee5d9f92c1dfae
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 03:04:44 +0900

    test: 오단계 테스트 추가 및 미사용 import 정리

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,17 +5,14 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.MyReservationResponse;
 import roomescape.reservation.ReservationResponse;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import roomescape.time.Time;
-import roomescape.time.TimeRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -120,4 +117,17 @@ public class MissionStepTest {
                 .statusCode(200);
     }
 
+    @Test
+    void 오단계() {
+        String adminToken = createToken("admin@email.com", "password");
+
+        List<MyReservationResponse> reservations = RestAssured.given().log().all()
+                .cookie("token", adminToken)
+                .get("/reservations-mine")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList(".", MyReservationResponse.class);
+
+        assertThat(reservations).hasSize(3);
+    }
 }
