354d06e155db6dedc4b452b33605b7a55f020aa9
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-02 03:35:02 +0900

    refactor: MyReservationResponse의 reservationId를 id로 변경 (예약/대기 통합 응답의 공통 식별자)

--- src/main/java/roomescape/reservation/MyReservationResponse.java
+++ src/main/java/roomescape/reservation/MyReservationResponse.java
@@ -1,22 +1,22 @@
 package roomescape.reservation;
 
 public class MyReservationResponse {
-    private Long reservationId;
+    private Long id;
     private String theme;
     private String date;
     private String time;
     private String status;
 
-    public MyReservationResponse(Long reservationId, String theme, String date, String time, String status) {
-        this.reservationId = reservationId;
+    public MyReservationResponse(Long id, String theme, String date, String time, String status) {
+        this.id = id;
         this.theme = theme;
         this.date = date;
         this.time = time;
         this.status = status;
     }
 
-    public Long getReservationId() {
-        return reservationId;
+    public Long getId() {
+        return id;
     }
 
     public String getTheme() {
