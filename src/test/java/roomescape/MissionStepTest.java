dfabb0c42eb4e758266e01ee4e6ad061cdb0a1b5
chanh <leechan7120@skku.edu>
2026-07-04 16:22:48 +0900

    feat : 4단계

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -106,4 +106,19 @@ public class MissionStepTest {
 
         return response.headers().get("Set-Cookie").getValue().split(";")[0].split("=")[1];
     }
+
+    @Test
+    void 오단계() {
+        String adminToken = createToken("admin@email.com", "password");
+
+        List<MyReservationResponse> reservations = RestAssured.given().log().all()
+                .cookie("token", adminToken)
+                .get("/reservations-mine")
+                .then().log().all()
+                .statusCode(200)
+                .extract().jsonPath().getList(".", MyReservationResponse.class);
+
+        assertThat(reservations).hasSize(3);
+    }
+}
 }
