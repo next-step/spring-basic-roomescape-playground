16b97aa434a11a88264f026595befcd14cd63e40
chaehyunL <chaehyun1012@gmail.com>
2026-07-04 20:01:03 +0900

    refector: 메서드 extracttoken 삭제

--- src/main/java/roomescape/login/LoginController.java
+++ src/main/java/roomescape/login/LoginController.java
@@ -41,17 +41,4 @@ public class LoginController {
         MemberResponse memberResponse = loginService.checkLogin(memberId);
         return ResponseEntity.ok(memberResponse);
     }
-
-    private String extractTokenFromCookie(Cookie[] cookies) {
-        if (cookies == null) {
-            throw new RuntimeException("Invalid cookies");
-        }
-        for (Cookie cookie : cookies) {
-            if (cookie.getName().equals("token")) {
-                return cookie.getValue();
-            }
-        }
-        throw new RuntimeException("Token not found");
-    }
-
 }
