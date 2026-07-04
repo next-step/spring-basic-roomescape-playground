3128ef8539eb3e9aca20d5851eddf79f464a5387
chaehyunL <chaehyun1012@gmail.com>
2026-07-04 19:59:29 +0900

    refector: 메서드 입력 객체 변경

--- src/main/java/roomescape/login/LoginController.java
+++ src/main/java/roomescape/login/LoginController.java
@@ -1,7 +1,6 @@
 package roomescape.login;
 
 import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
