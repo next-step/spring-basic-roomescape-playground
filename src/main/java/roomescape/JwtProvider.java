4836e92c6f5b3a9d72bef9f42f0863fe92d0676e
chaehyunL <chaehyun1012@gmail.com>
2026-07-04 20:38:43 +0900

    refector: secretKey 숨김

--- src/main/java/roomescape/JwtProvider.java
+++ src/main/java/roomescape/JwtProvider.java
@@ -3,6 +3,7 @@ package roomescape;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.member.Member;
 
@@ -12,10 +13,13 @@ import java.security.Key;
 @Component
 public class JwtProvider {
 
-    private final String secretKeyString = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
-    private final Key secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    private final Key secretKey;
 
-    public String createToken(Member member){
+    public JwtProvider(@Value("${jwt.secret}") String secretKeyString) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createToken(Member member) {
         return Jwts.builder()
                 .setSubject(member.getId().toString())
                 .claim("name", member.getName())
