2ec1baeb599429491bc5bb1a4fc530fbf25e814c
chaehyunL <chaehyun1012@gmail.com>
2026-07-05 00:49:05 +0900

    feat:  JwtProvider에서 시크릿 키 삭세

--- src/main/java/roomescape/JwtProvider.java
+++ src/main/java/roomescape/JwtProvider.java
@@ -3,6 +3,7 @@ package roomescape;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import roomescape.member.Member;
 
@@ -12,9 +13,12 @@ import java.security.Key;
 @Component
 public class JwtProvider {
 
-    private final String secretKeyString = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
-    private final Key secretKey = Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
 
+    private final Key secretKey;
+
+    public JwtProvider(@Value("${roomescape.auth.jwt.secret}")String secretKeyString){
+        this.secretKey= Keys.hmacShaKeyFor(secretKeyString.getBytes(StandardCharsets.UTF_8));
+    }
     public String createToken(Member member){
         return Jwts.builder()
                 .setSubject(member.getId().toString())
