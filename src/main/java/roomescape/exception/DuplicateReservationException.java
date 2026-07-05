a6ba5db13c564f341b586c9ae3a50770f0ec6232
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-05 14:10:39 +0900

    feat: 예약 중복 예외 추가

--- src/main/java/roomescape/exception/DuplicateReservationException.java
+++ src/main/java/roomescape/exception/DuplicateReservationException.java
@@ -1,4 +1,7 @@
 package roomescape.exception;
 
-public class DuplicateReservationException {
+public class DuplicateReservationException extends RoomescapeException {
+    public DuplicateReservationException(String message) {
+        super(message);
+    }
 }
