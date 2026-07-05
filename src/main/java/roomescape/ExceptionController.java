1ebb6dd8cf04cdcbe7beb59584c2f1d9e243dced
PARK JUNSEONG <jun0829park@gmail.com>
2026-07-05 14:10:39 +0900

    refactor: ExceptionController에 인증/중복 예외별 상태 코드 매핑 추가

--- src/main/java/roomescape/ExceptionController.java
+++ src/main/java/roomescape/ExceptionController.java
@@ -1,14 +1,27 @@
 package roomescape;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import roomescape.exception.AuthenticationException;
+import roomescape.exception.DuplicateReservationException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<Void> handleAuthentication(AuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    @ExceptionHandler(DuplicateReservationException.class)
+    public ResponseEntity<Void> handleDuplicate(DuplicateReservationException e) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> handleRuntimeException(Exception e) {
-        e.printStackTrace();
+    public ResponseEntity<Void> handleException(Exception e) {
         return ResponseEntity.badRequest().build();
     }
 }
