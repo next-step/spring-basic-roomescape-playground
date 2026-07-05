4184b798916af00823bc776146ca5cb69b9e81fc
chaehyunL <chaehyun1012@gmail.com>
2026-07-05 16:43:57 +0900

    refector: Exception 패키지 내로 이동

--- src/main/java/roomescape/exception/ExceptionController.java
+++ src/main/java/roomescape/exception/ExceptionController.java
@@ -1,5 +1,6 @@
-package roomescape;
+package roomescape.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,5 +11,10 @@ public class ExceptionController {
     public ResponseEntity<Void> handleRuntimeException(Exception e) {
         e.printStackTrace();
         return ResponseEntity.badRequest().build();
+    }
+    @ExceptionHandler(TokenNotFoundException.class)
+    public ResponseEntity<String> handleTokenNotFoundException(Exception e){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(e.getMessage());
     }
 }
