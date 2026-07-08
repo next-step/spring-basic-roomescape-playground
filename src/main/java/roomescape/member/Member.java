1eb39ab1523ae211f6e21de7da1f47c14157bd97
chaehyunL <chaehyun1012@gmail.com>
2026-07-09 00:33:29 +0900

    refector: protected 추가

--- src/main/java/roomescape/member/Member.java
+++ src/main/java/roomescape/member/Member.java
@@ -10,11 +10,14 @@ public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final String name;
-    private final String email;
-    private final String password;
-    private final String role;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
 
+    protected Member(){
+
+    }
     public Member(String name, String email, String password, String role) {
         this.name = name;
         this.email = email;
