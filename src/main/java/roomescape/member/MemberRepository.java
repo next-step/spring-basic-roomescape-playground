b9cc04bef1821582801f62e904787bf73add35d8
chaehyunL <chaehyun1012@gmail.com>
2026-07-08 19:59:15 +0900

    refector: JPA 변경에 따른 수정

--- src/main/java/roomescape/member/MemberRepository.java
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -2,8 +2,11 @@ package roomescape.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import roomescape.theme.Theme;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmailAndPasword(String email,String passward);
 }
