715960c27de3e50a1ca93f94e123c3cc96cbe2a5
chaehyunL <chaehyun1012@gmail.com>
2026-07-11 00:55:49 +0900

    feat: cookieManager 생성

---
+++ src/main/java/roomescape/CookieManager.java
@@ -0,0 +1,18 @@
+package roomescape;
+
+import jakarta.servlet.http.Cookie;
+import roomescape.exception.TokenNotFoundException;
+
+import java.util.Arrays;
+
+public class CookieManager {
+
+
+    public String extractToken(Cookie[] cookies, String cookieName) {
+        return Arrays.stream(cookies)
+                .filter(cookie -> cookieName.equals(cookie.getName()))
+                .map(Cookie::getValue)
+                .findFirst()
+                .orElseThrow(TokenNotFoundException::new);
+    }
+}
