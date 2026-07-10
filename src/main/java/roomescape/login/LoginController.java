a27286332b5a841307b00de32552cdf3ed07684b
chaehyunL <chaehyun1012@gmail.com>
2026-07-11 00:56:11 +0900

    refector: cookieManager 이용해서 수정

--- src/main/java/roomescape/login/LoginController.java
+++ src/main/java/roomescape/login/LoginController.java
@@ -9,21 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import roomescape.CookieManager;
 import roomescape.JwtProvider;
-import roomescape.exception.TokenNotFoundException;
 import roomescape.member.Member;
 import roomescape.member.MemberResponse;
-
-import java.util.Arrays;
 
 @RestController
 public class LoginController {
     private final LoginService loginService;
     private final JwtProvider jwtProvider;
+    private final CookieManager cookieManager;
 
-    public LoginController(LoginService loginService, JwtProvider jwtProvider) {
+    public LoginController(LoginService loginService, JwtProvider jwtProvider, CookieManager cookieManager) {
         this.jwtProvider = jwtProvider;
         this.loginService = loginService;
+        this.cookieManager = cookieManager;
     }
 
     @PostMapping("/login")
@@ -31,10 +31,10 @@ public class LoginController {
         Member member = loginService.login(loginRequest.email(), loginRequest.password());
 
         String accessToken = jwtProvider.createAccessToken(member);
-        String refreshToken= jwtProvider.createRefreshToken(member);
+        String refreshToken = jwtProvider.createRefreshToken(member);
 
         Cookie accessCookie = new Cookie("accessToken", accessToken);
-        Cookie refreshCookie= new Cookie("refreshToken",refreshToken);
+        Cookie refreshCookie = new Cookie("refreshToken", refreshToken);
 
         accessCookie.setHttpOnly(true);
         accessCookie.setPath("/");
@@ -47,21 +47,21 @@ public class LoginController {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<Void> refresh(HttpServletRequest request,HttpServletResponse response){
-        Cookie[] cookies= request.getCookies();
+    public ResponseEntity<Void> refresh(HttpServletRequest request, HttpServletResponse response) {
+        Cookie[] cookies = request.getCookies();
 
-        if(cookies == null){
+        if (cookies == null) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
-        String refreshToken=extractToken(cookies,"refreshToken");
+        String refreshToken = cookieManager.extractToken(cookies, "refreshToken");
 
         Long memberId = jwtProvider.getMemberId(refreshToken);
 
-        Member member= loginService.findById(memberId);
+        Member member = loginService.findById(memberId);
 
-        String accessToken=jwtProvider.createAccessToken(member);
+        String accessToken = jwtProvider.createAccessToken(member);
 
-        Cookie cookie=new Cookie("accessToken",accessToken);
+        Cookie cookie = new Cookie("accessToken", accessToken);
         cookie.setHttpOnly(true);
         cookie.setPath("/");
         response.addCookie(cookie);
@@ -69,13 +69,6 @@ public class LoginController {
         return ResponseEntity.ok().build();
     }
 
-    private String extractToken(Cookie[] cookies,String cookieName) {
-        return Arrays.stream(cookies)
-                .filter(cookie -> cookieName.equals(cookie.getName()))
-                .map(Cookie::getValue)
-                .findFirst()
-                .orElseThrow(TokenNotFoundException::new);
-    }
     @GetMapping("/login/check")
     public ResponseEntity<MemberResponse> checkLogin(LoginMember loginMember) {
         Long memberId = loginMember.id();
--- src/main/java/roomescape/login/LoginInterceptor.java
+++ src/main/java/roomescape/login/LoginInterceptor.java
@@ -5,28 +5,29 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.CookieManager;
 import roomescape.JwtProvider;
 import roomescape.member.Member;
 import roomescape.member.MemberDao;
-
-import java.util.Arrays;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
 
     private final JwtProvider jwtProvider;
     private final MemberDao memberDao;
+    private final CookieManager cookieManager;
 
-    public LoginInterceptor(JwtProvider jwtProvider, MemberDao memberDao) {
+    public LoginInterceptor(JwtProvider jwtProvider, MemberDao memberDao, CookieManager cookieManager) {
         this.jwtProvider = jwtProvider;
         this.memberDao = memberDao;
+        this.cookieManager = cookieManager;
     }
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Cookie[] cookies = getCookies(request);
+        Cookie[] cookies = request.getCookies();
 
-        String token = extractToken(cookies);
+        String token = cookieManager.extractToken(cookies, "accessToken");
         Long memberId = jwtProvider.getMemberId(token);
         Member member = memberDao.findById(memberId);
         if (member == null || !member.getRole().equals("ADMIN")) {
@@ -34,22 +35,5 @@ public class LoginInterceptor implements HandlerInterceptor {
             return false;
         }
         return true;
-    }
-
-    private Cookie[] getCookies(HttpServletRequest httpServletRequest) {
-        Cookie[] cookies = httpServletRequest.getCookies();
-        if (cookies == null) {
-            throw new IllegalStateException("cookie is not exist");
-        }
-        return cookies;
-    }
-
-    private String extractToken(Cookie[] cookies) {
-        String token = Arrays.stream(cookies)
-                .filter(cookie -> "accessToken".equals(cookie.getName()))
-                .map(Cookie::getValue)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("token is not found in cookies"));
-        return token;
     }
 }
