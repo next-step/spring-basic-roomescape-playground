87aca69fb3145629d0b3df280e4a80bad4312efd
hapdaypy <alsdnrskfk85@naver.com>
2026-07-13 00:18:29 +0900

    fix: cherry-pick conflict cleanup

--- src/main/java/roomescape/WebConfig.java
+++ src/main/java/roomescape/WebConfig.java
@@ -25,7 +25,6 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(adminInterceptor);
     }
