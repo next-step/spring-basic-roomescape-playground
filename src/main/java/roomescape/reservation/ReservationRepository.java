98e7900271a072401976a320334310c200f6dd2c
jxinwoo <12jack030220@gmail.com>
2026-07-14 21:56:52 +0900

    fix: JPA 이관하며 생긴 오류 해결

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
-    public Reservation save(JpaRepository<Reservation, Long> reservation);
-
     public List<Reservation> findByDateAndThemeId(String date, Long themeId);
 
     public List<Reservation> findByMemberEmail(String email);
