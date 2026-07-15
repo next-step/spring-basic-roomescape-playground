cd1a819f0d40a12df58d2bdc5cddb0625d6d2c4f
chaehyunL <chaehyun1012@gmail.com>
2026-07-15 18:50:21 +0900

    feat: entity매니저로 변경

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -1,11 +1,41 @@
 package roomescape.reservation;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-    List<Reservation> findByDateAndThemeId(String date, Long themeId);
+public class ReservationRepository {
+    private final EntityManager entityManager;
+
+    public ReservationRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public Reservation save(Reservation reservation) {
+        entityManager.persist(reservation);
+        return reservation;
+    }
+
+    public Optional<Reservation> findById(Long id) {
+        return Optional.ofNullable(entityManager.find(Reservation.class, id));
+    }
+
+    public List<Reservation> findAll() {
+        return entityManager.createQuery(
+                "SELECT r FROM Reservation r",
+                Reservation.class
+        ).getResultList();
+    }
+
+    public void deleteById(Long id) {
+        Reservation reservation = entityManager.find(Reservation.class, id);
+
+        if (reservation != null) {
+            entityManager.remove(reservation);
+        }
+    }
 }
+
