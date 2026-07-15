3db99b74ab2b9d5c38690f01a71221609f20562c
chaehyunL <chaehyun1012@gmail.com>
2026-07-15 17:06:52 +0900

    style:ctrl_alt_l

--- src/main/java/roomescape/member/MemberRepository.java
+++ src/main/java/roomescape/member/MemberRepository.java
@@ -18,28 +18,27 @@ public class MemberRepository {
         return Optional.ofNullable(entityManager.find(Member.class, id));
     }
 
-    public void save(Member member){
+    public void save(Member member) {
         entityManager.persist(member);
     }
 
-    public List<Member> findAll(){
+    public List<Member> findAll() {
         return entityManager.createQuery(
                 "SELECT m FROM Member m",
                 Member.class
         ).getResultList();
     }
 
-    public Optional<Member> findByEmailAndPassword(String email, String password){
-        List<Member> members =entityManager.createQuery(
-                "SELECT m FROM Member WHERE m.email= :email AND m.password =:password",
-                Member.class
-        ).setParameter("email",email)
-                .setParameter("password",password)
-                        .getResultList();
+    public Optional<Member> findByEmailAndPassword(String email, String password) {
+        List<Member> members = entityManager.createQuery(
+                        "SELECT m FROM Member WHERE m.email= :email AND m.password =:password",
+                        Member.class
+                ).setParameter("email", email)
+                .setParameter("password", password)
+                .getResultList();
 
         return members.stream().findFirst();
     }
-
 
 
 }
