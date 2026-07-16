8a2b3fd9de30ca3161196fd428b7849057183841
hapdaypy <alsdnrskfk85@naver.com>
2026-07-16 21:35:31 +0900

    refactor: 사용되지 않은 생성자 삭제

--- src/main/java/roomescape/member/Member.java
+++ src/main/java/roomescape/member/Member.java
@@ -31,10 +31,6 @@ public class Member {
         this.role = role;
     }
 
-    public Member(Long id, String name, String email, MemberRole role) {
-        this(id, name, email, null, role);
-    }
-
     public Member(String name, String email, String password, MemberRole role) {
         this(null, name, email, password, role);
     }
