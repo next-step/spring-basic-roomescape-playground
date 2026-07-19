afcde38f9c24b522ef0ce64b61dcc30eaa55309e
chaehyunL <chaehyun1012@gmail.com>
2026-07-19 17:06:04 +0900

    refector: entity 삭제

--- src/main/java/roomescape/login/LoginMember.java
+++ src/main/java/roomescape/login/LoginMember.java
@@ -1,9 +1,48 @@
 package roomescape.login;
 
-public record LoginMember(
-        Long id,
-        String name,
-        String email,
-        String role
-) {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+public class LoginMember {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    protected LoginMember() {
+
+    }
+
+    public LoginMember(Long id, String name, String email, String password, String role) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
