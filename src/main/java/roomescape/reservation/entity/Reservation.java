4ff25504a46969d0107bcd8a70979b1284821008
htdufhc-bob <htdufhc@gmail.com>
2026-07-21 12:02:10 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/roomescape/reservation/entity/Reservation.java
+++ src/main/java/roomescape/reservation/entity/Reservation.java
@@ -1,6 +1,5 @@
 package roomescape.reservation.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
