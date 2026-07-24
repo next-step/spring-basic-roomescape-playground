0b719fd52d06b8eea3b9726449610ffec917a578
chaehyunL <chaehyun1012@gmail.com>
2026-07-24 12:12:41 +0900

    refector: 개행정리

--- src/main/java/roomescape/reservation/ReservationController.java
+++ src/main/java/roomescape/reservation/ReservationController.java
@@ -37,7 +37,7 @@ public class ReservationController {
         }
         ReservationResponse reservation = reservationService.save(reservationRequest, loginMember);
 
-        return ResponseEntity.created(URI.create("/reservations/" + reservation.getId())).body(reservation);
+        return ResponseEntity.created(URI.create("/reservations/" + reservation.id())).body(reservation);
     }
 
     @DeleteMapping("/reservations/{id}")
