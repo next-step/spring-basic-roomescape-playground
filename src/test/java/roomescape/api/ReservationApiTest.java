bada4a5f553d07dc0137d2ee99f8586812b0c927
htdufhc-bob <htdufhc@gmail.com>
2026-07-26 08:43:02 +0900

    test: Reservation과 Waiting api 테스트 추가

--- src/test/java/roomescape/api/ReservationApiTest.java
+++ src/test/java/roomescape/api/ReservationApiTest.java
@@ -7,7 +7,9 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import roomescape.reservation.dto.MyReservationResponse;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
@@ -16,6 +18,29 @@ import static org.assertj.core.api.SoftAssertions.assertSoftly;
 public class ReservationApiTest extends ApiTest {
 
     private final static String ACCESS_TOKEN = "access-token";
+
+    @Test
+    void 이미_예약된_시간과_테마로_예약하면_409를_반환한다() {
+        // given
+        String accessToken = 로그인_시도("admin@email.com", "password").cookie(ACCESS_TOKEN);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("date", "2024-03-01");
+        params.put("time", "1");
+        params.put("theme", "1");
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .cookie(ACCESS_TOKEN, accessToken)
+                .body(params)
+                .when().post("/reservations")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(409);
+    }
 
     @Test
     void 로그인한_회원은_자신의_예약_목록을_조회할_수_있다() {
@@ -51,6 +76,4 @@ public class ReservationApiTest extends ApiTest {
         // then
         assertThat(response.statusCode()).isEqualTo(401);
     }
-
-
 }
