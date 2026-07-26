be706722b8a7c36ebba8e0b594ad52887042d45d
haeun92e0 <gracekim8729@gmail.com>
2026-07-27 01:08:53 +0900

    feat: @Configuration 기반의 JwtUtils 빈 등록 설정 추가

--- src/main/java/roomescape/auth/AuthConfig.java
+++ src/main/java/roomescape/auth/AuthConfig.java
@@ -2,7 +2,9 @@ package roomescape.auth;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AuthConfig {
 
     @Value("${roomescape.auth.jwt.secret}")
