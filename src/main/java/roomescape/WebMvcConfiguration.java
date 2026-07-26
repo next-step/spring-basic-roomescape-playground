77f7d71d9c00e6a47330c411acca395c632fc147
haeun92e0 <gracekim8729@gmail.com>
2026-07-27 01:09:02 +0900

    refactor: MemberRepository 의존성 제거 및 JwtUtils 기반 설정으로 수정

--- src/main/java/roomescape/WebMvcConfiguration.java
+++ src/main/java/roomescape/WebMvcConfiguration.java
@@ -1,12 +1,11 @@
 package roomescape;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import roomescape.auth.JwtUtils;
 import roomescape.member.AdminInterceptor;
-import roomescape.member.JwtTokenProvider;
 import roomescape.member.LoginMemberArgumentResolver;
 import roomescape.member.MemberRepository;
 
@@ -15,21 +14,19 @@ import java.util.List;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    private final JwtTokenProvider jwtTokenProvider;
-    private final MemberRepository memberRepository;
+    private final JwtUtils jwtUtils;
 
-    public WebMvcConfiguration(JwtTokenProvider jwtTokenProvider, MemberRepository memberRepository){
-        this.jwtTokenProvider =  jwtTokenProvider;
-        this.memberRepository = memberRepository;
+    public WebMvcConfiguration(JwtUtils jwtUtils){
+        this.jwtUtils = jwtUtils;
     }
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberArgumentResolver(jwtTokenProvider, memberRepository));
+        resolvers.add(new LoginMemberArgumentResolver(jwtUtils));
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(new AdminInterceptor(jwtTokenProvider))
+        registry.addInterceptor(new AdminInterceptor(jwtUtils))
                 .addPathPatterns("/admin/**", "/admin");
     }
 }
