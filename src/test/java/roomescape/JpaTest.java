ff9eff0af0d2ee20032a3fc0e908c478224b66d9
dahyn <k0d5h16@gmail.com>
2026-07-27 00:24:01 +0900

    feat: DataLoader와 TestDataLoader 적용

--- src/test/java/roomescape/JpaTest.java
+++ src/test/java/roomescape/JpaTest.java
@@ -1,32 +1,21 @@
 package roomescape;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Import;
-import roomescape.time.Time;
-import roomescape.time.TimeRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
-@Import(TimeRepository.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class JpaTest {
-    @Autowired
-    private TestEntityManager entityManager;
-
-    @Autowired
-    private TimeRepository timeRepository;
+    @Value("${roomescape.auth.jwt.secret}")
+    private String secretKey;
 
     @Test
-    void 사단계() {
-        Time time = new Time("10:00");
-        entityManager.persist(time);
-        entityManager.flush();
-
-        Time persistTime = timeRepository.findById(time.getId()).orElse(null);
-
-        assertThat(persistTime.getValue()).isEqualTo(time.getValue());
+    void 팔단계() {
+        assertThat(secretKey).isNotBlank();
     }
 }
+
--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
 import roomescape.auth.JwtProvider;
 import roomescape.reservation.dto.MyReservationResponse;
 import roomescape.reservation.dto.ReservationResponse;
@@ -22,6 +23,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 public class MissionStepTest {
 
     @Test
