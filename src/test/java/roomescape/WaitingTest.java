209a9cec9e6c55d0c355ac43fc3fa2cb05512e4c
haeun92e0 <gracekim8729@gmail.com>
2026-07-29 18:21:37 +0900

    test: Waiting 도메인 검증 및 대기 생성 관련 단위 테스트 추가(잘못 삭제해서 다시 커밋)

--- src/test/java/roomescape/WaitingTest.java
+++ src/test/java/roomescape/WaitingTest.java
@@ -9,20 +9,15 @@ import roomescape.waiting.Waiting;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class WaitingTest {
-
     @Test
-    void 동일한_조건의_대기_신청인지_확인_테스트(){
-
+    void 동일한_조건의_대기_신청인지_확인_테스트() {
         Time time = new Time();
         Theme theme = new Theme();
-        Member member =new Member(1L,"MEMBER");
+        Member member = new Member(1L, "MEMBER");
         Waiting waiting = new Waiting("2024-03-01", time, theme, member);
 
         boolean isSame = waiting.isSameWaiting("2024-03-01", time, theme, member);
 
         assertThat(isSame).isTrue();
     }
-
-
-
 }
