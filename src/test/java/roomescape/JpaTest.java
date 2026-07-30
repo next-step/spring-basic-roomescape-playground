0c5d97a4d536fabeb3fd35914db3a3f2d54e7f96
dahyn <k0d5h16@gmail.com>
2026-07-30 15:51:29 +0900

    refactor: JPATest 사단게 롤백 및 테스트 환경 수정

--- src/test/java/roomescape/JpaTest.java
+++ src/test/java/roomescape/JpaTest.java
@@ -1,15 +1,35 @@
 package roomescape;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import roomescape.time.Time;
+import roomescape.time.TimeRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DataJpaTest
 public class JpaTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private TimeRepository timeRepository;
+
+    @Test
+    void 사단계() {
+        Time time = new Time("10:00");
+        entityManager.persist(time);
+        entityManager.flush();
+
+        Time persistTime = timeRepository.findById(time.getId()).orElse(null);
+
+        assertThat(persistTime.getValue()).isEqualTo(time.getValue());
+    }
+
     @Value("${roomescape.auth.jwt.secret}")
     private String secretKey;
 
