79b95a49ea3a32d44be01f6b079997494f56e915
htdufhc-bob <htdufhc@gmail.com>
2026-07-30 22:04:30 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/roomescape/auth/repository/RefreshTokenRepository.java
+++ src/main/java/roomescape/auth/repository/RefreshTokenRepository.java
@@ -11,7 +11,5 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long
 
     Optional<RefreshToken> findByToken(String token);
 
-    Optional<RefreshToken> findByMemberId(Long memberId);
-
     void deleteByMemberId(Long memberId);
 }
