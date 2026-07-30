6097143c8b6453b6bf1f48526eac8f5603fe82ed
htdufhc-bob <htdufhc@gmail.com>
2026-07-30 22:04:18 +0900

    refactor: `@Getter` 적용

--- src/main/java/roomescape/global/entity/BaseSoftDeleteEntity.java
+++ src/main/java/roomescape/global/entity/BaseSoftDeleteEntity.java
@@ -2,6 +2,7 @@ package roomescape.global.entity;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,7 @@ import java.time.ZoneId;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
+@Getter
 public class BaseSoftDeleteEntity {
 
     private static final ZoneId KST_ZONE = ZoneId.of("Asia/Seoul");
@@ -17,9 +19,5 @@ public class BaseSoftDeleteEntity {
 
     public void markDeleted() {
         this.deletedAt = LocalDateTime.now(KST_ZONE);
-    }
-
-    public LocalDateTime getDeletedAt() {
-        return deletedAt;
     }
 }
