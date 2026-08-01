1d863b86a4923f542096ad98bddf591be03fdcf4
haeun92e0 <gracekim8729@gmail.com>
2026-08-01 23:59:12 +0900

    test: 테스트 코드 보완

--- src/test/java/roomescape/WaitingTest.java
+++ src/test/java/roomescape/WaitingTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import roomescape.member.Member;
 import roomescape.member.MemberRepository;
+import roomescape.reservation.Reservation;
+import roomescape.reservation.ReservationRepository;
 import roomescape.reservation.ReservationRequest;
 import roomescape.theme.Theme;
 import roomescape.theme.ThemeRepository;
@@ -30,17 +32,21 @@ public class WaitingTest {
     @Autowired
     private MemberRepository memberRepository;
 
+    @Autowired
+    private ReservationRepository reservationRepository;
+
     @Test
     void 동일한_조건의_대기_신청인지_확인_테스트() {
-        Time time = timeRepository.save(new Time());
-        Theme theme = themeRepository.save(new Theme());
+        Time time = timeRepository.save(new Time("10:00"));
+        Theme theme = themeRepository.save(new Theme("방탈출 테마", "테마 설명"));
         Member member = memberRepository.save(new Member("test@email.com", "1234", "하은", "USER"));
 
+        reservationRepository.save(new Reservation("하은", "2024-03-01", time, theme, member));
         ReservationRequest request = new ReservationRequest("하은", "2024-03-01", theme.getId(), time.getId());
         waitingService.createWaiting(request, member);
 
         assertThatThrownBy(() -> waitingService.createWaiting(request, member))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이미 신청한 대기입니다.");
+                .hasMessage("이미 대기를 신청한 타임입니다.");
     }
 }
