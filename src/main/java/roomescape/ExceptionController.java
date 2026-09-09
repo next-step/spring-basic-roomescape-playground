a334c8f86a26491ca282d67955c459549118b96d
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-09-09 15:10:49 +0900

    feat: MethodArgumentNotValidException 예외 핸들러 메소드와 예외 로깅 추가

--- src/main/java/roomescape/ExceptionController.java
+++ src/main/java/roomescape/ExceptionController.java
@@ -1,14 +1,38 @@
 package roomescape;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.lang.reflect.Executable;
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class ExceptionController {
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Void> handleRuntimeException(Exception e) {
-        e.printStackTrace();
+
+    private final Logger log = LoggerFactory.getLogger(ExceptionController.class);
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Executable executable = e.getParameter().getExecutable();
+        String where = executable.getDeclaringClass().getSimpleName() + "." + executable.getName();
+
+        String detail = e.getBindingResult().getFieldErrors().stream()
+                .map(fe -> "%s = %s' (%s)".formatted(fe.getField(), fe.getRejectedValue(), fe.getDefaultMessage()))
+                .collect(Collectors.joining(","));
+
+        log.info("[{}] {}", where, detail);
+
         return ResponseEntity.badRequest().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleException(Exception e, HttpServletRequest request) {
+        log.error("[{} {}] 처리되지 않은 예외", request.getMethod(), request.getRequestURI(), e);
+        return ResponseEntity.internalServerError().build();
     }
 }
