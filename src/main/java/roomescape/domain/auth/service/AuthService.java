45b70b14d2182b3180f930d17fbe17a73d91e7ea
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2026-09-12 21:00:14 +0900

    fix: 로그인 실패 시 401 반환하게끔 수정

--- src/main/java/roomescape/domain/auth/service/AuthService.java
+++ src/main/java/roomescape/domain/auth/service/AuthService.java
@@ -3,6 +3,7 @@ package roomescape.domain.auth.service;
 import org.springframework.stereotype.Service;
 import roomescape.domain.auth.principal.LoginMember;
 import roomescape.domain.auth.repository.AuthRepository;
+import roomescape.global.exception.UnauthorizedException;
 
 @Service
 public class AuthService {
@@ -15,6 +16,6 @@ public class AuthService {
 
     public LoginMember login(String email, String password) {
         return authRepository.findByEmailAndPassword(email, password)
-                .orElseThrow(() -> new IllegalArgumentException("아이디 혹은 비밀번호가 잘못되었습니다."));
+                .orElseThrow(UnauthorizedException::new);
     }
 }
