7c7cd9d1ff2fb6acb89c3ac58363b43942f71fd3
se0ye2n <hsy040925@gmail.com>
2026-09-15 20:11:15 +0900

    feat:[1단계] 쿠키 기반 로그인 및 인증 정보 조회 API 구현

---
+++ src/main/java/roomescape/member/LoginController.java
@@ -0,0 +1,59 @@
+package roomescape.member;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    private final LoginService loginService;
+
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(
+            @RequestBody LoginRequest request,
+            HttpServletResponse response
+    ) {
+        String token = loginService.login(request);
+
+        Cookie cookie = new Cookie("token", token);
+        cookie.setHttpOnly(true);
+        cookie.setPath("/");
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/login/check")
+    public ResponseEntity<LoginResponse> checkLogin(
+            HttpServletRequest request
+    ) {
+        String token = extractTokenFromCookie(request.getCookies());
+        LoginResponse response = loginService.checkLogin(token);
+
+        return ResponseEntity.ok(response);
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        if (cookies == null) {
+            return "";
+        }
+
+        for (Cookie cookie : cookies) {
+            if ("token".equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+
+        return "";
+    }
+}
