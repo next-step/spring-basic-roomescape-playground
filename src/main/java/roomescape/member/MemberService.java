965e642212d17eba849a3b2593043e25503efb4f
seungminbluebox <parksm1415@gmail.com>
2026-09-17 23:35:59 +0900

    feat(1단계, MemberService) : login, extractTokenFromCookie, tokenToMember 메서드 추가

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -1,9 +1,14 @@
 package roomescape.member;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
+
     private MemberDao memberDao;
 
     public MemberService(MemberDao memberDao) {
@@ -11,7 +16,41 @@ public class MemberService {
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
-        Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(), memberRequest.getPassword(), "USER"));
+        Member member = memberDao.save(new Member(memberRequest.getName(), memberRequest.getEmail(),
+            memberRequest.getPassword(), "USER"));
         return new MemberResponse(member.getId(), member.getName(), member.getEmail());
+    }
+
+    public String login(LoginRequest loginRequest) {
+        Member member = memberDao.findByEmailAndPassword(loginRequest.getEmail(),
+            loginRequest.getPassword());
+        String secretKey = "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=";
+        String accessToken = Jwts.builder()
+            .setSubject(member.getId().toString())
+            .claim("name", member.getName())
+            .claim("role", member.getRole())
+            .signWith(Keys.hmacShaKeyFor(secretKey.getBytes()))
+            .compact();
+        return accessToken;
+    }
+
+    public String extractTokenFromCookie(Cookie[] cookies) {
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+        return "";
+    }
+
+    public Member tokenToMember(String token) {
+        Long memberId = Long.valueOf(Jwts.parserBuilder()
+            .setSigningKey(
+                Keys.hmacShaKeyFor("Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=".getBytes()))
+            .build()
+            .parseClaimsJws(token)
+            .getBody().getSubject());
+        Member member = memberDao.findById(memberId);
+        return member;
     }
 }
