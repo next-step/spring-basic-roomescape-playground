aee9dfd4724d36c19c376f2fa14c4e600d2ca361
seungminbluebox <parksm1415@gmail.com>
2026-09-18 12:36:58 +0900

    feat(2-1단계, LoginMemberArgumentResolver) : LoginMemberArgumentResolver 클래스 생성

---
+++ src/main/java/roomescape/member/LoginMemberArgumentResolver.java
@@ -0,0 +1,32 @@
+package roomescape.member;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private MemberService memberService;
+
+    public LoginMemberArgumentResolver(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request=webRequest.getNativeRequest(HttpServletRequest.class);
+        Cookie[] cookies = request.getCookies();
+        String token = memberService.extractTokenFromCookie(cookies);
+        Member member = memberService.tokenToMember(token);
+
+        return new LoginMember(member.getId(), member.getName(), member.getEmail(), member.getRole());
+    }
+}
