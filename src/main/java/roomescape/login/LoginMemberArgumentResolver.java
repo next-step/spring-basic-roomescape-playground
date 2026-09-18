45a2de147d69cf13fb7060e0f11c8705b638e3de
장서인 <seoin1893@dankook.ac.kr>
2026-09-18 16:45:38 +0900

    feat : LoginMemberArgumentResolver 클래스 작성

---
+++ src/main/java/roomescape/login/LoginMemberArgumentResolver.java
@@ -0,0 +1,77 @@
+package roomescape.login;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import roomescape.member.Member;
+import roomescape.member.MemberDao;
+
+@Component
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberDao memberDao;
+
+    public LoginMemberArgumentResolver(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(LoginMember.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        HttpServletRequest request =
+                (HttpServletRequest) webRequest.getNativeRequest();
+
+        Cookie[] cookies = request.getCookies();
+        String token = extractTokenFromCookie(cookies);
+
+        Long memberId = Long.valueOf(
+                Jwts.parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(
+                            "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=".getBytes()
+                    ))
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody()
+                    .getSubject()
+        );
+
+        Member member = memberDao.findById(memberId);
+
+        return new LoginMember(
+                member.getId(),
+                member.getName(),
+                member.getEmail(),
+                member.getRole()
+        );
+    }
+
+    private String extractTokenFromCookie(Cookie[] cookies) {
+        if (cookies == null) {
+            return "";
+        }
+
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                return cookie.getValue();
+            }
+        }
+
+        return "";
+    }
+}
