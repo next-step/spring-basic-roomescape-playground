329fc7bb32339c089285c1ddb78d85e3ea1d0833
JisuHan <ivyjisu@gmail.com>
2026-09-19 02:58:29 +0900

    feat: 예약 생성 시 요청 이름 유무에 따른 예약자 처리 분기 및 테스트 추가

---
+++ src/test/java/roomescape/LoginMemberReservationTest.java
@@ -0,0 +1,71 @@
+package roomescape;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import roomescape.reservation.ReservationResponse;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class LoginMemberReservationTest {
+    @Test
+    @DisplayName("예약 생성 시 이름이 없으면 로그인 정보로, 이름이 있으면 이름으로 예약된다.")
+    void create_Reservation_with_LoginMember_Or_Name() {
+        String token = createToken("admin@email.com", "password");  // 일단계에서 토큰을 추출하는 로직을 메서드로 따로 만들어서 활용하세요.
+
+        Map<String, String> params = new HashMap<>();
+        params.put("date", "2024-03-01");
+        params.put("time", "1");
+        params.put("theme", "1");
+
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(params)
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .post("/reservations")
+                .then().log().all()
+                .extract();
+
+        assertThat(response.statusCode()).isEqualTo(201);
+        assertThat(response.as(ReservationResponse.class).getName()).isEqualTo("어드민");
+
+        params.put("name", "브라운");
+
+        ExtractableResponse<Response> adminResponse = RestAssured.given().log().all()
+                .body(params)
+                .cookie("token", token)
+                .contentType(ContentType.JSON)
+                .post("/reservations")
+                .then().log().all()
+                .extract();
+
+        assertThat(adminResponse.statusCode()).isEqualTo(201);
+        assertThat(adminResponse.as(ReservationResponse.class).getName()).isEqualTo("브라운");
+    }
+
+    private String createToken(String email, String password) {
+        Map<String, String> params = Map.of(
+                "email", email,
+                "password", password
+        );
+        return RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(params)
+                .when().post("/login")
+                .then().statusCode(200)
+                .extract()
+                .cookie("token");
+    }
+}
+
