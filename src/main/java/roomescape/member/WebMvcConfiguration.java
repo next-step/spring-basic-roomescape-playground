ffa2c2cd8d8cde92dc119ff2ee622a7e08dd5daa
seungminbluebox <parksm1415@gmail.com>
2026-09-18 12:37:19 +0900

    feat(2-1단계, WebMvcConfiguration) : WebMvcConfiguration 생성

---
+++ src/main/java/roomescape/member/WebMvcConfiguration.java
@@ -0,0 +1,19 @@
+package roomescape.member;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    private MemberService memberService;
+
+    public WebMvcConfiguration(MemberService memberService) {
+        this.memberService=memberService;
+    }
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver(memberService));
+    }
+}
