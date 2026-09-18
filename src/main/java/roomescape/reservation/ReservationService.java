259dcdbc424a3cab453da34aad12287e64411721
seungminbluebox <parksm1415@gmail.com>
2026-09-18 13:13:57 +0900

    fix(2-2단계, ReservationService) : 필드변수 MemberDao 추가 및 name 확보로직 추가

--- src/main/java/roomescape/reservation/ReservationService.java
+++ src/main/java/roomescape/reservation/ReservationService.java
@@ -3,19 +3,34 @@ package roomescape.reservation;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import roomescape.member.LoginMember;
+import roomescape.member.Member;
+import roomescape.member.MemberDao;
 
 @Service
 public class ReservationService {
-    private ReservationDao reservationDao;
 
-    public ReservationService(ReservationDao reservationDao) {
+    private ReservationDao reservationDao;
+    private MemberDao memberDao;
+
+    public ReservationService(ReservationDao reservationDao, MemberDao memberDao) {
         this.reservationDao = reservationDao;
+        this.memberDao = memberDao;
     }
 
-    public ReservationResponse save(ReservationRequest reservationRequest) {
-        Reservation reservation = reservationDao.save(reservationRequest);
+    public ReservationResponse save(ReservationRequest reservationRequest,
+        LoginMember loginMember) {
+        Member member;
+        if (reservationRequest.getName() != null) {
+            member = memberDao.findByName(reservationRequest.getName());
+        } else {
+            member = memberDao.findByName(loginMember.getName());
+        }
+        Reservation reservation = reservationDao.save(reservationRequest, member.getName());
 
-        return new ReservationResponse(reservation.getId(), reservationRequest.getName(), reservation.getTheme().getName(), reservation.getDate(), reservation.getTime().getValue());
+        return new ReservationResponse(reservation.getId(), reservation.getName(),
+            reservation.getTheme().getName(), reservation.getDate(),
+            reservation.getTime().getValue());
     }
 
     public void deleteById(Long id) {
@@ -24,7 +39,8 @@ public class ReservationService {
 
     public List<ReservationResponse> findAll() {
         return reservationDao.findAll().stream()
-                .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(), it.getDate(), it.getTime().getValue()))
-                .toList();
+            .map(it -> new ReservationResponse(it.getId(), it.getName(), it.getTheme().getName(),
+                it.getDate(), it.getTime().getValue()))
+            .toList();
     }
 }
